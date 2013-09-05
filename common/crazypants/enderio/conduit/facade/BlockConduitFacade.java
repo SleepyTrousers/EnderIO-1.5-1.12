78863f16199290fb77baf49b702274409a06c6e1
brad-c <crazypants.mc@gmail.com>
2013-09-05 21:07:00 +0800

    Issue 39: Grass facades not rendered correctly.

--- common/crazypants/enderio/conduit/facade/BlockConduitFacade.java
+++ common/crazypants/enderio/conduit/facade/BlockConduitFacade.java
@@ -5,6 +5,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Icon;
+
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -22,12 +23,15 @@ public class BlockConduitFacade extends Block {
     return result;
   }
 
+  private Block blockOverride;
+  
   private BlockConduitFacade() {
-    super(ModObject.blockConduitFacade.id, Material.rock);
+    super(ModObject.blockConduitFacade.id, Material.grass);
     setHardness(0.5F);
     setStepSound(Block.soundStoneFootstep);
     setUnlocalizedName(ModObject.blockConduitFacade.unlocalisedName);
     setCreativeTab(null);
+    
   }
 
   private void init() {
@@ -58,6 +62,62 @@ public class BlockConduitFacade extends Block {
       return block.getIcon(side, meta);
     }
     return blockIcon;
+  }
+
+  @Override
+  @SideOnly(Side.CLIENT)
+  public Icon getIcon(int par1, int par2) {
+    if(blockOverride != null) {
+      return blockOverride.getIcon(par1, par2);
+    }
+    return blockIcon;
+  }
+  
+  @SideOnly(Side.CLIENT)
+  public int getBlockColor() {
+    if(blockOverride != null) {
+      return blockOverride.getBlockColor();
+    } else {
+      return super.getBlockColor();
+    }      
+  }
+  
+  @SideOnly(Side.CLIENT)
+  public int colorMultiplier(IBlockAccess par1IBlockAccess, int par2, int par3, int par4) {
+    if(blockOverride != null) {
+      return blockOverride.colorMultiplier(par1IBlockAccess, par2, par3, par4);
+    } else {
+      return super.colorMultiplier(par1IBlockAccess, par2, par3, par4);
+    }
+  }
+
+
+  public Block getIconOverrideBlock() {
+    return blockOverride;
+  }
+  
+  @SideOnly(Side.CLIENT)
+  public int getRenderColor(int par1) {
+    if(blockOverride != null) {
+      return blockOverride.getRenderColor(par1);
+    } else {
+      return super.getRenderColor(par1);
+    }
+  }
+  
+
+  public void setBlockOverride(IConduitBundle cb) {
+    if(cb == null) {
+      blockOverride = null;
+      return;
+    }
+    
+    int id = cb.getFacadeId();
+    int meta = cb.getFacadeMetadata();
+    if (id <= 0 || id == blockID) {
+      return;
+    }
+    blockOverride = Block.blocksList[id];    
   }
 
   @Override
