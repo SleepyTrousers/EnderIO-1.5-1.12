b14d856ba70cf8cffc60b26fd6a1d0b57fbf7b8e
Alchemist <AlchemistDS@gmail.com>
2013-09-10 15:17:33 +0000

    Fixed chassi code

--- common/crazypants/enderio/material/BlockMachineChassi.java
+++ common/crazypants/enderio/material/BlockMachineChassi.java
@@ -1,7 +1,8 @@
 package crazypants.enderio.material;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
-import net.minecraft.item.ItemBlock;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import cpw.mods.fml.relauncher.Side;
@@ -9,30 +10,32 @@ import cpw.mods.fml.relauncher.SideOnly;
 import crazypants.enderio.EnderIOTab;
 import crazypants.enderio.ModObject;
 
-public class BlockMachineChassi extends ItemBlock {
+public class BlockMachineChassi extends Block {
 
-  public static BlockMachineChassi create() {
-    BlockMachineChassi result = new BlockMachineChassi();
-    result.init();
-    return result;
+	  public static BlockMachineChassi create() {
+		  BlockMachineChassi result = new BlockMachineChassi();
+		    result.init();
+		    return result;
   }
 
-  protected BlockMachineChassi() {
-    super(ModObject.blockMachineChassi.id);
-    setCreativeTab(EnderIOTab.tabEnderIO);
-    setUnlocalizedName(ModObject.blockMachineChassi.unlocalisedName);
-    setMaxStackSize(64);
+	  private BlockMachineChassi() {
+		    super(ModObject.blockMachineChassi.id, Material.iron);
+		    setHardness(0.5F);
+		    setStepSound(Block.soundStoneFootstep);
+		    setUnlocalizedName(ModObject.blockMachineChassi.unlocalisedName);
+		    setCreativeTab(EnderIOTab.tabEnderIO);
   }
 
-  protected void init() {
-    LanguageRegistry.addName(this, ModObject.blockMachineChassi.name);
-    GameRegistry.registerItem(this, ModObject.blockMachineChassi.unlocalisedName);
+	  
+  private void init() {
+	LanguageRegistry.addName(this, ModObject.blockMachineChassi.name);
+	GameRegistry.registerBlock(this, ModObject.blockMachineChassi.unlocalisedName);
   }
 
   @Override
   @SideOnly(Side.CLIENT)
   public void registerIcons(IconRegister iconRegister) {
-    itemIcon = iconRegister.registerIcon("enderio:machineChassi");
+  blockIcon = iconRegister.registerIcon("enderio:machineChassi");
   }
 
 }
