8df876fcea74ed5b9d9f720447675e330ebd5ff3
cp <crazypants.mc@gmail.com>
2013-09-16 21:28:13 +0800

    Fixed case of textures.

--- common/crazypants/enderio/conduit/facade/ItemConduitFacade.java
+++ common/crazypants/enderio/conduit/facade/ItemConduitFacade.java
@@ -47,7 +47,7 @@ public class ItemConduitFacade extends Item {
 
   @Override
   public void registerIcons(IconRegister iconRegister) {
-    itemIcon = iconRegister.registerIcon("enderio:ConduitFacade");
+    itemIcon = iconRegister.registerIcon("enderio:conduitFacade");
     overlayIcon = iconRegister.registerIcon("enderio:conduitFacadeOverlay");
   }
 
