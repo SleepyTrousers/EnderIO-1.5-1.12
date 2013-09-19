218a60dae07528657ffd2b5447d94f9164e8f560
cp <crazypants.mc@gmail.com>
2013-09-19 16:36:36 +0800

    #103 Fixed NPE (merge fail from 1.6)

--- common/crazypants/enderio/power/PowerHandlerUtil.java
+++ common/crazypants/enderio/power/PowerHandlerUtil.java
@@ -3,29 +3,24 @@ package crazypants.enderio.power;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.ForgeDirection;
-import crazypants.enderio.EnderIO;
-import crazypants.enderio.conduit.IConduitBundle;
-import crazypants.enderio.conduit.power.IPowerConduit;
-import crazypants.enderio.conduit.power.NetworkPowerManager;
-import crazypants.enderio.conduit.power.PowerConduitNetwork;
 
 public class PowerHandlerUtil {
 
   public static float getStoredEnergyForItem(ItemStack item) {
     NBTTagCompound tag = item.getTagCompound();
-    if(tag == null) {
+    if (tag == null) {
       return 0;
     }
     return tag.getFloat("storedEnergy");
   }
-  
+
   public static void setStoredEnergyForItem(ItemStack item, float storedEnergy) {
     NBTTagCompound tag = item.getTagCompound();
-    if(tag == null) {
+    if (tag == null) {
       tag = new NBTTagCompound();
-    }    
-    tag.setFloat("storedEnergy", storedEnergy);   
-    item.setTagCompound(tag);    
+    }
+    tag.setFloat("storedEnergy", storedEnergy);
+    item.setTagCompound(tag);
   }
 
   public static EnderPowerProvider createHandler(ICapacitor capacitor) {
@@ -50,27 +45,23 @@ public class PowerHandlerUtil {
     if (ph == null) {
       return 0;
     }
-    
-    float energyStored = ph.getEnergyStored();       
+
+    float energyStored = ph.getEnergyStored();
     float canUse = quantity;
     canUse = Math.min(canUse, ph.getMaxEnergyReceived());
-    //Don't overflow it    
-    canUse = Math.min(canUse, ph.getMaxEnergyStored() - energyStored);   
+    // Don't overflow it
+    canUse = Math.min(canUse, ph.getMaxEnergyStored() - energyStored);
     if (canUse < ph.getMinEnergyReceived()) {
-      ph.receiveEnergy(0, null);
-      ph.setEnergy(energyStored);
       return 0;
     }
 
     ph.receiveEnergy(quantity, from);
-    ph.setEnergy(energyStored);     
+    ph.setEnergy(energyStored);
 
     ph.setEnergy(energyStored + canUse);
-    receptor.applyPerdition();    
+    receptor.applyPerdition();
 
     return canUse;
   }
-
-
 
 }
