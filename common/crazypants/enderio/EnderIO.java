db5bd5850f1a21ff27a072b91a9c163405a16ab3
cp <crazypants.mc@gmail.com>
2013-09-19 18:01:06 +0800

    Release 0.2.0
    Features
    - Added 'Dimensional Transceiver'. Similar in functionality to TE tesseract.
      Power transfer loss is 10%, liquid transfer to be implemeneted soon.
      The block is not correctly balanced at present. The recipe is too cheap and it will probably require continious power to stay open. This will be adressed in the next release.
    
    - Added ability to specify SAG Mill recipes via an XML config file (see config/EnderIoCrusherRecipes.xml for documentation).
      Basic recipes for AE, Forestry and IC2 are enabled by default. If you would like recipes for another mods items included by default raise an issue on Github.
    
    - #96 Provided progamtic access to the XML config to other developers / mods to add recipes at runtime.
    
    - Updated and Improved recipes for some machines.
    
    Bugs
    - #25 Breaking any of machines gives you the block back in creative
    - #40 Stirling gen not filling a TE energy cell
    - #70 Performance loss when using Electric Lights
    - #78 Light passing through facades
    - #88 Forestry's Fermenter not accepting power
    - #94 Crash when removing Capacitor Banks
    - #99 Lighting issue
    - #103 Crash wehn using transciever

--- common/crazypants/enderio/EnderIO.java
+++ common/crazypants/enderio/EnderIO.java
@@ -54,7 +54,7 @@ import crazypants.enderio.material.ItemPowderIngot;
 import crazypants.enderio.material.ItemYetaWrench;
 import crazypants.enderio.material.MaterialRecipes;
 
-@Mod(name = "EnderIO", modid = "EnderIO", version = "0.1.25", dependencies = "required-after:Forge@[9.10.0.800,)")
+@Mod(name = "EnderIO", modid = "EnderIO", version = "0.2.0", dependencies = "required-after:Forge@[9.10.0.800,)")
 @NetworkMod(clientSideRequired = true, serverSideRequired = true, channels = { "EnderIO" }, packetHandler = PacketHandler.class)
 public class EnderIO {
 
