c29996604cec328ed7ba19c78bd849377aa383d7
CrazyPants <crazypants.mc@gmail.com>
2013-10-05 16:55:48 +0800

    Fixed fused quartz frame rendering. Fixed build issue.

--- common/crazypants/util/IOUtils.java
+++ common/crazypants/util/IOUtils.java
@@ -1,8 +1,6 @@
 package crazypants.util;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.Reader;
 import java.io.Writer;
@@ -20,14 +18,22 @@ public class IOUtils {
     try {
       writer.close();
     } catch (Exception e) {
-    }    
+    }
   }
 
   public static void closeQuietly(InputStream is) {
     try {
       is.close();
     } catch (Exception e) {
-    }    
+    }
+  }
+
+  public static void closeQuietly(FileOutputStream fos) {
+    try {
+      fos.close();
+    } catch (Exception e) {
+    }
+
   }
 
 }
