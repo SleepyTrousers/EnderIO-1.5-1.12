d50b472155ae91a017c4ba660c0efab0f0d9e502
CrazyPants <crazypants.mc@gmail.com>
2013-10-09 15:21:27 +0800

    Version 0.3.0 New - Added configurable input and output limits to Capacitor Banks - Added item charging to Capacitor Banks - Added BC gate triggers for Capacitor Banks (implemented by domi1819) - Update SAG Mill recipe update mecansim. The are now 'core' and user defined recipe files to avoid conflicts on update. - Tweaked SAG Mill recipes (implemented by Unrepentant-Atheist)
    Bugs
    - #168 Fixed crash when using Java 1.6
    - #170 Stopped conduits and capacitor banks from draining power into BC item and fluid pipes.

--- common/crazypants/enderio/EnderIO.java
+++ common/crazypants/enderio/EnderIO.java
@@ -59,7 +59,7 @@ import crazypants.enderio.material.MaterialRecipes;
 import crazypants.enderio.trigger.TriggerEnderIO;
 import crazypants.enderio.trigger.TriggerProviderEIO;
 
-@Mod(name = "EnderIO", modid = "EnderIO", version = "0.2.4b", dependencies = "required-after:Forge@[9.10.0.800,)")
+@Mod(name = "EnderIO", modid = "EnderIO", version = "0.3.0", dependencies = "required-after:Forge@[9.10.0.800,)")
 @NetworkMod(clientSideRequired = true, serverSideRequired = true, channels = { "EnderIO" }, packetHandler = PacketHandler.class)
 public class EnderIO {
 
