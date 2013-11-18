722934247f0f7321312b503d40260eac71176c88
CrazyPants <crazypants.mc@gmail.com>
2013-11-18 19:21:40 +0800

    #286 fixed.

--- common/crazypants/enderio/conduit/redstone/RedstoneSwitch.java
+++ common/crazypants/enderio/conduit/redstone/RedstoneSwitch.java
@@ -103,7 +103,7 @@ public class RedstoneSwitch extends RedstoneConduit {
 
   @Override
   public boolean onBlockActivated(EntityPlayer player, RaytraceResult res, List<RaytraceResult> all) {
-    if(res.component.data != null && res.component.data.equals(SWITCH_TAG)) {
+    if(res != null && res.component != null && res.component.data != null && res.component.data.equals(SWITCH_TAG)) {
       toggleSwitch();
       return true;
     }
