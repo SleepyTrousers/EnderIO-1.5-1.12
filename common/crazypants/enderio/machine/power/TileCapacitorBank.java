e193d1fe35246fca3ef02b88ca029a1f37f467e4
Alchemist <Alch3mist@users.noreply.github.com>
2013-11-30 14:43:41 +0000

    Increased capacitor's bank capacity!
    (Thank you thatsIch for correcting me =3)

--- common/crazypants/enderio/machine/power/TileCapacitorBank.java
+++ common/crazypants/enderio/machine/power/TileCapacitorBank.java
@@ -36,7 +36,7 @@ import crazypants.vecmath.VecmathUtil;
 
 public class TileCapacitorBank extends TileEntity implements IInternalPowerReceptor, IInventory {
 
-  static final BasicCapacitor BASE_CAP = new BasicCapacitor(100, 250000);
+  static final BasicCapacitor BASE_CAP = new BasicCapacitor(100, 500000);
 
   BlockCoord[] multiblock = null;
 
