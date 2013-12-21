e154638d561b04e649b62e9932ce361fd1ffac71
CrazyPants <crazypants.mc@gmail.com>
2013-12-21 12:10:12 +0800

    0.5.4 - #262 Machines not getting power if placed after conduits (MCPC). - #350 Item conduits connecting to BC fluid pipes. - #355 ItemConduit crash when modifying TE machine outputs. - #362 NPE in AbstractConduit. - #363 Factorization Barrels no updating display when items removed.

--- common/crazypants/enderio/EnderIO.java
+++ common/crazypants/enderio/EnderIO.java
@@ -67,7 +67,7 @@ import crazypants.enderio.material.MaterialRecipes;
 import crazypants.enderio.trigger.TriggerEnderIO;
 import crazypants.enderio.trigger.TriggerProviderEIO;
 
-@Mod(name = "EnderIO", modid = "EnderIO", version = "0.5.3", dependencies = "required-after:Forge@[9.11.0.883,)")
+@Mod(name = "EnderIO", modid = "EnderIO", version = "0.5.4", dependencies = "required-after:Forge@[9.11.0.883,)")
 @NetworkMod(clientSideRequired = true, serverSideRequired = true, channels = { "EnderIO" }, packetHandler = PacketHandler.class)
 public class EnderIO {
 
