2b6018bf289901c3e3354594d463399d3d82d7c6
CrazyPants <crazypants.mc@gmail.com>
2014-03-03 18:18:54 +0800

    #519 Can't shift-wrench to pick up certain TE blocks

--- common/crazypants/enderio/item/ItemYetaWrench.java
+++ common/crazypants/enderio/item/ItemYetaWrench.java
@@ -49,7 +49,7 @@ public class ItemYetaWrench extends Item implements IToolWrench {
   public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
     int blockId = world.getBlockId(x, y, z);
     Block block = Block.blocksList[blockId];
-    if(block != null && block.rotateBlock(world, x, y, z, ForgeDirection.getOrientation(side))) {
+    if(block != null && block.rotateBlock(world, x, y, z, ForgeDirection.getOrientation(side)) && !player.isSneaking()) {
       player.swingItem();
       return !world.isRemote;
     }
