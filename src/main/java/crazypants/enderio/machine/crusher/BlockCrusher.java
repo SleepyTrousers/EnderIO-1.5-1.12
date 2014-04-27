fb5c245aa5fc69b3fe176d76410177b0b7308d9f
CrazyPants <crazypants.mc@gmail.com>
2014-04-27 13:35:13 +0800

    Improved tool tip for grinding balls (hopefully)

--- src/main/java/crazypants/enderio/machine/crusher/BlockCrusher.java
+++ src/main/java/crazypants/enderio/machine/crusher/BlockCrusher.java
@@ -3,15 +3,21 @@ package crazypants.enderio.machine.crusher;
 import java.util.List;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import crazypants.enderio.GuiHandler;
 import crazypants.enderio.ModObject;
+import crazypants.enderio.gui.IAdvancedTooltipProvider;
+import crazypants.enderio.gui.TooltipAddera;
 import crazypants.enderio.machine.AbstractMachineBlock;
+import crazypants.util.Lang;
 
 public class BlockCrusher extends AbstractMachineBlock {
 
@@ -24,6 +30,8 @@ public class BlockCrusher extends AbstractMachineBlock {
     return res;
   }
 
+  private BallTipProvider btp = new BallTipProvider();
+
   private BlockCrusher() {
     super(ModObject.blockSagMill, TileCrusher.class);
   }
@@ -32,13 +40,12 @@ public class BlockCrusher extends AbstractMachineBlock {
   public void addGrindingBallTooltip(ItemTooltipEvent evt) {
     IGrindingMultiplier gb = CrusherRecipeManager.instance.getGrindballFromStack(evt.itemStack);
     if(gb != null) {
-      List<String> list = evt.toolTip;
-      list.add(EnumChatFormatting.BLUE + "Grinding Ball Properties");
-      list.add(EnumChatFormatting.GRAY + "Ouput x" + gb.getGrindingMultiplier());
-      list.add(EnumChatFormatting.GRAY + "Chance Outputs x" + gb.getChanceMultiplier());
-      list.add(EnumChatFormatting.GRAY + "Power Used x" + gb.getPowerMultiplier());
+      btp.ball = gb;
+      TooltipAddera.instance.addInformation(btp, evt.itemStack, evt.entityPlayer, evt.toolTip, false);
     }
   }
+
+
 
   @Override
   public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
@@ -71,6 +78,41 @@ public class BlockCrusher extends AbstractMachineBlock {
       return "enderio:crusherFrontOn";
     }
     return "enderio:crusherFront";
+  }
+
+  private static class BallTipProvider implements IAdvancedTooltipProvider {
+
+    IGrindingMultiplier ball;
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addCommonEntries(ItemStack itemstack, EntityPlayer entityplayer, List list, boolean flag) {
+
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addBasicEntries(ItemStack itemstack, EntityPlayer entityplayer, List list, boolean flag) {
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addDetailedEntries(ItemStack itemstack, EntityPlayer entityplayer, List list, boolean flag) {
+      if(ball == null) {
+        return;
+      }
+      list.add(EnumChatFormatting.BLUE + Lang.localize("darkGrindingBall.tooltip.detailed.line1"));
+      list.add(EnumChatFormatting.GRAY + Lang.localize("darkGrindingBall.tooltip.detailed.line2") + toPercent(ball.getGrindingMultiplier()));
+      list.add(EnumChatFormatting.GRAY + Lang.localize("darkGrindingBall.tooltip.detailed.line3") + toPercent(ball.getChanceMultiplier()));
+      list.add(EnumChatFormatting.GRAY + Lang.localize("darkGrindingBall.tooltip.detailed.line4") + toPercent(1 - ball.getPowerMultiplier()));
+    }
+
+    private String toPercent(float fl) {
+      fl = fl * 100;
+      int per = Math.round(fl);
+      return " " + per + "%";
+    }
+
   }
 
 }
