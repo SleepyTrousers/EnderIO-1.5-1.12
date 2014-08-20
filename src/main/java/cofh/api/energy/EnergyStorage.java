7ef1b73c26a2731b5ecf58512f04b50bf95f5b6e
tterrag1098 <tterrag1098@gmail.com>
2014-08-19 21:54:39 -0400

    Update vecmath javadoc and CoFHLib API

--- src/main/java/cofh/api/energy/EnergyStorage.java
+++ src/main/java/cofh/api/energy/EnergyStorage.java
@@ -87,7 +87,7 @@ public class EnergyStorage implements IEnergyStorage {
 	}
 
 	/**
-	 * This function is included to allow for server -> client sync. Do not call this externally to the containing Tile Entity, as not all IEnergyHandlers are
+	 * This function is included to allow for server -{@literal >} client sync. Do not call this externally to the containing Tile Entity, as not all IEnergyHandlers are
 	 * guaranteed to have it.
 	 * 
 	 * @param energy
