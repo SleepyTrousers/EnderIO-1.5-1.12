886046df95d25dfcaccf1c4ac553cf5970d36316
Tonius <tonbro96@gmail.com>
2014-08-31 20:25:00 +0200

    Removed obsolete registration of vanilla items in the oredict, and fixed oredict registrations of *regular* glass blocks.

--- src/main/java/crazypants/enderio/material/MaterialRecipes.java
+++ src/main/java/crazypants/enderio/material/MaterialRecipes.java
@@ -37,15 +37,7 @@ public class MaterialRecipes {
     OreDictionary.registerOre("nuggetPulsatingIron", new ItemStack(EnderIO.itemMaterial, 1, Material.PHASED_IRON_NUGGET.ordinal()));
     OreDictionary.registerOre("nuggetVibrantAlloy", new ItemStack(EnderIO.itemMaterial, 1, Material.VIBRANT_NUGGET.ordinal()));
 
-    OreDictionary.registerOre("glass", Blocks.glass);
-    OreDictionary.registerOre("stickWood", Items.stick);
-    OreDictionary.registerOre("woodStick", Items.stick);
-    OreDictionary.registerOre("sand", Blocks.sand);
-    OreDictionary.registerOre("ingotIron", Items.iron_ingot);
-    OreDictionary.registerOre("ingotGold", Items.gold_ingot);
-
     ItemStack pureGlass = new ItemStack(EnderIO.blockFusedQuartz, 1, BlockFusedQuartz.Type.GLASS.ordinal());
-    OreDictionary.registerOre("glass", pureGlass);
     OreDictionary.registerOre("blockGlass", pureGlass);
     OreDictionary.registerOre("blockGlassHardened", new ItemStack(EnderIO.blockFusedQuartz, 1, BlockFusedQuartz.Type.FUSED_QUARTZ.ordinal()));
     OreDictionary.registerOre("blockGlassHardened", new ItemStack(EnderIO.blockFusedQuartz, 1, BlockFusedQuartz.Type.ENLIGHTENED_FUSED_QUARTZ.ordinal()));
