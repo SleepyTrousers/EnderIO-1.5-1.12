b1729961e06382b5fcaee6e78fe22d17119c881d
ChatFawkes <ChatFawkes@users.noreply.github.com>
2014-09-15 01:49:00 -0400

    Register Enlightened Clear Glass as "glass" too

--- src/main/java/crazypants/enderio/material/MaterialRecipes.java
+++ src/main/java/crazypants/enderio/material/MaterialRecipes.java
@@ -47,6 +47,7 @@ public class MaterialRecipes {
 
     ItemStack pureGlass = new ItemStack(EnderIO.blockFusedQuartz, 1, BlockFusedQuartz.Type.GLASS.ordinal());
     OreDictionary.registerOre("glass", pureGlass);
+    OreDictionary.registerOre("glass", new ItemStack(EnderIO.blockFusedQuartz, 1, BlockFusedQuartz.Type.ENLIGHTENED_GLASS.ordinal()));
     OreDictionary.registerOre("blockGlass", pureGlass);
     OreDictionary.registerOre("blockGlass", new ItemStack(EnderIO.blockFusedQuartz, 1, BlockFusedQuartz.Type.ENLIGHTENED_GLASS.ordinal()));
     OreDictionary.registerOre("blockGlassHardened", new ItemStack(EnderIO.blockFusedQuartz, 1, BlockFusedQuartz.Type.FUSED_QUARTZ.ordinal()));
