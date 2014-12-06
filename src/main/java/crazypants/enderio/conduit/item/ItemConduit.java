52fe7c9e10168da4c3b80a850a18e9cff4759868
CrazyPants <crazypants.mc@gmail.com>
2014-12-06 21:03:28 +0800

    #1348 Item conduits not connecting to tesseracts in receive only mode

--- src/main/java/crazypants/enderio/conduit/item/ItemConduit.java
+++ src/main/java/crazypants/enderio/conduit/item/ItemConduit.java
@@ -547,7 +547,9 @@ public class ItemConduit extends AbstractConduit implements IItemConduit {
       //TODO: The MFR thing is a horrible hack. Blocks like the harvester make no slots accessible but will push into a connected
       //conduit. I could just return true for sided inventories but this will lead to confusing connections in some cases.
       //Therefore, bad hack for now.
-      return (slots != null && slots.length != 0) || inv.getClass().getName().startsWith("powercrystals.minefactoryreloaded");
+
+      return (slots != null && slots.length != 0) || inv.getClass().getName().startsWith("powercrystals.minefactoryreloaded") ||
+          inv.getClass().getName().endsWith("TileTesseract");
     } else {
       return inv != null;
     }
