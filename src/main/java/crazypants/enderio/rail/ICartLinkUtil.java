6f459cda536beaafa76338ce774de48a1d8cef0a
CrazyPants <crazypants.mc@gmail.com>
2014-12-12 00:31:00 +0800

    #1285 Ender Rail not adjusting the direction of RC locomotives when required

--- src/main/java/crazypants/enderio/rail/ICartLinkUtil.java
+++ src/main/java/crazypants/enderio/rail/ICartLinkUtil.java
@@ -5,6 +5,7 @@ import java.util.List;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityMinecart;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
 
 public interface ICartLinkUtil {
 
@@ -17,5 +18,7 @@ public interface ICartLinkUtil {
   void recreateLinks(EntityMinecart cart);
 
   void breakLinks(World worldObj, Entity entity);
+
+  void setCartDirection(EntityMinecart cart, ForgeDirection dir);
 
 }
