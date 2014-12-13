170b0287f798f49ff584716ba57be874414bfc3f
CrazyPants <crazypants.mc@gmail.com>
2014-12-13 12:56:45 +0800

    Reverted ME conduit fix until more testing is done

--- src/main/java/crazypants/enderio/conduit/me/MEConduit.java
+++ src/main/java/crazypants/enderio/conduit/me/MEConduit.java
@@ -3,7 +3,6 @@ package crazypants.enderio.conduit.me;
 import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.List;
-import java.util.Set;
 
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
@@ -331,13 +330,13 @@ public class MEConduit extends AbstractConduit implements IMEConduit {
 
   @Override
   public EnumSet<ForgeDirection> getConnections() {
-    Set<ForgeDirection> cons = getConduitConnections();
-    cons.addAll(getExternalConnections());
-    if(cons.isEmpty()) {
-      return EnumSet.noneOf(ForgeDirection.class);
-    }
-    return EnumSet.copyOf(cons);
-    //return validConnections;
+    //    Set<ForgeDirection> cons = getConduitConnections();
+    //    cons.addAll(getExternalConnections());
+    //    if(cons.isEmpty()) {
+    //      return EnumSet.noneOf(ForgeDirection.class);
+    //    }
+    //    return EnumSet.copyOf(cons);
+    return validConnections;
   }
   
   @Override
