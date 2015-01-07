2fccdaa68330ea64251af73dab8a8b97990b69bf
Matthias Mann <MatthiasMann@gmx.de>
2015-01-07 16:34:27 +0100

    right clicking while sneaking with an existing item filter on an inventory will add the content to the snapshot

--- src/main/java/crazypants/enderio/conduit/item/filter/ExistingItemFilter.java
+++ src/main/java/crazypants/enderio/conduit/item/filter/ExistingItemFilter.java
@@ -14,6 +14,7 @@ import net.minecraft.nbt.NBTTagList;
 import net.minecraftforge.oredict.OreDictionary;
 import crazypants.enderio.conduit.item.NetworkedInventory;
 import crazypants.enderio.network.NetworkUtil;
+import net.minecraft.inventory.IInventory;
 
 public class ExistingItemFilter implements IItemFilter {
 
@@ -112,6 +113,22 @@ public class ExistingItemFilter implements IItemFilter {
         snapshot.add(stack);
       }
     }
+  }
+
+  public boolean mergeSnapshot(IInventory inventory) {
+    if(snapshot == null) {
+      snapshot = new ArrayList<ItemStack>();
+    }
+    int size = inventory.getSizeInventory();
+    boolean added = false;
+    for (int i = 0; i < size; i++) {
+      ItemStack stack = inventory.getStackInSlot(i);
+      if(stack != null && !isStackInSnapshot(stack)) {
+        snapshot.add(stack);
+        added = true;
+      }
+    }
+    return added;
   }
 
   public List<ItemStack> getSnapshot() {
