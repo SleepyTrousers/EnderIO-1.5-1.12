0bb7ef4f5c3b5861b7a2ac2841a6ec65e78599ae
Matthias Mann <MatthiasMann@gmx.de>
2015-01-10 15:04:21 +0100

    added chargeable item filter - part of #1330

---
+++ src/main/java/crazypants/enderio/conduit/item/filter/ItemPowerItemFilter.java
@@ -0,0 +1,65 @@
+package crazypants.enderio.conduit.item.filter;
+
+import java.util.List;
+
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import crazypants.enderio.EnderIOTab;
+import crazypants.enderio.ModObject;
+import crazypants.enderio.conduit.item.FilterRegister;
+import crazypants.enderio.gui.TooltipAddera;
+import crazypants.util.Lang;
+
+/**
+ *
+ * @author matthias
+ */
+public class ItemPowerItemFilter extends Item implements IItemFilterUpgrade {
+
+  public static ItemPowerItemFilter create() {
+    ItemPowerItemFilter result = new ItemPowerItemFilter();
+    result.init();
+    return result;
+  }
+
+  protected ItemPowerItemFilter() {
+    setCreativeTab(EnderIOTab.tabEnderIO);
+    setUnlocalizedName(ModObject.itemPowerItemFilter.unlocalisedName);
+    setHasSubtypes(true);
+    setMaxDamage(0);
+    setMaxStackSize(64);
+  }
+
+  protected void init() {
+    GameRegistry.registerItem(this, ModObject.itemPowerItemFilter.unlocalisedName);
+  }
+
+  @Override
+  public IItemFilter createFilterFromStack(ItemStack stack) {
+    IItemFilter filter = new PowerItemFilter();
+    if(stack.stackTagCompound != null && stack.stackTagCompound.hasKey("filter")) {
+      filter.readFromNBT(stack.stackTagCompound.getCompoundTag("filter"));
+    }
+    return filter;
+  }
+
+  @Override
+  public void registerIcons(IIconRegister IIconRegister) {
+    itemIcon = IIconRegister.registerIcon("enderio:filterUpgradePower");
+  }
+
+  @Override
+  public String getUnlocalizedName(ItemStack stack) {
+    return getUnlocalizedName();
+  }
+
+}
