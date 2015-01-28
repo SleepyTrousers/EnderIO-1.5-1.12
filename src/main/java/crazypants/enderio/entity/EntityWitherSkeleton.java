c9946e21ecfc78a6b270b6b0f9a0f0f4d2db2367
tterrag1098 <tterrag1098@gmail.com>
2015-01-27 22:57:32 -0500

    Fix wither skeletons not spawning and fix their equipment/rendering

--- src/main/java/crazypants/enderio/entity/EntityWitherSkeleton.java
+++ src/main/java/crazypants/enderio/entity/EntityWitherSkeleton.java
@@ -11,45 +11,47 @@ import net.minecraft.world.World;
 
 import java.util.Calendar;
 
-public class EntityWitherSkeleton extends EntitySkeleton
-{
-    public EntityWitherSkeleton(World world)
-    {
-        super(world);
+public class EntityWitherSkeleton extends EntitySkeleton {
+  public EntityWitherSkeleton(World world) {
+    super(world);
+  }
+
+  public EntityWitherSkeleton(EntitySkeleton entity) {
+    this(entity.worldObj);
+    this.copyLocationAndAnglesFrom(entity);
+    for (int i = 0; i < entity.getLastActiveItems().length; i++) {
+      this.setCurrentItemOrArmor(i, entity.getEquipmentInSlot(i));
+    }
+  }
+
+  @Override
+  public void setFire(int p_70015_1_) {
+  }
+
+  @Override
+  public IEntityLivingData onSpawnWithEgg(IEntityLivingData entityData) {
+    this.getEntityAttribute(SharedMonsterAttributes.followRange)
+        .applyModifier(new AttributeModifier("Random spawn bonus", this.rand.nextGaussian() * 0.05D, 1));
+    this.setSkeletonType(1);
+    this.setCurrentItemOrArmor(0, new ItemStack(Items.stone_sword));
+    this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(4.0D);
+    this.setCombatTask();
+    this.setCanPickUpLoot(this.rand.nextFloat() < 0.55F * this.worldObj.func_147462_b(this.posX, this.posY, this.posZ));
+
+    if(this.getEquipmentInSlot(4) == null) {
+      Calendar calendar = this.worldObj.getCurrentDate();
+
+      if(calendar.get(2) + 1 == 10 && calendar.get(5) == 31 && this.rand.nextFloat() < 0.25F) {
+        this.setCurrentItemOrArmor(4, new ItemStack(this.rand.nextFloat() < 0.1F ? Blocks.lit_pumpkin : Blocks.pumpkin));
+        this.equipmentDropChances[4] = 0.0F;
+      }
     }
 
-    public EntityWitherSkeleton(EntitySkeleton entity)
-    {
-        this(entity.worldObj);
-        this.copyLocationAndAnglesFrom(entity);
-    }
+    return entityData;
+  }
 
-    @Override
-    public void setFire(int p_70015_1_)
-    {
-    }
-
-    @Override
-    public IEntityLivingData onSpawnWithEgg(IEntityLivingData entityData)
-    {
-        this.getEntityAttribute(SharedMonsterAttributes.followRange).applyModifier(new AttributeModifier("Random spawn bonus", this.rand.nextGaussian() * 0.05D, 1));
-        this.setSkeletonType(1);
-        this.setCurrentItemOrArmor(0, new ItemStack(Items.stone_sword));
-        this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(4.0D);
-        this.setCombatTask();
-        this.setCanPickUpLoot(this.rand.nextFloat() < 0.55F * this.worldObj.func_147462_b(this.posX, this.posY, this.posZ));
-
-        if (this.getEquipmentInSlot(4) == null)
-        {
-            Calendar calendar = this.worldObj.getCurrentDate();
-
-            if (calendar.get(2) + 1 == 10 && calendar.get(5) == 31 && this.rand.nextFloat() < 0.25F)
-            {
-                this.setCurrentItemOrArmor(4, new ItemStack(this.rand.nextFloat() < 0.1F ? Blocks.lit_pumpkin : Blocks.pumpkin));
-                this.equipmentDropChances[4] = 0.0F;
-            }
-        }
-
-        return entityData;
-    }
+  @Override
+  public int getSkeletonType() {
+    return 1;
+  }
 }
