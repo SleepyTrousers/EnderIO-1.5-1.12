681407a555a133e16bb86eaab27b7b9ab20ac27b
tterrag1098 <tterrag1098@gmail.com>
2015-03-22 17:28:30 -0400

    Rewrite Fertilizer to be an enum

--- src/main/java/crazypants/enderio/machine/farm/Fertilizer.java
+++ src/main/java/crazypants/enderio/machine/farm/Fertilizer.java
@@ -1,10 +1,5 @@
 package crazypants.enderio.machine.farm;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import cpw.mods.fml.common.registry.GameRegistry;
-import crazypants.util.BlockCoord;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
@@ -13,12 +8,88 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
+import net.minecraftforge.oredict.OreDictionary;
+import cpw.mods.fml.common.registry.GameRegistry;
+import crazypants.util.BlockCoord;
 
-abstract public class Fertilizer {
+public enum Fertilizer {
 
-  private static boolean initialized = false;
-  private static final Fertilizer NONE = new None();
-  protected static List<Fertilizer> instances = new ArrayList<Fertilizer>();
+  /**
+   * Not a fertilizer. Using this handler class any item can be "used" as a
+   * fertilizer. Meaning, fertilizing will always fail.
+   */
+  NONE((ItemStack) null) {
+
+    @Override
+    public boolean apply(ItemStack stack, EntityPlayer player, World world, BlockCoord bc) {
+      return false;
+    }
+  },
+
+  BONEMEAL(new ItemStack(Items.dye, 1, 15)) {
+
+    public boolean apply(ItemStack stack, EntityPlayer player, World world, BlockCoord bc) {
+      return stack.getItem().onItemUse(stack, player, world, bc.x, bc.y, bc.z, 1, 0.5f, 0.5f, 0.5f);
+    }
+  },
+
+  FORESTRY_FERTILIZER_COMPOUND(GameRegistry.findItem("Forestry", "fertilizerCompound")) {
+
+    @Override
+    public boolean apply(ItemStack stack, EntityPlayer player, World world, BlockCoord bc) {
+      return BONEMEAL.apply(stack, player, world, bc);
+    }
+  },
+
+  BOTANIA_FLORAL_FERTILIZER(GameRegistry.findItem("Botania", "fertilizer")) {
+
+    public boolean apply(ItemStack stack, EntityPlayer player, World world, BlockCoord bc) {
+      BlockCoord below = bc.getLocation(ForgeDirection.DOWN);
+      Block belowBlock = below.getBlock(world);
+      if(belowBlock == Blocks.dirt || belowBlock == Blocks.grass) {
+        return stack.getItem().onItemUse(stack, player, world, below.x, below.y, below.z, 1, 0.5f, 0.5f, 0.5f);
+      }
+      return false;
+    }
+
+    public boolean applyOnAir() {
+      return true;
+    }
+
+    public boolean applyOnPlant() {
+      return false;
+    }
+  },
+
+  METALLURGY_FERTILIZER(GameRegistry.findItem("Metallurgy", "fertilizer")) {
+
+    @Override
+    public boolean apply(ItemStack stack, EntityPlayer player, World world, BlockCoord bc) {
+      return BONEMEAL.apply(stack, player, world, bc);
+    }
+  },
+
+  GARDEN_CORE_COMPOST(GameRegistry.findItem("GardenCore", "compost_pile")) {
+
+    @Override
+    public boolean apply(ItemStack stack, EntityPlayer player, World world, BlockCoord bc) {
+      return BONEMEAL.apply(stack, player, world, bc);
+    }
+  };
+
+  private ItemStack stack;
+
+  private Fertilizer(Item item) {
+    this(new ItemStack(item));
+  }
+
+  private Fertilizer(Block block) {
+    this(new ItemStack(block));
+  }
+
+  private Fertilizer(ItemStack stack) {
+    this.stack = stack;
+  }
 
   /**
    * Returns the singleton instance for the fertilizer that was given as
@@ -27,23 +98,14 @@ abstract public class Fertilizer {
    * 
    */
   public static Fertilizer getInstance(ItemStack stack) {
-    if (instances.isEmpty()) {
-      new Bonemeal().addInstance();
-      new ForestryFertilizerCompound().addInstance();
-      new BotaniaFloralFertilizer().addInstance();
-      new MetallurgyFertilizer().addInstance();
-      new GardenCoreCompost_pile().addInstance();
-    }
-    for (Fertilizer fertilizer : instances) {
-      if (fertilizer.matches(stack)) {
+    for (Fertilizer fertilizer : values()) {
+      if(fertilizer.matches(stack)) {
         return fertilizer;
       }
     }
     return NONE;
   }
 
-  protected abstract void addInstance();
-  
   /**
    * Returns true if the given item can be used as fertilizer.
    */
@@ -51,7 +113,9 @@ abstract public class Fertilizer {
     return getInstance(stack) != NONE;
   }
 
-  protected abstract boolean matches(ItemStack stack);
+  protected boolean matches(ItemStack stack) {
+    return OreDictionary.itemMatches(this.stack, stack, false);
+  }
 
   /**
    * Tries to apply the given item on the given block using the type-specific
@@ -69,138 +133,11 @@ abstract public class Fertilizer {
    */
   public abstract boolean apply(ItemStack stack, EntityPlayer player, World world, BlockCoord bc);
 
-  public boolean applyOnAir() { return false; }
-  public boolean applyOnPlant() { return true; }
-
-  /**
-   * Not a fertilizer. Using this handler class any item can be "used" as a
-   * fertilizer. Meaning, fertilizing will always fail.
-   *
-   */
-  private static class None extends Fertilizer {
-
-    @Override
-    protected boolean matches(ItemStack stack) {
-      return true;
-    }
-
-    @Override
-    public boolean apply(ItemStack stack, EntityPlayer player, World world, BlockCoord bc) {
-      return false;
-    }
-
-    @Override
-    protected void addInstance() {
-    }
-
+  public boolean applyOnAir() {
+    return false;
   }
 
-  private static class Bonemeal extends Fertilizer {
-
-    @Override
-    protected boolean matches(ItemStack stack) {
-      return stack.getItem() == Items.dye && stack.getItemDamage() == 15;
-    }
-
-    public boolean apply(ItemStack stack, EntityPlayer player, World world, BlockCoord bc) {
-      return stack.getItem().onItemUse(stack, player, world, bc.x, bc.y, bc.z, 1, 0.5f, 0.5f, 0.5f);
-    }
-
-    @Override
-    protected void addInstance() {
-      instances.add(this);
-    }
-
+  public boolean applyOnPlant() {
+    return true;
   }
-
-  private static class ForestryFertilizerCompound extends Bonemeal {
-
-    private Item forestryFertilizerCompound;
-
-    @Override
-    protected void addInstance() {
-      forestryFertilizerCompound = GameRegistry.findItem("Forestry", "fertilizerCompound");
-      if (forestryFertilizerCompound!= null) {
-        instances.add(this);
-      }
-    }
-
-    @Override
-    protected boolean matches(ItemStack stack) {
-      return forestryFertilizerCompound != null && stack.getItem() == forestryFertilizerCompound;
-    }
-
-  }
-
-  private static class BotaniaFloralFertilizer extends Fertilizer {
-
-    private Item floralFertilizer;
-
-    @Override
-    protected void addInstance() {
-      floralFertilizer = GameRegistry.findItem("Botania", "fertilizer");
-      if (floralFertilizer!= null) {
-        instances.add(this);
-      }
-    }
-
-    @Override
-    protected boolean matches(ItemStack stack) {
-      return floralFertilizer != null && stack.getItem() == floralFertilizer;
-    }
-
-    public boolean apply(ItemStack stack, EntityPlayer player, World world, BlockCoord bc) {
-      BlockCoord below = bc.getLocation(ForgeDirection.DOWN);
-      Block belowBlock = below.getBlock(world);
-      if (belowBlock == Blocks.dirt || belowBlock == Blocks.grass) {
-        return stack.getItem().onItemUse(stack, player, world, below.x, below.y, below.z, 1, 0.5f, 0.5f, 0.5f);
-      }
-      return false;
-    }
-
-    public boolean applyOnAir() { return true; }
-    public boolean applyOnPlant() { return false; }
-
-
-  }
-
-  private static class MetallurgyFertilizer extends Bonemeal {
-
-    private Item metallurgyFertilizer;
-
-    @Override
-    protected void addInstance() {
-      metallurgyFertilizer = GameRegistry.findItem("Metallurgy", "fertilizer");
-      if (metallurgyFertilizer!= null) {
-        instances.add(this);
-      }
-    }
-
-    @Override
-    protected boolean matches(ItemStack stack) {
-      return metallurgyFertilizer != null && stack.getItem() == metallurgyFertilizer;
-    }
-
-  }
-
-  private static class GardenCoreCompost_pile extends Bonemeal {
-
-    private Item compost_pile;
-
-    @Override
-    protected void addInstance() {
-      compost_pile = GameRegistry.findItem("GardenCore", "compost_pile");
-      if (compost_pile!= null) {
-        instances.add(this);
-      }
-    }
-
-    @Override
-    protected boolean matches(ItemStack stack) {
-      return compost_pile != null && stack.getItem() == compost_pile;
-    }
-
-  }
-
-
 }
