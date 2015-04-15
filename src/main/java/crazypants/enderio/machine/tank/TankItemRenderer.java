d78057108b92c8a15f6cfd73a1e52e1ff367e323
HenryLoenwind <henry@loenwind.info>
2015-04-15 22:10:06 +0200

    New: Tanks can be used in item form

--- src/main/java/crazypants/enderio/machine/tank/TankItemRenderer.java
+++ src/main/java/crazypants/enderio/machine/tank/TankItemRenderer.java
@@ -7,10 +7,12 @@ import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraftforge.client.IItemRenderer;
+import net.minecraftforge.fluids.FluidTank;
 
 import org.lwjgl.opengl.GL11;
 
 import crazypants.enderio.EnderIO;
+import crazypants.enderio.tool.SmartTank;
 import crazypants.render.BoundingBox;
 import crazypants.render.CubeRenderer;
 import crazypants.render.RenderUtil;
@@ -38,13 +40,11 @@ public class TankItemRenderer implements IItemRenderer {
 
   public void renderToInventory(ItemStack item, RenderBlocks renderBlocks) {
 
-    boolean hasFluid = false;
-    if(item.stackTagCompound != null) {
-      TileTank tt = new TileTank();
-      tt.readCommon(item.stackTagCompound);
-
-      TankFluidRenderer.renderTankFluid(tt.tank, 0, -0.1f, 0);
-      hasFluid = true;
+    if (item.getItem() instanceof BlockItemTank) {
+      SmartTank tank = TileTank.readTankFromItem(item);
+      if (tank != null) {
+        TankFluidRenderer.renderTankFluid(tank, 0, -0.1f, 0);
+      }
     }
 
     GL11.glEnable(GL11.GL_ALPHA_TEST);
