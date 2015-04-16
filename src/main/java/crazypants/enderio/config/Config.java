93ba2428a21929f980de05e65691deaa97d9d23c
HenryLoenwind <henry@loenwind.info>
2015-04-16 20:40:33 +0200

    New: Config value to disable upticking
    * Config value to have machines reject an update rate that is higher
    than the global tick rate.
    * Default is unchanged (allow).

--- src/main/java/crazypants/enderio/config/Config.java
+++ src/main/java/crazypants/enderio/config/Config.java
@@ -99,6 +99,7 @@ public final class Config {
   public static File configDirectory;
 
   public static boolean useHardRecipes = false;
+  public static boolean allowExternalTickSpeedup = true;
 
   public static boolean useSteelInChassi = false;
 
@@ -566,6 +567,11 @@ public final class Config {
         "The number of conduits crafted per recipe.").getInt(numConduitsPerRecipe);
     transceiverUseEasyRecipe= config.get(sectionRecipe.name, "transceiverUseEasyRecipe", transceiverUseEasyRecipe, "When enabled the dim trans. will use a cheaper recipe")
         .getBoolean(useHardRecipes);
+
+    allowExternalTickSpeedup = config.get(sectionMisc.name, "allowExternalTickSpeedup", allowExternalTickSpeedup,
+        "Allows machines to run faster if another mod speeds up the tickrate. Running at higher tickrates is "
+            + "unsupported. Disable this if you run into any kind of problem.")
+        .getBoolean(allowExternalTickSpeedup);
 
     enchanterBaseLevelCost = config.get(sectionRecipe.name, "enchanterBaseLevelCost", enchanterBaseLevelCost,
         "Base level cost added to all recipes in the enchanter.").getInt(enchanterBaseLevelCost);
