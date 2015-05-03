354e6098a8e08ab3e930f0cc764395ac3688a7fe
tterrag1098 <tterrag1098@gmail.com>
2015-05-03 17:38:48 -0400

    Add required mekanism version to dependencies

--- src/main/java/crazypants/enderio/EnderIO.java
+++ src/main/java/crazypants/enderio/EnderIO.java
@@ -153,7 +153,7 @@ import static crazypants.enderio.EnderIO.MODID;
 import static crazypants.enderio.EnderIO.MOD_NAME;
 import static crazypants.enderio.EnderIO.VERSION;
 
-@Mod(modid = MODID, name = MOD_NAME, version = VERSION, dependencies = "required-after:Forge@10.13.0.1150,);after:MineFactoryReloaded;after:appliedenergistics2@[rv2-beta-8,)", guiFactory = "crazypants.enderio.config.ConfigFactoryEIO")
+@Mod(modid = MODID, name = MOD_NAME, version = VERSION, dependencies = "required-after:Forge@10.13.0.1150,);after:MineFactoryReloaded;after:appliedenergistics2@[rv2-beta-8,);before:Mekanism@[7.0, 8.0)", guiFactory = "crazypants.enderio.config.ConfigFactoryEIO")
 public class EnderIO {
 
   public static final String MODID = "EnderIO";
