5e3d22ce2f3dd6758bfe70b94ea026bcfb4330f8
tterrag1098 <tterrag1098@gmail.com>
2015-05-15 01:42:32 -0400

    Skip reading NBT data of ME/Gas conduits if they are disabled

--- src/main/java/crazypants/enderio/conduit/ConduitUtil.java
+++ src/main/java/crazypants/enderio/conduit/ConduitUtil.java
@@ -28,6 +28,7 @@ import crazypants.enderio.api.tool.IConduitControl;
 import crazypants.enderio.api.tool.IHideFacades;
 import crazypants.enderio.conduit.IConduitBundle.FacadeRenderState;
 import crazypants.enderio.conduit.gas.GasConduitNetwork;
+import crazypants.enderio.conduit.gas.GasUtil;
 import crazypants.enderio.conduit.gas.IGasConduit;
 import crazypants.enderio.conduit.item.IItemConduit;
 import crazypants.enderio.conduit.item.ItemConduitNetwork;
@@ -39,6 +40,7 @@ import crazypants.enderio.conduit.liquid.ILiquidConduit;
 import crazypants.enderio.conduit.liquid.LiquidConduitNetwork;
 import crazypants.enderio.conduit.me.IMEConduit;
 import crazypants.enderio.conduit.me.MEConduitNetwork;
+import crazypants.enderio.conduit.me.MEUtil;
 import crazypants.enderio.conduit.power.IPowerConduit;
 import crazypants.enderio.conduit.power.PowerConduitNetwork;
 import crazypants.enderio.conduit.redstone.IInsulatedRedstoneConduit;
@@ -311,10 +313,10 @@ public class ConduitUtil {
   }
 
   public static void writeToNBT(IConduit conduit, NBTTagCompound conduitRoot) {
-    if(conduit == null) {
+    if (conduit == null) {
       return;
     }
-
+    
     NBTTagCompound conduitBody = new NBTTagCompound();
     conduit.writeToNBT(conduitBody);
 
@@ -328,7 +330,10 @@ public class ConduitUtil {
     if(typeName == null || conduitBody == null) {
       return null;
     }
-    if(nbtVersion == 0 && "crazypants.enderio.conduit.liquid.LiquidConduit".equals(typeName)) {
+    if ((typeName.contains("conduit.me") && MEUtil.isMEEnabled()) || typeName.contains("conduit.gas") && GasUtil.isGasConduitEnabled()) {
+      return null;
+    }
+    if (nbtVersion == 0 && "crazypants.enderio.conduit.liquid.LiquidConduit".equals(typeName)) {
       Log.debug("ConduitUtil.readConduitFromNBT: Converted pre 0.7.3 fluid conduit to advanced fluid conduit.");
       typeName = "crazypants.enderio.conduit.liquid.AdvancedLiquidConduit";
     }
