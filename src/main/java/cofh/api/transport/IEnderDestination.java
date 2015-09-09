285cb7338cc43cb57335ee0305cf3126f65e884d
tterrag1098 <tterrag1098@gmail.com>
2015-09-08 20:36:42 -0400

    Update CoFH API to 1.0.13

--- src/main/java/cofh/api/transport/IEnderDestination.java
+++ src/main/java/cofh/api/transport/IEnderDestination.java
@@ -5,8 +5,11 @@ public interface IEnderDestination extends IEnderAttuned {
 	public boolean isNotValid();
 
 	public int x();
+
 	public int y();
+
 	public int z();
+
 	public int dimension();
 
 	public int getDestination();
@@ -14,4 +17,5 @@ public interface IEnderDestination extends IEnderAttuned {
 	public boolean setDestination(int frequency);
 
 	public boolean clearDestination();
+
 }
