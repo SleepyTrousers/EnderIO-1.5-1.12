cf8a55d3d3bc7c98537cf05a8e7bef730dd3004b
HenryLoenwind <henry@loenwind.info>
2016-01-21 22:46:54 +0100

    Try to prevent render errors from non-solid painted blocks

--- src/main/java/crazypants/enderio/machine/painter/PaintedBlockRenderer.java
+++ src/main/java/crazypants/enderio/machine/painter/PaintedBlockRenderer.java
@@ -6,9 +6,11 @@ import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.common.util.ForgeDirection;
 
 import com.enderio.core.client.render.CubeRenderer;
 import com.enderio.core.client.render.IconUtil;
+import com.enderio.core.common.util.BlockCoord;
 
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 
@@ -50,11 +52,21 @@ public class PaintedBlockRenderer implements ISimpleBlockRenderingHandler {
     IBlockAccess origBa = rb.blockAccess;
     try {      
       rb.blockAccess = new PaintedBlockAccessWrapper(origBa);
-      if(srcBlk == block) {
-        rb.renderStandardBlock(srcBlk, x, y, z);
+      if (block.isOpaqueCube() && nonSolidPaintedBlockAround(new BlockCoord(x, y, z), origBa, rb.blockAccess)) {
+        if (srcBlk == block) {
+          rb.setRenderAllFaces(true);
+          rb.renderStandardBlock(srcBlk, x, y, z);
+          rb.setRenderAllFaces(false);
+        } else {
+          rb.renderBlockAllFaces(srcBlk, x, y, z);
+        }
       } else {
-        rb.renderBlockByRenderType(srcBlk, x, y, z);
-      }      
+        if (srcBlk == block) {
+          rb.renderStandardBlock(srcBlk, x, y, z);
+        } else {
+          rb.renderBlockByRenderType(srcBlk, x, y, z);
+        }
+      }
     } catch (Exception e) {
       //just in case the paint source wont render safely in this way
       rb.setOverrideBlockTexture(IconUtil.errorTexture);
@@ -65,6 +77,24 @@ public class PaintedBlockRenderer implements ISimpleBlockRenderingHandler {
     }
 
     return true;
+  }
+
+  /*
+   * If a non-solid block is painted with a solid block, the renderer would
+   * think it didn't need to render faces of adjacent blocks that touch it
+   * because it only sees the paint.
+   * 
+   * This detects this case, so the renderer can be told to just render all
+   * faces, even if it thinks it could save some quads.
+   */
+  private boolean nonSolidPaintedBlockAround(BlockCoord bc, IBlockAccess reality, IBlockAccess fake) {
+    for (ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS) {
+      BlockCoord toTest = bc.getLocation(dir);
+      if (toTest.y >= 0 && toTest.y <= 255 && toTest.getBlock(reality).isOpaqueCube() != toTest.getBlock(fake).isOpaqueCube()) {
+        return true;
+      }
+    }
+    return false;
   }
 
   @Override
