137b9cb7d786710d5e62bfbec2bd85d9a51779f0
CrazyPants <crazypants.mc@gmail.com>
2016-03-10 16:36:15 +0800

    Added guard for (very odd) NPE

--- src/main/java/crazypants/enderio/item/darksteel/DarkSteelController.java
+++ src/main/java/crazypants/enderio/item/darksteel/DarkSteelController.java
@@ -60,17 +60,14 @@ public class DarkSteelController {
   private final AttributeModifier[] walkModifiers = new AttributeModifier[] {
       new AttributeModifier(new UUID(12879874982l, 320981923), "generic.movementSpeed", SpeedUpgrade.WALK_MULTIPLIERS[0], 1),
       new AttributeModifier(new UUID(12879874982l, 320981923), "generic.movementSpeed", SpeedUpgrade.WALK_MULTIPLIERS[1], 1),
-      new AttributeModifier(new UUID(12879874982l, 320981923), "generic.movementSpeed", SpeedUpgrade.WALK_MULTIPLIERS[2], 1),
-  };
+      new AttributeModifier(new UUID(12879874982l, 320981923), "generic.movementSpeed", SpeedUpgrade.WALK_MULTIPLIERS[2], 1), };
 
   private final AttributeModifier[] sprintModifiers = new AttributeModifier[] {
       new AttributeModifier(new UUID(12879874982l, 320981923), "generic.movementSpeed", SpeedUpgrade.SPRINT_MULTIPLIERS[0], 1),
       new AttributeModifier(new UUID(12879874982l, 320981923), "generic.movementSpeed", SpeedUpgrade.SPRINT_MULTIPLIERS[1], 1),
-      new AttributeModifier(new UUID  (12879874982l, 320981923), "generic.movementSpeed", SpeedUpgrade.SPRINT_MULTIPLIERS[2], 1),
-  };
+      new AttributeModifier(new UUID(12879874982l, 320981923), "generic.movementSpeed", SpeedUpgrade.SPRINT_MULTIPLIERS[2], 1), };
 
-  private final AttributeModifier swordDamageModifierPowered = new AttributeModifier(new UUID(63242325, 320981923), "Weapon modifier",
-      2, 0);
+  private final AttributeModifier swordDamageModifierPowered = new AttributeModifier(new UUID(63242325, 320981923), "Weapon modifier", 2, 0);
 
   private boolean wasJumping;
   private int jumpCount;
@@ -94,9 +91,9 @@ public class DarkSteelController {
     GameProfile gameProfile = player.getGameProfile();
     UUID id = gameProfile == null ? null : gameProfile.getId();
     active = id == null ? null : allActive.get(id);
-    if(active == null) {
+    if (active == null) {
       active = DEFAULT_ACTIVE.clone();
-      if(id != null) {
+      if (id != null) {
         allActive.put(id, active);
       }
     }
@@ -109,7 +106,7 @@ public class DarkSteelController {
 
   public void setActive(EntityPlayer player, Type type, boolean isActive) {
     EnumSet<Type> set = getActiveSet(player);
-    if(isActive) {
+    if (isActive) {
       set.add(type);
     } else {
       set.remove(type);
@@ -119,39 +116,39 @@ public class DarkSteelController {
   public boolean isGlideActive(EntityPlayer player) {
     return isActive(player, Type.GLIDE);
   }
-  
+
   public boolean isSpeedActive(EntityPlayer player) {
     return isActive(player, Type.SPEED);
   }
-  
+
   public boolean isStepAssistActive(EntityPlayer player) {
     return isActive(player, Type.STEP_ASSIST);
   }
-  
+
   public boolean isJumpActive(EntityPlayer player) {
     return isActive(player, Type.JUMP);
   }
-  
+
   @SubscribeEvent
   public void onPlayerTick(TickEvent.PlayerTickEvent event) {
     EntityPlayer player = event.player;
 
-    if(event.phase == Phase.START) {
-      //boots
+    if (event.phase == Phase.START) {
+      // boots
       updateStepHeightAndFallDistance(player);
 
-      //leggings
+      // leggings
       updateSpeed(player);
 
-      //sword
+      // sword
       updateSword(player);
 
       updateGlide(player);
 
       updateSwim(player);
-      
+
       updateSolar(player);
-      
+
     }
 
   }
@@ -161,32 +158,34 @@ public class DarkSteelController {
     if (player.worldObj.isRemote) {
       return;
     }
-    
+
     ItemStack helm = player.getEquipmentInSlot(4);
     SolarUpgrade upgrade = SolarUpgrade.loadFromItem(helm);
-    if(upgrade == null) {
+    if (upgrade == null) {
       return;
     }
-    
-    int RFperSecond = Math.round(upgrade.getRFPerSec() * TileEntitySolarPanel.calculateLightRatio(player.worldObj, new BlockPos(MathHelper.floor_double(player.posX), MathHelper.floor_double(player.posY + 1), MathHelper.floor_double(player.posZ))));
-    
+
+    int RFperSecond = Math.round(upgrade.getRFPerSec() * TileEntitySolarPanel.calculateLightRatio(player.worldObj,
+        new BlockPos(MathHelper.floor_double(player.posX), MathHelper.floor_double(player.posY + 1), MathHelper.floor_double(player.posZ))));
+
     int leftover = RFperSecond % 20;
     boolean addExtraRF = player.worldObj.getTotalWorldTime() % 20 < leftover;
-    
+
     int toAdd = (RFperSecond / 20) + (addExtraRF ? 1 : 0);
 
-    if(toAdd != 0) {
+    if (toAdd != 0) {
 
       int nextIndex = player.getEntityData().getInteger("dsarmor:solar") % 4;
 
       for (int i = 0; i < 4 && toAdd > 0; i++) {
         ItemStack stack = player.inventory.armorInventory[nextIndex];
-        if(stack != null && (EnergyUpgrade.loadFromItem(stack) != null || (Config.darkSteelSolarChargeOthers && stack.getItem() instanceof IEnergyContainerItem))) {
+        if (stack != null
+            && (EnergyUpgrade.loadFromItem(stack) != null || (Config.darkSteelSolarChargeOthers && stack.getItem() instanceof IEnergyContainerItem))) {
           toAdd -= ((IEnergyContainerItem) stack.getItem()).receiveEnergy(stack, toAdd, false);
         }
         nextIndex = (nextIndex + 1) % 4;
       }
-      
+
       player.getEntityData().setInteger("dsarmor:solar", nextIndex);
     }
   }
@@ -194,25 +193,25 @@ public class DarkSteelController {
   private void updateSwim(EntityPlayer player) {
     ItemStack boots = player.getEquipmentInSlot(1);
     SwimUpgrade upgrade = SwimUpgrade.loadFromItem(boots);
-    if(upgrade == null) {
+    if (upgrade == null) {
       return;
     }
-    if(player.isInWater() && !player.capabilities.isFlying) {
+    if (player.isInWater() && !player.capabilities.isFlying) {
       player.motionX *= 1.1;
       player.motionZ *= 1.1;
     }
   }
 
   private void updateGlide(EntityPlayer player) {
-    if(!isGlideActive(player) || !isGliderUpgradeEquipped(player)) {
+    if (!isGlideActive(player) || !isGliderUpgradeEquipped(player)) {
       return;
     }
 
-    if(!player.onGround && player.motionY < 0 && !player.isSneaking() && !player.isInWater()) {
+    if (!player.onGround && player.motionY < 0 && !player.isSneaking() && !player.isInWater()) {
 
       double horizontalSpeed = Config.darkSteelGliderHorizontalSpeed;
       double verticalSpeed = Config.darkSteelGliderVerticalSpeed;
-      if(player.isSprinting()) {
+      if (player.isSprinting()) {
         verticalSpeed = Config.darkSteelGliderVerticalSpeedSprinting;
       }
 
@@ -228,16 +227,14 @@ public class DarkSteelController {
       VecmathUtil.computePlaneEquation(playerPos, b, c, plane);
       double dist = Math.abs(VecmathUtil.distanceFromPointToPlane(plane, new Vector3d(player.posX, player.posY, player.posZ)));
       double minDist = 0.15;
-      if(dist < minDist) {
+      if (dist < minDist) {
         double dropRate = (minDist * 10) - (dist * 10);
         verticalSpeed = verticalSpeed + (verticalSpeed * dropRate * 8);
         horizontalSpeed -= (0.02 * dropRate);
       }
 
-      double x = Math.cos(Math.toRadians(player.rotationYawHead + 90))
-          * horizontalSpeed;
-      double z = Math.sin(Math.toRadians(player.rotationYawHead + 90))
-          * horizontalSpeed;
+      double x = Math.cos(Math.toRadians(player.rotationYawHead + 90)) * horizontalSpeed;
+      double z = Math.sin(Math.toRadians(player.rotationYawHead + 90)) * horizontalSpeed;
 
       player.motionX += x;
       player.motionZ += z;
@@ -252,39 +249,40 @@ public class DarkSteelController {
   public boolean isGliderUpgradeEquipped(EntityPlayer player) {
     ItemStack chestPlate = player.getEquipmentInSlot(3);
     GliderUpgrade glideUpgrade = GliderUpgrade.loadFromItem(chestPlate);
-    if(glideUpgrade == null) {
+    if (glideUpgrade == null) {
       return false;
     }
     return true;
   }
 
   private void updateSword(EntityPlayer player) {
-    if(ItemDarkSteelSword.isEquipped(player)) {
+    if (ItemDarkSteelSword.isEquipped(player)) {
       IAttributeInstance attackInst = player.getAttributeMap().getAttributeInstance(SharedMonsterAttributes.attackDamage);
       attackInst.removeModifier(swordDamageModifierPowered);
 
       ItemStack sword = player.getCurrentEquippedItem();
-      if(Config.darkSteelSwordPowerUsePerHit <= 0 || EnergyUpgrade.getEnergyStored(sword) >= Config.darkSteelSwordPowerUsePerHit) {
+      if (Config.darkSteelSwordPowerUsePerHit <= 0 || EnergyUpgrade.getEnergyStored(sword) >= Config.darkSteelSwordPowerUsePerHit) {
         attackInst.applyModifier(swordDamageModifierPowered);
       }
     }
   }
 
   private void updateSpeed(EntityPlayer player) {
-    if(player.worldObj.isRemote || !player.onGround) {
+    if (player.worldObj.isRemote || !player.onGround) {
       return;
     }
 
     IAttributeInstance moveInst = player.getAttributeMap().getAttributeInstance(SharedMonsterAttributes.movementSpeed);
-    if(moveInst.getModifier(walkModifiers[0].getID()) != null) {
-      moveInst.removeModifier(walkModifiers[0]); //any will so as they all have the same UID
-    } else if(moveInst.getModifier(sprintModifiers[0].getID()) != null) {
+    if (moveInst.getModifier(walkModifiers[0].getID()) != null) {
+      moveInst.removeModifier(walkModifiers[0]); // any will so as they all have
+                                                 // the same UID
+    } else if (moveInst.getModifier(sprintModifiers[0].getID()) != null) {
       moveInst.removeModifier(sprintModifiers[0]);
     }
 
     ItemStack leggings = player.getEquipmentInSlot(2);
     SpeedUpgrade speedUpgrade = SpeedUpgrade.loadFromItem(leggings);
-    if(leggings != null && leggings.getItem() == DarkSteelItems.itemDarkSteelLeggings && speedUpgrade != null && isSpeedActive(player)) {
+    if (leggings != null && leggings.getItem() == DarkSteelItems.itemDarkSteelLeggings && speedUpgrade != null && isSpeedActive(player)) {
 
       double horzMovement = Math.abs(player.distanceWalkedModified - player.prevDistanceWalkedModified);
       double costModifier = player.isSprinting() ? Config.darkSteelSprintPowerCost : Config.darkSteelWalkPowerCost;
@@ -292,9 +290,9 @@ public class DarkSteelController {
       int cost = (int) (horzMovement * costModifier);
       int totalEnergy = getPlayerEnergy(player, DarkSteelItems.itemDarkSteelLeggings);
 
-      if(totalEnergy > 0) {
+      if (totalEnergy > 0) {
         usePlayerEnergy(player, DarkSteelItems.itemDarkSteelLeggings, cost);
-        if(player.isSprinting()) {
+        if (player.isSprinting()) {
           moveInst.applyModifier(sprintModifiers[speedUpgrade.getLevel() - 1]);
         } else {
           moveInst.applyModifier(walkModifiers[speedUpgrade.getLevel() - 1]);
@@ -306,12 +304,12 @@ public class DarkSteelController {
   private void updateStepHeightAndFallDistance(EntityPlayer player) {
     ItemStack boots = player.getEquipmentInSlot(1);
 
-    if(boots != null && boots.getItem() == DarkSteelItems.itemDarkSteelBoots && !player.capabilities.allowFlying) {
+    if (boots != null && boots.getItem() == DarkSteelItems.itemDarkSteelBoots && !player.capabilities.allowFlying) {
       int costedDistance = (int) player.fallDistance;
-      if(costedDistance > 0) {
+      if (costedDistance > 0) {
         int energyCost = costedDistance * Config.darkSteelFallDistanceCost;
         int totalEnergy = getPlayerEnergy(player, DarkSteelItems.itemDarkSteelBoots);
-        if(totalEnergy > 0 && totalEnergy >= energyCost) {
+        if (totalEnergy > 0 && totalEnergy >= energyCost) {
           usePlayerEnergy(player, DarkSteelItems.itemDarkSteelBoots, energyCost);
           player.fallDistance -= costedDistance;
         }
@@ -319,33 +317,33 @@ public class DarkSteelController {
     }
 
     JumpUpgrade jumpUpgrade = JumpUpgrade.loadFromItem(boots);
-    if(jumpUpgrade != null && boots != null && boots.getItem() == DarkSteelItems.itemDarkSteelBoots && isStepAssistActive(player)) {
+    if (jumpUpgrade != null && boots != null && boots.getItem() == DarkSteelItems.itemDarkSteelBoots && isStepAssistActive(player)) {
       player.stepHeight = 1.0023F;
-    } else if(player.stepHeight == 1.0023F) {
+    } else if (player.stepHeight == 1.0023F) {
       player.stepHeight = 0.5001F;
     }
   }
 
   void usePlayerEnergy(EntityPlayer player, ItemDarkSteelArmor armor, int cost) {
-    if(cost == 0) {
+    if (cost == 0) {
       return;
     }
     int remaining = cost;
-    if(Config.darkSteelDrainPowerFromInventory) {
+    if (Config.darkSteelDrainPowerFromInventory) {
       for (ItemStack stack : player.inventory.mainInventory) {
-        if(stack != null && stack.getItem() instanceof IEnergyContainerItem) {
+        if (stack != null && stack.getItem() instanceof IEnergyContainerItem) {
           IEnergyContainerItem cont = (IEnergyContainerItem) stack.getItem();
           int used = cont.extractEnergy(stack, remaining, false);
           remaining -= used;
-          if(remaining <= 0) {    
+          if (remaining <= 0) {
             return;
           }
         }
       }
     }
-    if(armor != null && remaining > 0) {
+    if (armor != null && remaining > 0) {
       ItemStack stack = player.inventory.armorInventory[3 - armor.armorType];
-      if(stack != null) {
+      if (stack != null) {
         armor.extractEnergy(stack, remaining, false);
       }
     }
@@ -354,26 +352,27 @@ public class DarkSteelController {
   private int getPlayerEnergy(EntityPlayer player, ItemDarkSteelArmor armor) {
     int res = 0;
 
-    if(Config.darkSteelDrainPowerFromInventory) {
+    if (Config.darkSteelDrainPowerFromInventory) {
       for (ItemStack stack : player.inventory.mainInventory) {
-        if(stack != null && stack.getItem() instanceof IEnergyContainerItem) {
+        if (stack != null && stack.getItem() instanceof IEnergyContainerItem) {
           IEnergyContainerItem cont = (IEnergyContainerItem) stack.getItem();
           res += cont.extractEnergy(stack, Integer.MAX_VALUE, true);
         }
       }
     }
-    if(armor != null) {
+    if (armor != null) {
       ItemStack stack = player.inventory.armorInventory[3 - armor.armorType];
       res = armor.getEnergyStored(stack);
     }
     return res;
   }
-  
+
   @SubscribeEvent
   public void onStartTracking(PlayerEvent.StartTracking event) {
     if (event.target instanceof EntityPlayerMP) {
       for (PacketUpgradeState.Type type : PacketUpgradeState.Type.values()) {
-        PacketHandler.sendTo(new PacketUpgradeState(type, isActive((EntityPlayer) event.target, type), event.target.getEntityId()), (EntityPlayerMP) event.entityPlayer);
+        PacketHandler.sendTo(new PacketUpgradeState(type, isActive((EntityPlayer) event.target, type), event.target.getEntityId()),
+            (EntityPlayerMP) event.entityPlayer);
       }
     }
   }
@@ -381,7 +380,7 @@ public class DarkSteelController {
   @SideOnly(Side.CLIENT)
   @SubscribeEvent
   public void onClientTick(TickEvent.ClientTickEvent event) {
-    if(event.phase == TickEvent.Phase.END) {
+    if (event.phase == TickEvent.Phase.END) {
       EntityPlayerSP player = Minecraft.getMinecraft().thePlayer;
       if (player == null) {
         return;
@@ -391,14 +390,16 @@ public class DarkSteelController {
         return;
       }
       MovementInput input = player.movementInput;
-      if(input.jump && !wasJumping) {
-        doJump(player);
-      } else if(input.jump && jumpCount < 3 && ticksSinceLastJump > 5) {
-        doJump(player);
+      if (input != null) {
+        if (input.jump && !wasJumping) {
+          doJump(player);
+        } else if (input.jump && jumpCount < 3 && ticksSinceLastJump > 5) {
+          doJump(player);
+        }
       }
 
       wasJumping = !player.onGround;
-      if(!wasJumping) {
+      if (!wasJumping) {
         jumpCount = 0;
       }
       ticksSinceLastJump++;
@@ -422,7 +423,7 @@ public class DarkSteelController {
     float pitch = player.prevRotationPitch + (player.rotationPitch - player.prevRotationPitch) * event.partialRenderTick;
 
     GL11.glPushMatrix();
-    if(player.isSneaking()) {
+    if (player.isSneaking()) {
       GL11.glTranslatef(0, 0.0625f, 0);
     }
     GL11.glRotatef(yawOffset, 0, -1, 0);
@@ -460,26 +461,27 @@ public class DarkSteelController {
     if (!isJumpActive(player)) {
       return;
     }
-    
+
     ItemStack boots = player.getEquipmentInSlot(1);
     JumpUpgrade jumpUpgrade = JumpUpgrade.loadFromItem(boots);
 
-    if(jumpUpgrade == null || boots == null || boots.getItem() != DarkSteelItems.itemDarkSteelBoots) {
+    if (jumpUpgrade == null || boots == null || boots.getItem() != DarkSteelItems.itemDarkSteelBoots) {
       return;
     }
 
     int requiredPower = Config.darkSteelBootsJumpPowerCost * (int) Math.pow(jumpCount + 1, 2.5);
     int availablePower = getPlayerEnergy(player, DarkSteelItems.itemDarkSteelBoots);
-    if(availablePower > 0 && requiredPower <= availablePower && jumpCount < jumpUpgrade.getLevel()) {
+    if (availablePower > 0 && requiredPower <= availablePower && jumpCount < jumpUpgrade.getLevel()) {
       jumpCount++;
       player.motionY += 0.15 * Config.darkSteelBootsJumpModifier * jumpCount;
       ticksSinceLastJump = 0;
       usePlayerEnergy(player, DarkSteelItems.itemDarkSteelBoots, requiredPower);
-      player.worldObj.playSound(player.posX, player.posY, player.posZ, EnderIO.MODID + ":ds.jump", 1.0f, player.worldObj.rand.nextFloat() * 0.5f + 0.75f, false);
+      player.worldObj.playSound(player.posX, player.posY, player.posZ, EnderIO.MODID + ":ds.jump", 1.0f, player.worldObj.rand.nextFloat() * 0.5f + 0.75f,
+          false);
       Random rand = player.worldObj.rand;
       for (int i = rand.nextInt(10) + 5; i >= 0; i--) {
-        EntityFX fx = Minecraft.getMinecraft().effectRenderer.spawnEffectParticle(EnumParticleTypes.REDSTONE.getParticleID(), player.posX + (rand.nextDouble() * 0.5 - 0.25), player.posY - player.getYOffset(), player.posZ
-            + (rand.nextDouble() * 0.5 - 0.25), 1, 1, 1);
+        EntityFX fx = Minecraft.getMinecraft().effectRenderer.spawnEffectParticle(EnumParticleTypes.REDSTONE.getParticleID(),
+            player.posX + (rand.nextDouble() * 0.5 - 0.25), player.posY - player.getYOffset(), player.posZ + (rand.nextDouble() * 0.5 - 0.25), 1, 1, 1);
         fx.setVelocity(player.motionX + (rand.nextDouble() * 0.5 - 0.25), (player.motionY / 2) + (rand.nextDouble() * -0.05),
             player.motionZ + (rand.nextDouble() * 0.5 - 0.25));
         Minecraft.getMinecraft().effectRenderer.addEffect(fx);
@@ -488,32 +490,31 @@ public class DarkSteelController {
     }
 
   }
-  
-  
+
   private void updateNightvision(EntityPlayer player) {
-    if(isNightVisionUpgradeEquipped(player) && nightVisionActive) {
+    if (isNightVisionUpgradeEquipped(player) && nightVisionActive) {
       player.addPotionEffect(new PotionEffect(Potion.nightVision.getId(), 210, 0, true, true));
-    } 
-    if(!isNightVisionUpgradeEquipped(player) && nightVisionActive) {
+    }
+    if (!isNightVisionUpgradeEquipped(player) && nightVisionActive) {
       nightVisionActive = false;
       removeNightvision = true;
     }
-    if(removeNightvision) {
+    if (removeNightvision) {
       player.removePotionEffect(Potion.nightVision.getId());
       removeNightvision = false;
     }
   }
 
   public boolean isNightVisionUpgradeEquipped(EntityPlayer player) {
-    ItemStack helmet = player.getEquipmentInSlot(4);    
-    return NightVisionUpgrade.loadFromItem(helmet) != null;    
+    ItemStack helmet = player.getEquipmentInSlot(4);
+    return NightVisionUpgrade.loadFromItem(helmet) != null;
   }
 
   public void setNightVisionActive(boolean isNightVisionActive) {
-    if(nightVisionActive && !isNightVisionActive) {
+    if (nightVisionActive && !isNightVisionActive) {
       removeNightvision = true;
     }
-    this.nightVisionActive = isNightVisionActive;    
+    this.nightVisionActive = isNightVisionActive;
   }
 
   public boolean isNightVisionActive() {
