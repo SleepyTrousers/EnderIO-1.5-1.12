1a02a7abfc6485c0343032508f71a8bd9a39f66b
CrazyPants <crazypants.mc@gmail.com>
2016-05-29 21:09:10 +0800

    Added back EnderIOCrashCallable registration as Forge bug fixed in release 12.17.0.1932

--- src/main/java/crazypants/enderio/EnderIO.java
+++ src/main/java/crazypants/enderio/EnderIO.java
@@ -129,6 +129,7 @@ import crazypants.enderio.teleport.anchor.BlockTravelAnchor;
 import crazypants.enderio.teleport.anchor.TileTravelAnchor;
 import crazypants.enderio.teleport.telepad.BlockTelePad;
 import crazypants.enderio.teleport.telepad.ItemCoordSelector;
+import crazypants.enderio.tool.EnderIOCrashCallable;
 import crazypants.util.CapturedMob;
 import crazypants.util.Things;
 import net.minecraft.block.Block;
@@ -278,8 +279,7 @@ public class EnderIO {
   @EventHandler
   public void preInit(FMLPreInitializationEvent event) {
 
-    //TODO: 1.9.4 This causes a crash on forge version 1.9.4-12.17.0.1925-27 I have opened issue on #2924 on forge github
-//    EnderIOCrashCallable.create();
+    EnderIOCrashCallable.create();
 
     Config.load(event);
 
