bfeb16a9b4b6a8b18aec0353490e9ab891b19d4d
Henry Loenwind <henry@loenwind.info>
2016-05-30 05:52:59 +0200

    Cherrypick #3163

--- src/main/java/crazypants/enderio/item/ItemMagnet.java
+++ src/main/java/crazypants/enderio/item/ItemMagnet.java
@@ -68,7 +68,7 @@ public class ItemMagnet extends ItemEnergyContainer implements IResourceTooltipP
   }
 
   public static void drainPerSecondPower(ItemStack itemStack) {
-    DarkSteelItems.itemMagnet.extractEnergy(itemStack, Config.magnetPowerUsePerSecondRF, false);
+    DarkSteelItems.itemMagnet.extractEnergyInternal(itemStack, Config.magnetPowerUsePerSecondRF, false);
   }
 
   static MagnetController controller = new MagnetController();
@@ -122,6 +122,19 @@ public class ItemMagnet extends ItemEnergyContainer implements IResourceTooltipP
   @Override
   public void onCreated(ItemStack itemStack, World world, EntityPlayer entityPlayer) {
     setEnergy(itemStack, 0);
+  }
+
+  @Override
+  public int extractEnergy(ItemStack container, int maxExtract, boolean simulate) {
+    if (Config.magnetAllowPowerExtraction) {
+      return extractEnergyInternal(container, maxExtract, simulate);
+    } else {
+      return 0;
+    }
+  }
+
+  public int extractEnergyInternal(ItemStack container, int maxExtract, boolean simulate) {
+    return super.extractEnergy(container, maxExtract, simulate);
   }
 
   void setEnergy(ItemStack container, int energy) {
