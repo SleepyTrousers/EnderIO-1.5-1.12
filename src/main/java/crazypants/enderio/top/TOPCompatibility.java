87d0a66b71ddad1ef4bb08e43f4499e6cafed4c6
Henry Loenwind <henry@loenwind.info>
2016-06-04 18:57:47 +0200

    More TOP

--- src/main/java/crazypants/enderio/top/TOPCompatibility.java
+++ src/main/java/crazypants/enderio/top/TOPCompatibility.java
@@ -1,6 +1,7 @@
 package crazypants.enderio.top;
 
 import java.util.List;
+import java.util.Locale;
 
 import javax.annotation.Nullable;
 
@@ -12,10 +13,13 @@ import com.google.common.base.Function;
 import crazypants.enderio.BlockEio;
 import crazypants.enderio.EnderIO;
 import crazypants.enderio.Log;
+import crazypants.enderio.gui.IconEIO;
 import crazypants.enderio.machine.AbstractMachineEntity;
 import crazypants.enderio.machine.AbstractPoweredTaskEntity;
 import crazypants.enderio.machine.ContinuousTask;
+import crazypants.enderio.machine.IIoConfigurable;
 import crazypants.enderio.machine.IRedstoneModeControlable;
+import crazypants.enderio.machine.IoMode;
 import crazypants.enderio.machine.RedstoneControlMode;
 import crazypants.enderio.machine.RedstoneControlMode.IconHolder;
 import crazypants.enderio.machine.obelisk.spawn.AbstractMobObelisk;
@@ -40,6 +44,7 @@ import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 
 public class TOPCompatibility implements Function<ITheOneProbe, Void>, IProbeInfoProvider, IProbeConfigProvider {
@@ -69,7 +74,6 @@ public class TOPCompatibility implements Function<ITheOneProbe, Void>, IProbeInf
         boolean active = true;
         float progress = -1;
         int rf = -1, maxrf = 0;
-        String range = null;
 
         if (tileEntity instanceof AbstractMachineEntity) {
           AbstractMachineEntity te = (AbstractMachineEntity) tileEntity;
@@ -92,14 +96,6 @@ public class TOPCompatibility implements Function<ITheOneProbe, Void>, IProbeInf
             }
           }
         }
-        if (tileEntity instanceof IRanged) {
-          IRanged te = (IRanged) tileEntity;
-          BoundingBox bounds = te.getBounds();
-          int sizeX = (int) bounds.sizeX();
-          int sizeY = (int) bounds.sizeY();
-          int sizeZ = (int) bounds.sizeZ();
-          range = sizeX + "x" + sizeY + "x" + sizeZ;
-        }
 
         IProbeInfo eiobox = probeInfo.vertical(probeInfo.defaultLayoutStyle().borderColor(0xffff0000));
 
@@ -110,7 +106,7 @@ public class TOPCompatibility implements Function<ITheOneProbe, Void>, IProbeInf
                 probeInfo.defaultProgressStyle().suffix(EnderIO.lang.localize("top.suffix.percent")).filledColor(0xffffb600).alternateFilledColor(0xffffb600));
           } else if (maxrf > 0 && rf == 0) {
             eiobox.horizontal(probeInfo.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER)).item(new ItemStack(Items.CLOCK))
-                .text(EnderIO.lang.localize("top.progress.outofpower"));
+                .text(TextFormatting.DARK_RED + EnderIO.lang.localize("top.progress.outofpower"));
           } else {
             eiobox.horizontal(probeInfo.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER)).item(new ItemStack(Items.CLOCK))
                 .text(EnderIO.lang.localize("top.progress.idle"));
@@ -131,7 +127,7 @@ public class TOPCompatibility implements Function<ITheOneProbe, Void>, IProbeInf
                 probeInfo.defaultProgressStyle().suffix(EnderIO.lang.localize("top.suffix.rf")).filledColor(0xffd63223).alternateFilledColor(0xffd63223));
           } else {
             eiobox.horizontal(probeInfo.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER)).item(new ItemStack(Items.REDSTONE))
-                .text(EnderIO.lang.localize("top.machine.outofpower"));
+                .text(TextFormatting.DARK_RED + EnderIO.lang.localize("top.machine.outofpower"));
           }
         }
 
@@ -141,21 +137,34 @@ public class TOPCompatibility implements Function<ITheOneProbe, Void>, IProbeInf
           boolean redstoneControlStatus = te.getRedstoneControlStatus();
           IconHolder iconHolder = RedstoneControlMode.IconHolder.getFromMode(redstoneControlMode);
           IWidgetIcon icon = iconHolder.getIcon();
-          ResourceLocation texture = icon.getMap().getTexture();
-          int x = icon.getX();
-          int y = icon.getY();
-          int width = icon.getWidth();
-          int height = icon.getHeight();
           String tooltip = iconHolder.getTooltip();
 
-          eiobox.horizontal(probeInfo.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER))
-              .icon(texture, x, y, width, height, probeInfo.defaultIconStyle().width(20).height(20))
-              .text(EnderIO.lang.localize("top.redstone." + redstoneControlStatus, tooltip));
+          addIcon(eiobox.horizontal(probeInfo.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER)), icon)
+              .vertical(probeInfo.defaultLayoutStyle().spacing(-1)).text(tooltip).text(TextFormatting.YELLOW
+                  + EnderIO.lang.localize("top.redstone.header", TextFormatting.WHITE + EnderIO.lang.localize("top.redstone." + redstoneControlStatus)));
         }
 
-        if (range != null) {
+        if (tileEntity instanceof IIoConfigurable) {
+          IIoConfigurable te = (IIoConfigurable) tileEntity;
+          IoMode ioMode = te.getIoMode(data.getSideHit());
+
+          addIcon(eiobox.horizontal(probeInfo.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER)), IconEIO.IO_CONFIG_UP)
+              .vertical(probeInfo.defaultLayoutStyle().spacing(-1))
+              .text(TextFormatting.YELLOW + EnderIO.lang.localize("gui.machine.side",
+                  TextFormatting.WHITE + EnderIO.lang.localize("gui.machine.side." + data.getSideHit().name().toLowerCase(Locale.US))))
+              .text(TextFormatting.YELLOW + EnderIO.lang.localize("gui.machine.ioMode", ioMode.colorLocalisedName()));
+        }
+
+        if (tileEntity instanceof IRanged) {
+          IRanged te = (IRanged) tileEntity;
+          BoundingBox bounds = te.getBounds();
+          int sizeX = (int) bounds.sizeX();
+          int sizeY = (int) bounds.sizeY();
+          int sizeZ = (int) bounds.sizeZ();
+
           eiobox.horizontal(probeInfo.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER)).item(new ItemStack(Blocks.STONE))
-              .text(EnderIO.lang.localize("top.header.range", range));
+              .text(TextFormatting.YELLOW
+                  + EnderIO.lang.localize("top.range.header", TextFormatting.WHITE + EnderIO.lang.localize("top.range", sizeX, sizeY, sizeZ)));
         }
 
         if (tileEntity instanceof AbstractMobObelisk) {
@@ -165,10 +174,10 @@ public class TOPCompatibility implements Function<ITheOneProbe, Void>, IProbeInf
 
           IProbeInfo mobbox = probeInfo.vertical(probeInfo.defaultLayoutStyle().borderColor(0xffff0000));
 
-          mobbox.text(EnderIO.lang.localize("top.header.action", spawnObeliskAction.getActionString()));
+          mobbox.text(TextFormatting.YELLOW + EnderIO.lang.localize("top.action.header", spawnObeliskAction.getActionString()));
 
           if (mobsInFilter.isEmpty()) {
-            mobbox.text("nothing");
+            mobbox.text(TextFormatting.DARK_RED + EnderIO.lang.localize("top.action.none"));
           } else if (mobsInFilter.size() <= 4) {
             for (CapturedMob capturedMob : mobsInFilter) {
               mobbox.horizontal(probeInfo.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER)).entity(capturedMob.getEntity(world, false))
@@ -211,6 +220,16 @@ public class TOPCompatibility implements Function<ITheOneProbe, Void>, IProbeInf
     if (config != null && blockState != null && blockState.getBlock() instanceof BlockEio) {
       config.setRFMode(0);
     }
+  }
+
+  private IProbeInfo addIcon(IProbeInfo probeInfo, IWidgetIcon icon) {
+    ResourceLocation texture = icon.getMap().getTexture();
+    int x = icon.getX();
+    int y = icon.getY();
+    int width = icon.getWidth();
+    int height = icon.getHeight();
+
+    return probeInfo.icon(texture, x, y, width, height, probeInfo.defaultIconStyle().width(20).height(20));
   }
 
 }
