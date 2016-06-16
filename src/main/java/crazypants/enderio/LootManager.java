502b76e31f4d3b2fd4e884346e371c52728538da
CrazyPants <crazypants.mc@gmail.com>
2016-06-17 01:36:57 +0800

    Updated loot implementation

--- src/main/java/crazypants/enderio/LootManager.java
+++ src/main/java/crazypants/enderio/LootManager.java
@@ -1,7 +1,9 @@
 package crazypants.enderio;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Random;
 
 import crazypants.enderio.capacitor.LootSelector;
 import crazypants.enderio.config.Config;
@@ -9,16 +11,14 @@ import crazypants.enderio.item.darksteel.DarkSteelItems;
 import crazypants.enderio.material.Alloy;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.storage.loot.LootContext;
 import net.minecraft.world.storage.loot.LootEntry;
-import net.minecraft.world.storage.loot.LootEntryItem;
 import net.minecraft.world.storage.loot.LootPool;
 import net.minecraft.world.storage.loot.LootTable;
 import net.minecraft.world.storage.loot.LootTableList;
 import net.minecraft.world.storage.loot.RandomValueRange;
 import net.minecraft.world.storage.loot.conditions.LootCondition;
-import net.minecraft.world.storage.loot.functions.LootFunction;
-import net.minecraft.world.storage.loot.functions.SetCount;
-import net.minecraft.world.storage.loot.functions.SetMetadata;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -34,137 +34,184 @@ public class LootManager {
   private LootManager() {
   }
 
-  private LootCondition[] noConditions = new LootCondition[0];
-
   @SubscribeEvent
   public void onLootTableLoad(LootTableLoadEvent evt) {
 
     LootTable table = evt.getTable();
 
+    InnerPool lp = new InnerPool();
+
     if (evt.getName().equals(LootTableList.CHESTS_SIMPLE_DUNGEON)) {
 
-      List<LootEntry> entries = new ArrayList<LootEntry>();
       if (Config.lootDarkSteel) {
-        entries.add(createLootEntry(EnderIO.itemAlloy, Alloy.DARK_STEEL.ordinal(), 1, 3, 15));
+        lp.addItem(createLootEntry(EnderIO.itemAlloy, Alloy.DARK_STEEL.ordinal(), 1, 3, 0.25));
       }
       if (Config.lootItemConduitProbe) {
-        entries.add(createLootEntry(EnderIO.itemConduitProbe, 1, 1, 10));
+        lp.addItem(createLootEntry(EnderIO.itemConduitProbe, 0.10));
       }
       if (Config.lootQuartz) {
-        entries.add(createLootEntry(Items.QUARTZ, 3, 16, 20));
+        lp.addItem(createLootEntry(Items.QUARTZ, 3, 16, 0.25));
       }
       if (Config.lootNetherWart) {
-        entries.add(createLootEntry(Items.NETHER_WART, 1, 4, 10));
+        lp.addItem(createLootEntry(Items.NETHER_WART, 1, 4, 0.20));
       }
       if (Config.lootEnderPearl) {
-        entries.add(createLootEntry(Items.ENDER_PEARL, 1, 2, 30));
+        lp.addItem(createLootEntry(Items.ENDER_PEARL, 1, 2, 0.30));
       }
-      if(Config.lootTheEnder) {
-        entries.add(createLootEntry(DarkSteelItems.itemDarkSteelSword, 1,1, 5));
+      if (Config.lootTheEnder) {
+        lp.addItem(createLootEntry(DarkSteelItems.itemDarkSteelSword, 0.1));
       }
-      if(Config.lootDarkSteelBoots) {
-        entries.add(createLootEntry(DarkSteelItems.itemDarkSteelBoots, 1,1, 5));
+      if (Config.lootDarkSteelBoots) {
+        lp.addItem(createLootEntry(DarkSteelItems.itemDarkSteelBoots, 0.1));
       }
-      entries.add(createLootCapacitor(15));
-      if (entries.isEmpty()) {
-        return;
-      }
-      int minRolls = Math.min(2, Math.max(0, entries.size() - 2));
-      int maxRolls = Math.min(entries.size() , entries.size() - 2);
-      LootPool lp = createPool(minRolls, maxRolls);
-      for (LootEntry entry : entries) {
-        lp.addEntry(entry);
-      }
-      table.addPool(lp);
+      lp.addItem(createLootCapacitor(0.15));
+      lp.addItem(createLootCapacitor(0.15));
+      lp.addItem(createLootCapacitor(0.15));
 
     } else if (evt.getName().equals(LootTableList.CHESTS_VILLAGE_BLACKSMITH)) {
-      
-      List<LootEntry> entries = new ArrayList<LootEntry>();
+
       if (Config.lootElectricSteel) {
-        entries.add(createLootEntry(EnderIO.itemAlloy, Alloy.ELECTRICAL_STEEL.ordinal(), 2, 6, 20));
+        lp.addItem(createLootEntry(EnderIO.itemAlloy, Alloy.ELECTRICAL_STEEL.ordinal(), 2, 6, 0.20));
       }
       if (Config.lootRedstoneAlloy) {
-        entries.add(createLootEntry(EnderIO.itemAlloy, Alloy.REDSTONE_ALLOY.ordinal(), 3, 6, 35));
+        lp.addItem(createLootEntry(EnderIO.itemAlloy, Alloy.REDSTONE_ALLOY.ordinal(), 3, 6, 0.35));
       }
       if (Config.lootDarkSteel) {
-        entries.add(createLootEntry(EnderIO.itemAlloy, Alloy.DARK_STEEL.ordinal(), 3, 6, 35));
+        lp.addItem(createLootEntry(EnderIO.itemAlloy, Alloy.DARK_STEEL.ordinal(), 3, 6, 0.35));
       }
       if (Config.lootPhasedIron) {
-        entries.add(createLootEntry(EnderIO.itemAlloy, Alloy.PULSATING_IRON.ordinal(), 1, 2, 10));
+        lp.addItem(createLootEntry(EnderIO.itemAlloy, Alloy.PULSATING_IRON.ordinal(), 1, 2, 0.3));
       }
       if (Config.lootPhasedGold) {
-        entries.add(createLootEntry(EnderIO.itemAlloy, Alloy.VIBRANT_ALLOY.ordinal(), 1, 2, 5));
+        lp.addItem(createLootEntry(EnderIO.itemAlloy, Alloy.VIBRANT_ALLOY.ordinal(), 1, 2, 0.2));
       }
-      if(Config.lootTheEnder) {
-        entries.add(createLootEntry(DarkSteelItems.itemDarkSteelSword, 1,1, 5));
+      if (Config.lootTheEnder) {
+        lp.addItem(createLootEntry(DarkSteelItems.itemDarkSteelSword, 1, 1, 0.25));
       }
-      if(Config.lootDarkSteelBoots) {
-        entries.add(createLootEntry(DarkSteelItems.itemDarkSteelBoots, 1,1, 5));
+      if (Config.lootDarkSteelBoots) {
+        lp.addItem(createLootEntry(DarkSteelItems.itemDarkSteelBoots, 1, 1, 0.25));
       }
-      entries.add(createLootCapacitor(5));
+      lp.addItem(createLootCapacitor(0.1));
 
-      if (entries.isEmpty()) {
-        return;
-      }
-      int minRolls = Math.min(2, Math.max(0, entries.size() - 2));
-      int maxRolls = Math.min(entries.size() , entries.size() - 2);
-      LootPool lp = createPool(minRolls, maxRolls);
-      for (LootEntry entry : entries) {
-        lp.addEntry(entry);
-      }
-      table.addPool(lp);
     } else if (evt.getName().equals(LootTableList.CHESTS_DESERT_PYRAMID)) {
-      LootPool mainPool = table.getPool("main");
-      if(mainPool == null) {
-        Log.error("LootManager.onLootTableLoad Could not add to the main loot pool of CHESTS_DESERT_PYRAMID");
-        return;
+
+      if (Config.lootTheEnder) {
+        lp.addItem(createLootEntry(DarkSteelItems.itemDarkSteelSword, 0.2));
       }
-      if(Config.lootTheEnder) {               
-        mainPool.addEntry(createLootEntry(DarkSteelItems.itemDarkSteelSword, 1,1, 15));        
+      if (Config.lootTravelStaff) {
+        lp.addItem(createLootEntry(EnderIO.itemTravelStaff, 0.1));
       }
-      if(Config.lootTravelStaff) {
-        mainPool.addEntry(createLootEntry(EnderIO.itemTravelStaff, 1,1, 1));
-      }
-      mainPool.addEntry(createLootCapacitor(25));
+      lp.addItem(createLootCapacitor(25));
+
     } else if (evt.getName().equals(LootTableList.CHESTS_JUNGLE_TEMPLE)) {
-      LootPool mainPool = table.getPool("main");
-      if(mainPool == null) {
-        Log.error("LootManager.onLootTableLoad Could not add to the main loot pool of CHESTS_JUNGLE_TEMPLE");
-        return;
+
+      if (Config.lootTheEnder) {
+        lp.addItem(createLootEntry(DarkSteelItems.itemDarkSteelSword, 1, 1, 0.25));
       }
-      if(Config.lootTheEnder) {       
-        mainPool.addEntry(createLootEntry(DarkSteelItems.itemDarkSteelSword, 1,1, 15));        
+      if (Config.lootTravelStaff) {
+        lp.addItem(createLootEntry(EnderIO.itemTravelStaff, 1, 1, 0.1));
       }
-      if(Config.lootTravelStaff) {
-        mainPool.addEntry(createLootEntry(EnderIO.itemTravelStaff, 1,1, 1));
+      lp.addItem(createLootCapacitor(0.25));
+      lp.addItem(createLootCapacitor(0.25));
+    }
+    if (!lp.isEmpty()) {
+      table.addPool(lp);
+    }
+
+  }
+
+  private LootItem createLootEntry(Item item, double chance) {
+    return new LootItem(new ItemStack(item), chance, 1, 1);
+  }
+
+  private LootItem createLootEntry(Item item, int minSize, int maxSize, double chance) {
+    return new LootItem(new ItemStack(item), chance, minSize, maxSize);
+  }
+
+  private LootItem createLootEntry(Item item, int ordinal, int minStackSize, int maxStackSize, double chance) {
+    return new LootItem(new ItemStack(item, 1, ordinal), chance, minStackSize, maxStackSize);
+  }
+
+  private LootItem createLootCapacitor(double weight) {    
+    return new CapItem(weight);
+  }
+
+
+  private static LootSelector ls = new LootSelector(new LootCondition[0]);
+
+  private static class CapItem extends LootItem {
+
+    public CapItem(double chance) {
+      super(new ItemStack(EnderIO.itemBasicCapacitor), chance);
+    }
+
+    public ItemStack createStack(Random rnd) {
+      ItemStack res = item.copy();
+      ls.apply(res, rnd, null);
+      return res;
+    }
+  }
+
+  private static class LootItem {
+
+    ItemStack item;
+    double chance;
+    int minSize;
+    int maxSize;
+
+    public LootItem(ItemStack item, double chance) {
+      this(item, chance, 1, 1);
+    }
+
+    public LootItem(ItemStack item, double chance, int minSize, int maxSize) {
+      this.item = item;
+      this.chance = chance;
+      this.minSize = minSize;
+      this.maxSize = maxSize;
+    }
+
+    public ItemStack createStack(Random rnd) {
+      int size = minSize;
+      if (maxSize > minSize) {
+        size += rnd.nextInt(maxSize - minSize + 1);
       }
-      mainPool.addEntry(createLootCapacitor(15));
-    } 
 
+      ItemStack result = item.copy();
+      result.stackSize = size;
+      return result;
+    }
   }
 
-  private LootPool createPool(int minRolls, int maxRolls) {
-    RandomValueRange bonusRollsIn = new RandomValueRange(0, 0);
-    RandomValueRange rollsIn = new RandomValueRange(minRolls, maxRolls);
-    return new LootPool(new LootEntry[0], noConditions, rollsIn, bonusRollsIn, EnderIO.MOD_NAME);
-  }
+  private static class InnerPool extends LootPool {
 
-  private LootEntryItem createLootEntry(Item item, float minStackSize, float maxStackSize, int weight) {
-    LootFunction[] functionsIn = new LootFunction[] { new SetCount(noConditions, new RandomValueRange(minStackSize, maxStackSize)) };
-    return new LootEntryItem(item, weight, 1, functionsIn, noConditions, item.getUnlocalizedName());
-  }
+    private final List<LootItem> items = new ArrayList<LootItem>();
 
-  private LootEntryItem createLootEntry(Item item, int meta, float minStackSize, float maxStackSize, int weight) {
-    LootFunction[] functionsIn = new LootFunction[] { new SetCount(noConditions, new RandomValueRange(minStackSize, maxStackSize)),
-        new SetMetadata(noConditions, new RandomValueRange(meta, meta))
+    public InnerPool() {
+      super(new LootEntry[0], new LootCondition[0], new RandomValueRange(0, 0), new RandomValueRange(0, 0), EnderIO.MOD_NAME);
+    }
 
-    };
-    return new LootEntryItem(item, weight, 1, functionsIn, noConditions, item.getUnlocalizedName() + ":" + meta);
-  }
+    public boolean isEmpty() {
+      return items.isEmpty();
+    }
 
-  private LootEntryItem createLootCapacitor(int weight) {
-    LootFunction[] functionsIn = new LootFunction[] { new SetCount(noConditions, new RandomValueRange(1, 1)), new LootSelector(noConditions) };
-    return new LootEntryItem(EnderIO.itemBasicCapacitor, weight, 1, functionsIn, noConditions, EnderIO.itemBasicCapacitor.getUnlocalizedName());
+    public void addItem(LootItem entry) {
+      if (entry != null) {
+        items.add(entry);
+      }
+
+    }
+
+    public void generateLoot(Collection<ItemStack> stacks, Random rand, LootContext context) {      
+      for (LootItem entry : items) {
+        if (rand.nextDouble() < entry.chance) {
+          ItemStack stack = entry.createStack(rand);
+          if (stack != null) {
+//            System.out.println("LootManager.InnerPool.generateLoot: Added " + stack.getDisplayName() + " " + stack);
+            stacks.add(stack);
+          }
+        }
+      }
+    }
   }
 
 }
