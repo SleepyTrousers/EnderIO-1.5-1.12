dbe21b689f2113e0d98cfdaf7c3c34ee4da38262
Henry Loenwind <henry@loenwind.info>
2016-09-20 00:33:15 +0200

    Deco Block Stage 2

--- src/main/java/crazypants/enderio/block/BlockDecorationFacing.java
+++ src/main/java/crazypants/enderio/block/BlockDecorationFacing.java
@@ -6,6 +6,7 @@ import crazypants.enderio.machine.AbstractMachineEntity;
 import crazypants.enderio.paint.render.PaintedBlockAccessWrapper;
 import crazypants.enderio.render.property.EnumDecoBlock;
 import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
@@ -25,6 +26,7 @@ public class BlockDecorationFacing extends BlockDecoration {
   }
 
   public static final PropertyEnum<EnumFacing> FACING = PropertyEnum.create("facing", EnumFacing.class, EnumFacing.HORIZONTALS);
+  public static final PropertyBool ACTIVE = PropertyBool.create("active");
 
   private BlockDecorationFacing(@Nonnull String name) {
     super(name);
@@ -32,28 +34,32 @@ public class BlockDecorationFacing extends BlockDecoration {
 
   @Override
   protected void initDefaultState() {
-    setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.SOUTH));
+    setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.SOUTH).withProperty(ACTIVE, false));
   }
 
   @Override
   protected BlockStateContainer createBlockState() {
-    return new BlockStateContainer(this, new IProperty[] { EnumDecoBlock.TYPE, FACING });
+    return new BlockStateContainer(this, new IProperty[] { EnumDecoBlock.TYPE, FACING, ACTIVE });
   }
 
   @Override
   public IBlockState getStateFromMeta(int meta) {
-    return super.getStateFromMeta(meta).withProperty(FACING, EnumFacing.SOUTH);
+    return super.getStateFromMeta(meta).withProperty(FACING, EnumFacing.SOUTH).withProperty(ACTIVE, false);
   }
 
   @Override
   public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
+    IBlockState result = state;
     if (worldIn instanceof PaintedBlockAccessWrapper) {
       TileEntity tileEntity = ((PaintedBlockAccessWrapper) worldIn).getRealTileEntity(pos);
       if (tileEntity instanceof AbstractMachineEntity) {
-        return state.withProperty(FACING, ((AbstractMachineEntity) tileEntity).getFacing());
+        result = result.withProperty(FACING, ((AbstractMachineEntity) tileEntity).getFacing());
+      } else {
+        result = result.withProperty(FACING, EnumFacing.SOUTH);
       }
+      result = result.withProperty(ACTIVE, ((AbstractMachineEntity) tileEntity).isActive());
     }
-    return state.withProperty(FACING, EnumFacing.SOUTH);
+    return result;
   }
 
 }
