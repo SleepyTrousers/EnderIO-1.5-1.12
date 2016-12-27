40dc4c43e76ae32babb2534387c53762684b77e0
Henry Loenwind <henry@loenwind.info>
2016-12-27 11:33:23 +0100

    Removed unused old Storage Drawers API

--- src/api/java/com/jaquadro/minecraft/storagedrawers/api/inventory/package-info.java
+++
@@ -1,5 +0,0 @@
-@API(owner = "StorageDrawersAPI", provides = "StorageDrawersAPI|inventory", apiVersion = StorageDrawersApi.VERSION)
-package com.jaquadro.minecraft.storagedrawers.api.inventory;
-
-import com.jaquadro.minecraft.storagedrawers.api.StorageDrawersApi;
-import net.minecraftforge.fml.common.API;
--- src/api/java/com/jaquadro/minecraft/storagedrawers/api/pack/IPackBlock.java
+++
@@ -1,7 +0,0 @@
-/*package com.jaquadro.minecraft.storagedrawers.api.pack;
-
-public interface IPackBlock
-{
-    IPackDataResolver getDataResolver ();
-}
-*/
