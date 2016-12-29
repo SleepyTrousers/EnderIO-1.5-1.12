b8dbc4eb1a3531aa5f7ef11bbf9f7e75f5c7fb3c
Matthias Mann <MatthiasMann@gmx.de>
2016-12-29 16:00:31 +0100

    removed fire block from creative inventory, tooltips don't make sense either

--- src/main/java/crazypants/enderio/block/BlockColdFire.java
+++ src/main/java/crazypants/enderio/block/BlockColdFire.java
@@ -2,8 +2,6 @@ package crazypants.enderio.block;
 
 import java.util.Random;
 
-import com.enderio.core.api.client.gui.IResourceTooltipProvider;
-
 import crazypants.enderio.EnderIOTab;
 import crazypants.enderio.ModObject;
 import net.minecraft.block.BlockFire;
@@ -13,7 +11,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
-public class BlockColdFire extends BlockFire implements IResourceTooltipProvider {
+public class BlockColdFire extends BlockFire {
 
   public static BlockColdFire create() {
     BlockColdFire res = new BlockColdFire();
@@ -24,7 +22,6 @@ public class BlockColdFire extends BlockFire implements IResourceTooltipProvider
   private BlockColdFire() {
     setUnlocalizedName(ModObject.blockColdFire.getUnlocalisedName());
     setRegistryName(ModObject.blockColdFire.getUnlocalisedName());
-    setCreativeTab(EnderIOTab.tabEnderIO);
     setTickRandomly(false);
     setHardness(0.0F);
     setLightLevel(1.0F);
@@ -36,11 +33,6 @@ public class BlockColdFire extends BlockFire implements IResourceTooltipProvider
 
   @Override
   public void updateTick(World p_updateTick_1_, BlockPos p_updateTick_2_, IBlockState p_updateTick_3_, Random p_updateTick_4_) {
-  }
-
-  @Override
-  public String getUnlocalizedNameForTooltip(ItemStack itemStack) {
-    return this.getUnlocalizedName();
   }
 
 }
