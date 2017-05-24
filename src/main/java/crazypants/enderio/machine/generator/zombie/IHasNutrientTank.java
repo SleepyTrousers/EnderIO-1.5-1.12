80885b479ee1cdf5056d49d4b2ac9aa3906e0b18
Henry Loenwind <henry@loenwind.info>
2017-05-24 22:24:59 +0200

    Removed stuff

--- src/main/java/crazypants/enderio/machine/generator/zombie/IHasNutrientTank.java
+++
@@ -1,9 +0,0 @@
-package crazypants.enderio.machine.generator.zombie;
-
-import crazypants.enderio.fluid.SmartTank;
-
-public interface IHasNutrientTank {
-
-  SmartTank getNutrientTank();
-
-}
--- src/main/java/crazypants/enderio/machine/invpanel/client/SortOrder.java
+++
@@ -1,7 +0,0 @@
-package crazypants.enderio.machine.invpanel.client;
-
-public enum SortOrder {
-  NAME,
-  MOD,
-  COUNT
-}
