4a0da269f89c84306d8a5f343995915480c4f167
Henry Loenwind <henry@loenwind.info>
2017-06-17 20:43:44 +0200

    Chisel API part 2

--- src/main/java/crazypants/util/FacadeUtil.java
+++ src/main/java/crazypants/util/FacadeUtil.java
@@ -8,7 +8,6 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.fml.common.Optional.Method;
-import team.chisel.api.IFacade;
 
 public class FacadeUtil {
 
@@ -19,16 +18,55 @@ public class FacadeUtil {
 
   private static class InnerFacadeUtil extends FacadeUtil {
 
+    private static final FacadeUtil instance1 = new LegacyFacadeUtil();
+    private static final FacadeUtil instance2 = new CTMFacadeUtil();
+
+    @Override
+    public boolean isFacaded(IBlockState state) {
+      return instance1.isFacaded(state) || instance2.isFacaded(state);
+    }
+
+    @Override
+    public IBlockState getFacade(IBlockState state, @Nonnull IBlockAccess world, @Nonnull BlockPos pos, @Nullable EnumFacing side) {
+      if (instance1.isFacaded(state)) {
+        return instance1.getFacade(state, world, pos, side);
+      } else if (instance2.isFacaded(state)) {
+        return instance2.getFacade(state, world, pos, side);
+      } else {
+        return null;
+      }
+    }
+
+  }
+
+  private static class LegacyFacadeUtil extends FacadeUtil {
+
     @Override
     @Method(modid = "ChiselAPI")
     public boolean isFacaded(IBlockState state) {
-      return state != null && state.getBlock() instanceof IFacade;
+      return state != null && state.getBlock() instanceof team.chisel.api.IFacade;
     }
 
     @Override
     @Method(modid = "ChiselAPI")
     public IBlockState getFacade(IBlockState state, @Nonnull IBlockAccess world, @Nonnull BlockPos pos, @Nullable EnumFacing side) {
-      return isFacaded(state) ? ((IFacade) state.getBlock()).getFacade(world, pos, side) : null;
+      return isFacaded(state) ? ((team.chisel.api.IFacade) state.getBlock()).getFacade(world, pos, side) : null;
+    }
+
+  }
+
+  private static class CTMFacadeUtil extends FacadeUtil {
+
+    @Override
+    @Method(modid = "ctm-api")
+    public boolean isFacaded(IBlockState state) {
+      return state != null && state.getBlock() instanceof team.chisel.ctm.api.IFacade;
+    }
+
+    @Override
+    @Method(modid = "ctm-api")
+    public IBlockState getFacade(IBlockState state, @Nonnull IBlockAccess world, @Nonnull BlockPos pos, @Nullable EnumFacing side) {
+      return isFacaded(state) ? ((team.chisel.ctm.api.IFacade) state.getBlock()).getFacade(world, pos, side) : null;
     }
 
   }
