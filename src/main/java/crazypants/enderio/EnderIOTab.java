4aa401d70fe65d809b06da5eeabeefc103226ef1
tterrag <tterrag1098@gmail.com>
2017-10-25 17:00:11 -0400

    Fix creative tab unloc names, add lang entries

--- src/main/java/crazypants/enderio/EnderIOTab.java
+++ src/main/java/crazypants/enderio/EnderIOTab.java
@@ -14,23 +14,27 @@ public class EnderIOTab extends CreativeTabs {
   public static final @Nonnull CreativeTabs tabNoTab, tabEnderIO, tabEnderIOItems, tabEnderIOMaterials, tabEnderIOMachines;
 
   static {
-    tabNoTab = new EnderIOTab(0);
-    tabEnderIOMachines = new EnderIOTab(3, CreativeTabs.CREATIVE_TAB_ARRAY.length - 1);
-    tabEnderIOItems = new EnderIOTab(1);
-    tabEnderIOMaterials = new EnderIOTab(2);
-    tabEnderIO = new EnderIOTab(0);
+    tabNoTab = new EnderIOTab(0, "invalid");
+    tabEnderIOMachines = new EnderIOTab(3, CreativeTabs.CREATIVE_TAB_ARRAY.length - 1, "machines");
+    tabEnderIOItems = new EnderIOTab(1, "items");
+    tabEnderIOMaterials = new EnderIOTab(2, "materials");
+    tabEnderIO = new EnderIOTab(0, "main");
   }
 
   private final int meta;
 
-  public EnderIOTab(int meta) {
-    super(EnderIO.MOD_NAME);
+  public EnderIOTab(int meta, String name) {
+    super(getUnloc(name));
     this.meta = meta;
   }
 
-  public EnderIOTab(int meta, int index) {
-    super(index, EnderIO.MOD_NAME);
+  public EnderIOTab(int meta, int index, String name) {
+    super(index, getUnloc(name));
     this.meta = meta;
+  }
+  
+  private static String getUnloc(String name) {
+    return EnderIO.DOMAIN + "." + name;
   }
 
   @SuppressWarnings("null")
