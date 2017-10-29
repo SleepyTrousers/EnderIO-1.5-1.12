296672873c439331a237461573717cbd9aa728e7
tterrag <tterrag1098@gmail.com>
2017-10-29 13:57:28 -0400

    Add missing file

---
+++ src/main/java/crazypants/enderio/machine/gui/GuiInventoryMachineBase.java
@@ -0,0 +1,29 @@
+package crazypants.enderio.machine.gui;
+
+import crazypants.enderio.machine.baselegacy.AbstractInventoryMachineEntity;
+import crazypants.enderio.machine.baselegacy.SlotDefinition;
+import crazypants.enderio.machine.modes.IoMode;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
+
+public class GuiInventoryMachineBase<T extends AbstractInventoryMachineEntity> extends GuiMachineBase<T> {
+  
+  protected GuiInventoryMachineBase(T machine, Container par1Container, String[] guiTexture) {
+    super(machine, par1Container, guiTexture);
+  }
+
+  @Override
+  public void renderSlotHighlights(IoMode mode) {
+    SlotDefinition slotDef = getTileEntity().getSlotDefinition();
+
+    for (Slot invSlot : inventorySlots.inventorySlots) {
+      if (invSlot.inventory == getTileEntity()) {
+        if ((mode == IoMode.PULL || mode == IoMode.PUSH_PULL) && slotDef.isInputSlot(invSlot.getSlotIndex())) {
+          renderSlotHighlight(invSlot, PULL_COLOR);
+        } else if ((mode == IoMode.PUSH || mode == IoMode.PUSH_PULL) && slotDef.isOutputSlot(invSlot.getSlotIndex())) {
+          renderSlotHighlight(invSlot, PUSH_COLOR);
+        }
+      }
+    }
+  }
+}
