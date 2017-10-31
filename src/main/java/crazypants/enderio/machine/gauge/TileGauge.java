a4490476ca7fd56e9374d1ed260d5c2c391a3e38
tterrag <tterrag1098@gmail.com>
2017-10-31 17:13:04 -0400

    Gauge is moving over too

---
+++ src/main/java/crazypants/enderio/machine/gauge/TileGauge.java
@@ -0,0 +1,13 @@
+package crazypants.enderio.machine.gauge;
+
+import crazypants.enderio.TileEntityEio;
+import info.loenwind.autosave.annotations.Storable;
+
+@Storable
+public class TileGauge extends TileEntityEio {
+
+  public TileGauge() {
+    super();
+  }
+
+}
