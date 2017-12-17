42271ca65246767ed31e516ed495994e355042a2
Unknown <henry@loenwind.info>
2017-12-17 05:01:43 +0100

    Fixed JEI integration for painter recipes

--- src/main/java/crazypants/enderio/machines/integration/jei/PainterRecipeCategory.java
+++ src/main/java/crazypants/enderio/machines/integration/jei/PainterRecipeCategory.java
@@ -2,15 +2,19 @@ package crazypants.enderio.machines.integration.jei;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import javax.annotation.Nonnull;
 
 import com.enderio.core.common.util.NNList;
+import com.enderio.core.common.util.stackable.Things;
 
 import crazypants.enderio.base.EnderIO;
 import crazypants.enderio.base.Log;
 import crazypants.enderio.base.lang.LangPower;
+import crazypants.enderio.base.paint.PaintUtil;
 import crazypants.enderio.base.recipe.IMachineRecipe;
 import crazypants.enderio.base.recipe.IMachineRecipe.ResultStack;
 import crazypants.enderio.base.recipe.MachineRecipeRegistry;
@@ -31,12 +35,10 @@ import mezz.jei.api.recipe.BlankRecipeCategory;
 import mezz.jei.api.recipe.BlankRecipeWrapper;
 import mezz.jei.api.recipe.IFocus;
 import mezz.jei.api.recipe.IStackHelper;
-import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
@@ -45,7 +47,6 @@ import static crazypants.enderio.machines.machine.painter.ContainerPainter.FIRST
 import static crazypants.enderio.machines.machine.painter.ContainerPainter.FIRST_RECIPE_SLOT;
 import static crazypants.enderio.machines.machine.painter.ContainerPainter.NUM_INVENTORY_SLOT;
 import static crazypants.enderio.machines.machine.painter.ContainerPainter.NUM_RECIPE_SLOT;
-import static crazypants.enderio.util.NbtValue.BLOCKSTATE;
 
 public class PainterRecipeCategory extends BlankRecipeCategory<PainterRecipeCategory.PainterRecipeWrapper> {
 
@@ -73,7 +74,7 @@ public class PainterRecipeCategory extends BlankRecipeCategory<PainterRecipeCate
       }
     }
 
-    List<ItemStack> paints = ClientConfig.jeiUseShortenedPainterRecipes.get() ? getLimitedItems() : validItems;
+    List<ItemStack> paints = ClientConfig.jeiUseShortenedPainterRecipes.get() ? getLimitedItems(validItems) : validItems;
 
     int count = 0;
     for (ItemStack paint : paints) {
@@ -100,7 +101,8 @@ public class PainterRecipeCategory extends BlankRecipeCategory<PainterRecipeCate
       }
     }
 
-    Log.info(String.format("PainterRecipeCategory: Added %d painter recipes in %d groups to JEI in %.3f seconds.", count, recipesWrappers.size(), (end - start) / 1000000000d));
+    Log.info(String.format("PainterRecipeCategory: Added %d painter recipes in %d groups to JEI in %.3f seconds.", count, recipesWrappers.size(),
+        (end - start) / 1000000000d));
 
     return recipesWrappers;
   }
@@ -122,27 +124,28 @@ public class PainterRecipeCategory extends BlankRecipeCategory<PainterRecipeCate
       this.results = results;
     }
 
-    public long getEnergyRequired() { 
+    public long getEnergyRequired() {
       return energyRequired;
     }
 
     @Override
     public void getIngredients(@Nonnull IIngredients ingredients) {
-      List<ItemStack> list = new ArrayList<ItemStack>(paints);
-      list.add(target);
-      ingredients.setInputs(ItemStack.class, list);
+      List<List<ItemStack>> list = new ArrayList<>();
+      list.add(Collections.singletonList(target));
+      list.add(paints);
+      ingredients.setInputLists(ItemStack.class, list);
 
       ingredients.setOutputs(ItemStack.class, results);
     }
-    
+
     @Override
-    public void drawInfo(@Nonnull Minecraft minecraft, int recipeWidth, int recipeHeight, int mouseX, int mouseY) {           
+    public void drawInfo(@Nonnull Minecraft minecraft, int recipeWidth, int recipeHeight, int mouseX, int mouseY) {
       String energyString = LangPower.RF(energyRequired);
-      minecraft.fontRenderer.drawString(energyString, 6, 36, 0x808080, false);    
-      GlStateManager.color(1,1,1,1);      
+      minecraft.fontRenderer.drawString(energyString, 6, 36, 0x808080, false);
+      GlStateManager.color(1, 1, 1, 1);
     }
   }
- 
+
   @SuppressWarnings("null")
   public static void register(IModRegistry registry, IJeiHelpers jeiHelpers) {
     registry.addRecipeCategories(new PainterRecipeCategory(jeiHelpers));
@@ -150,8 +153,7 @@ public class PainterRecipeCategory extends BlankRecipeCategory<PainterRecipeCate
     registry.addRecipeCategoryCraftingItem(new ItemStack(block_painter.getBlock()), PainterRecipeCategory.UID);
 
     List<ItemStack> validItems = registry.getIngredientRegistry().getIngredients(ItemStack.class);
-    registry.addRecipes(
-        splitRecipes(MachineRecipeRegistry.instance.getRecipesForMachine(block_painter.getUnlocalisedName()).values(), validItems), UID);
+    registry.addRecipes(splitRecipes(MachineRecipeRegistry.instance.getRecipesForMachine(MachineRecipeRegistry.PAINTER).values(), validItems), UID);
 
     registry.getRecipeTransferRegistry().addRecipeTransferHandler(ContainerPainter.class, PainterRecipeCategory.UID, FIRST_RECIPE_SLOT, NUM_RECIPE_SLOT,
         FIRST_INVENTORY_SLOT, NUM_INVENTORY_SLOT);
@@ -159,10 +161,10 @@ public class PainterRecipeCategory extends BlankRecipeCategory<PainterRecipeCate
 
   // ------------ Category
 
-  //Offsets from full size gui, makes it much easier to get the location correct
+  // Offsets from full size gui, makes it much easier to get the location correct
   private final static int xOff = 34;
   private final static int yOff = 28;
-  
+
   @Nonnull
   private final IStackHelper stackHelper;
   @Nonnull
@@ -170,7 +172,7 @@ public class PainterRecipeCategory extends BlankRecipeCategory<PainterRecipeCate
 
   @Nonnull
   protected final IDrawableAnimated arror;
-  
+
   public PainterRecipeCategory(IJeiHelpers jeiHelpers) {
     stackHelper = jeiHelpers.getStackHelper();
     IGuiHelper guiHelper = jeiHelpers.getGuiHelper();
@@ -188,8 +190,7 @@ public class PainterRecipeCategory extends BlankRecipeCategory<PainterRecipeCate
 
   @Override
   public @Nonnull String getTitle() {
-    String localizedName = block_painter.getBlock().getLocalizedName();
-    return localizedName != null ? localizedName : "ERROR";
+    return block_painter.getBlockNN().getLocalizedName();
   }
 
   @Override
@@ -201,84 +202,76 @@ public class PainterRecipeCategory extends BlankRecipeCategory<PainterRecipeCate
   public void drawExtras(@Nonnull Minecraft minecraft) {
     arror.draw(minecraft, 88 - xOff, 34 - yOff);
   }
-  
+
   @Override
-  public void setRecipe(@Nonnull IRecipeLayout recipeLayout, @Nonnull PainterRecipeCategory.PainterRecipeWrapper currentRecipe, @Nonnull IIngredients ingredients) {
-      IGuiItemStackGroup guiItemStacks = recipeLayout.getItemStacks();
-      guiItemStacks.init(0, true, 67 - xOff - 1, 34 - yOff - 1);
-      guiItemStacks.init(1, true, 38 - xOff - 1, 34 - yOff - 1);
-      guiItemStacks.init(2, false, 121 - xOff - 1, 34 - yOff - 1);
+  public void setRecipe(@Nonnull IRecipeLayout recipeLayout, @Nonnull PainterRecipeCategory.PainterRecipeWrapper currentRecipe,
+      @Nonnull IIngredients ingredients) {
+    IGuiItemStackGroup guiItemStacks = recipeLayout.getItemStacks();
+    guiItemStacks.init(0, true, 67 - xOff - 1, 34 - yOff - 1);
+    guiItemStacks.init(1, true, 38 - xOff - 1, 34 - yOff - 1);
+    guiItemStacks.init(2, false, 121 - xOff - 1, 34 - yOff - 1);
 
-      guiItemStacks.set(0, currentRecipe.target);
+    guiItemStacks.set(0, currentRecipe.target);
 
-      // Not very nice, but the only way to get correct painting recipes into JEI, it seems.
-      try {
+    IFocus<?> focus = recipeLayout.getFocus();
+    if (focus != null) {
+      Object focusValue = focus.getValue();
+      if (focusValue instanceof ItemStack) {
+        ItemStack focused = (ItemStack) focusValue;
+
         List<ItemStack> paints = new ArrayList<ItemStack>();
         List<ItemStack> results = new ArrayList<ItemStack>();
 
-        IFocus<?> focus = recipeLayout.getFocus();
-        Object focusValue = focus.getValue();
-        if (focusValue instanceof ItemStack) { //&& !focus.isBlank()
-          ItemStack focused = (ItemStack) focusValue;
-          if (focus.getMode() == IFocus.Mode.OUTPUT) {
-            // JEI is focusing on the output item. Limit the recipe to only the paints that actually give this output item. Needs some extra comparison
-            // because we told JEI to ignore paint information, which is ok for crafting and soul binding, but not here.
-            for (int i = 0; i < currentRecipe.paints.size(); i++) {
-              ItemStack resultStack = currentRecipe.results.get(i);
-              ItemStack paintStack = currentRecipe.paints.get(i);
-              if (stackHelper.isEquivalent(focused, resultStack) && BLOCKSTATE.getString(focused).equals(BLOCKSTATE.getString(resultStack))) {
-                paints.add(paintStack);
-                results.add(resultStack);
-              }
-            }
-          } else if (!stackHelper.isEquivalent(focused, currentRecipe.target)) {
-            // JEI is focusing on the paint. Limit the output items to things that are painted with this paint.
-            for (int i = 0; i < currentRecipe.paints.size(); i++) {
-              ItemStack resultStack = currentRecipe.results.get(i);
-              ItemStack paintStack = currentRecipe.paints.get(i);
-              if (stackHelper.isEquivalent(focused, paintStack)) {
-                paints.add(paintStack);
-                results.add(resultStack);
-              }
-            }
-          } else {
-            // JEI is focusing on a paintable item. If that item also can be used as a paint source, it will display "item+item=anything", which is somewhere
-            // between weird and wrong. So remove the recipe "item+item" from the list to get "anything+item=anything".
-            for (int i = 0; i < currentRecipe.paints.size(); i++) {
-              ItemStack resultStack = currentRecipe.results.get(i);
-              ItemStack paintStack = currentRecipe.paints.get(i);
-              if (!stackHelper.isEquivalent(focused, paintStack)) {
-                paints.add(paintStack);
-                results.add(resultStack);
-              }
+        if (focus.getMode() == IFocus.Mode.OUTPUT) {
+          // JEI is focusing on the output item. Limit the recipe to the fixed input item, the focused output item and the matching paint.
+          IBlockState paint = PaintUtil.getSourceBlock(focused);
+          ItemStack paintAsStack = PaintUtil.getPaintAsStack(paint);
+          paints.add(paintAsStack);
+          results.add(focused);
+        } else if (stackHelper.isEquivalent(focused, currentRecipe.target)) {
+          // JEI is focusing on a paintable item. If that item also can be used as a paint source, it will display "item+item=anything", which is somewhere
+          // between weird and wrong. So remove the recipe "item+item" from the list to get "anything+item=anything".
+          for (int i = 0; i < currentRecipe.paints.size(); i++) {
+            ItemStack resultStack = currentRecipe.results.get(i);
+            ItemStack paintStack = currentRecipe.paints.get(i);
+            if (!stackHelper.isEquivalent(focused, paintStack)) {
+              paints.add(paintStack);
+              results.add(resultStack);
             }
           }
-          if (!paints.isEmpty()) {
-            guiItemStacks.set(1, paints);
-            guiItemStacks.set(2, results);
-            return;
+        } else {
+          // JEI is focusing on the paint. Limit the output items to things that are painted with this paint.
+          for (ResultStack result : currentRecipe.recipe.getCompletedResult(focused, currentRecipe.target)) {
+            paints.add(focused);
+            results.add(result.item);
           }
         }
-      } catch (Throwable t) {
-        Log.debug(t.getMessage());
-      }
-
-      guiItemStacks.set(1, currentRecipe.paints);
-      guiItemStacks.set(2, currentRecipe.results);
-  }
-  
-  private static @Nonnull List<ItemStack> getLimitedItems() {
-    NNList<ItemStack> list = new NNList<>();
-    for (Block block : new Block[] { Blocks.STONE, Blocks.COBBLESTONE, Blocks.GRASS, Blocks.DIRT, Blocks.PLANKS, Blocks.GLASS, Blocks.STONE_STAIRS,
-        Blocks.RED_FLOWER, Blocks.SLIME_BLOCK, Blocks.TNT}) {
-      Item item = Item.getItemFromBlock(block);
-      if (item != null) {
-        for (CreativeTabs tab : item.getCreativeTabs()) {
-          item.getSubItems(item, tab, list);
+        if (!paints.isEmpty()) {
+          guiItemStacks.set(1, paints);
+          guiItemStacks.set(2, results);
+          return;
         }
       }
     }
-    return list;
+
+    guiItemStacks.set(1, currentRecipe.paints);
+    guiItemStacks.set(2, currentRecipe.results);
+  }
+
+  private static @Nonnull List<ItemStack> getLimitedItems(List<ItemStack> validItems) {
+    Things paints = new Things().add(Blocks.STONE).add(Blocks.COBBLESTONE).add(Blocks.GRASS).add(Blocks.DIRT).add(Blocks.PLANKS).add(Blocks.GLASS)
+        .add(Blocks.STONE_STAIRS).add(Blocks.RED_FLOWER).add(Blocks.SLIME_BLOCK).add(Blocks.TNT);
+    Random rand = new Random();
+    for (ItemStack stack : validItems) {
+      if (rand.nextFloat() < 0.05f && !paints.contains(stack)) {
+        paints.add(stack);
+      }
+    }
+    NNList<ItemStack> paintStacks = paints.getItemStacks();
+    while (paintStacks.size() > 50) {
+      paintStacks.remove(paintStacks.size() - 1);
+    }
+    return paintStacks;
   }
 
 }
