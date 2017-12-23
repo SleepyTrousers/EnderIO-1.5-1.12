2c39125b978d7e3f81232dd8177ee824c1c3bdbc
Unknown <henry@loenwind.info>
2017-12-23 23:20:44 +0100

    Added method to target all players that are watching a chunk to network handler

--- src/main/java/crazypants/enderio/base/network/PacketHandler.java
+++ src/main/java/crazypants/enderio/base/network/PacketHandler.java
@@ -24,9 +24,7 @@ import crazypants.enderio.base.transceiver.PacketChannelList;
 import crazypants.enderio.base.xp.PacketExperienceContainer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.network.NetworkRegistry.TargetPoint;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.relauncher.Side;
 
@@ -40,13 +38,8 @@ public class PacketHandler {
     return ID++;
   }
 
-  public static void sendToAllAround(IMessage message, TileEntity te, int range) {
-    BlockPos p = te.getPos();
-    INSTANCE.sendToAllAround(message, new TargetPoint(te.getWorld().provider.getDimension(), p.getX(), p.getY(), p.getZ(), range));
-  }
-
   public static void sendToAllAround(IMessage message, TileEntity te) {
-    sendToAllAround(message, te, 64);
+    INSTANCE.sendToAllAround(message, te);
   }
 
   public static void sendTo(IMessage message, EntityPlayerMP player) {
