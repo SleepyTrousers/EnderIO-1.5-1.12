81e1036e33c961364ce3633e0a1321f270009683
Tamás Fehérvári <geever@users.sourceforge.net>
2017-12-31 02:34:43 +0100

    Add power to Painter recipe ingredients

--- src/main/java/crazypants/enderio/machines/integration/jei/PainterRecipeCategory.java
+++ src/main/java/crazypants/enderio/machines/integration/jei/PainterRecipeCategory.java
@@ -11,9 +11,16 @@ import javax.annotation.Nonnull;
 import com.enderio.core.common.util.NNList;
 import com.enderio.core.common.util.stackable.Things;
 
+import static crazypants.enderio.machines.init.MachineObject.block_painter;
+import static crazypants.enderio.machines.machine.painter.ContainerPainter.FIRST_INVENTORY_SLOT;
+import static crazypants.enderio.machines.machine.painter.ContainerPainter.FIRST_RECIPE_SLOT;
+import static crazypants.enderio.machines.machine.painter.ContainerPainter.NUM_INVENTORY_SLOT;
+import static crazypants.enderio.machines.machine.painter.ContainerPainter.NUM_RECIPE_SLOT;
+
 import crazypants.enderio.base.EnderIO;
 import crazypants.enderio.base.Log;
-import crazypants.enderio.base.lang.LangPower;
+import crazypants.enderio.base.integration.jei.energy.EnergyIngredient;
+import crazypants.enderio.base.integration.jei.energy.EnergyIngredientRenderer;
 import crazypants.enderio.base.paint.PaintUtil;
 import crazypants.enderio.base.recipe.IMachineRecipe;
 import crazypants.enderio.base.recipe.IMachineRecipe.ResultStack;
@@ -28,6 +35,7 @@ import mezz.jei.api.IModRegistry;
 import mezz.jei.api.gui.IDrawable;
 import mezz.jei.api.gui.IDrawableAnimated;
 import mezz.jei.api.gui.IDrawableStatic;
+import mezz.jei.api.gui.IGuiIngredientGroup;
 import mezz.jei.api.gui.IGuiItemStackGroup;
 import mezz.jei.api.gui.IRecipeLayout;
 import mezz.jei.api.ingredients.IIngredients;
@@ -37,16 +45,9 @@ import mezz.jei.api.recipe.IFocus;
 import mezz.jei.api.recipe.IStackHelper;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-
-import static crazypants.enderio.machines.init.MachineObject.block_painter;
-import static crazypants.enderio.machines.machine.painter.ContainerPainter.FIRST_INVENTORY_SLOT;
-import static crazypants.enderio.machines.machine.painter.ContainerPainter.FIRST_RECIPE_SLOT;
-import static crazypants.enderio.machines.machine.painter.ContainerPainter.NUM_INVENTORY_SLOT;
-import static crazypants.enderio.machines.machine.painter.ContainerPainter.NUM_RECIPE_SLOT;
 
 public class PainterRecipeCategory extends BlankRecipeCategory<PainterRecipeCategory.PainterRecipeWrapper> {
 
@@ -110,7 +111,6 @@ public class PainterRecipeCategory extends BlankRecipeCategory<PainterRecipeCate
   public static class PainterRecipeWrapper extends BlankRecipeWrapper {
 
     final AbstractPainterTemplate<?> recipe;
-    final int energyRequired;
     final @Nonnull ItemStack target;
     final @Nonnull List<ItemStack> paints;
     final @Nonnull List<ItemStack> results;
@@ -118,14 +118,9 @@ public class PainterRecipeCategory extends BlankRecipeCategory<PainterRecipeCate
     public PainterRecipeWrapper(@Nonnull AbstractPainterTemplate<?> recipe, @Nonnull ItemStack target, @Nonnull List<ItemStack> paints,
         @Nonnull List<ItemStack> results) {
       this.recipe = recipe;
-      this.energyRequired = recipe.getEnergyRequired();
       this.target = target;
       this.paints = paints;
       this.results = results;
-    }
-
-    public long getEnergyRequired() {
-      return energyRequired;
     }
 
     @Override
@@ -134,15 +129,8 @@ public class PainterRecipeCategory extends BlankRecipeCategory<PainterRecipeCate
       list.add(Collections.singletonList(target));
       list.add(paints);
       ingredients.setInputLists(ItemStack.class, list);
-
       ingredients.setOutputs(ItemStack.class, results);
-    }
-
-    @Override
-    public void drawInfo(@Nonnull Minecraft minecraft, int recipeWidth, int recipeHeight, int mouseX, int mouseY) {
-      String energyString = LangPower.RF(energyRequired);
-      minecraft.fontRenderer.drawString(energyString, 6, 36, 0x808080, false);
-      GlStateManager.color(1, 1, 1, 1);
+      ingredients.setInput(EnergyIngredient.class, new EnergyIngredient(recipe.getEnergyRequired()));
     }
   }
 
@@ -165,22 +153,19 @@ public class PainterRecipeCategory extends BlankRecipeCategory<PainterRecipeCate
   private final static int xOff = 34;
   private final static int yOff = 28;
 
-  @Nonnull
-  private final IStackHelper stackHelper;
-  @Nonnull
-  private final IDrawable background;
-
-  @Nonnull
-  protected final IDrawableAnimated arror;
+  private final @Nonnull IStackHelper stackHelper;
+  private final @Nonnull IDrawable background;
+  private final @Nonnull IDrawableAnimated arrow;
 
   public PainterRecipeCategory(IJeiHelpers jeiHelpers) {
     stackHelper = jeiHelpers.getStackHelper();
     IGuiHelper guiHelper = jeiHelpers.getGuiHelper();
+
     ResourceLocation backgroundLocation = EnderIO.proxy.getGuiTexture("painter");
     background = guiHelper.createDrawable(backgroundLocation, xOff, yOff, 120, 50);
 
     IDrawableStatic flameDrawable = guiHelper.createDrawable(backgroundLocation, 176, 14, 24, 16);
-    arror = guiHelper.createAnimatedDrawable(flameDrawable, 200, IDrawableAnimated.StartDirection.LEFT, false);
+    arrow = guiHelper.createAnimatedDrawable(flameDrawable, 200, IDrawableAnimated.StartDirection.LEFT, false);
   }
 
   @Override
@@ -200,18 +185,19 @@ public class PainterRecipeCategory extends BlankRecipeCategory<PainterRecipeCate
 
   @Override
   public void drawExtras(@Nonnull Minecraft minecraft) {
-    arror.draw(minecraft, 88 - xOff, 34 - yOff);
+    arrow.draw(minecraft, 88 - xOff, 34 - yOff);
   }
 
   @Override
   public void setRecipe(@Nonnull IRecipeLayout recipeLayout, @Nonnull PainterRecipeCategory.PainterRecipeWrapper currentRecipe,
       @Nonnull IIngredients ingredients) {
     IGuiItemStackGroup guiItemStacks = recipeLayout.getItemStacks();
+    IGuiIngredientGroup<EnergyIngredient> group = recipeLayout.getIngredientsGroup(EnergyIngredient.class);
+
     guiItemStacks.init(0, true, 67 - xOff - 1, 34 - yOff - 1);
     guiItemStacks.init(1, true, 38 - xOff - 1, 34 - yOff - 1);
     guiItemStacks.init(2, false, 121 - xOff - 1, 34 - yOff - 1);
-
-    guiItemStacks.set(0, currentRecipe.target);
+    group.init(3, true, EnergyIngredientRenderer.INSTANCE, 108 - xOff, 60 - yOff, 50, 10, 0, 0);
 
     IFocus<?> focus = recipeLayout.getFocus();
     if (focus != null) {
@@ -254,8 +240,10 @@ public class PainterRecipeCategory extends BlankRecipeCategory<PainterRecipeCate
       }
     }
 
+    guiItemStacks.set(0, currentRecipe.target);
     guiItemStacks.set(1, currentRecipe.paints);
     guiItemStacks.set(2, currentRecipe.results);
+    group.set(ingredients);
   }
 
   private static @Nonnull List<ItemStack> getLimitedItems(List<ItemStack> validItems) {
@@ -273,5 +261,4 @@ public class PainterRecipeCategory extends BlankRecipeCategory<PainterRecipeCate
     }
     return paintStacks;
   }
-
 }
