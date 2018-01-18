a471c826ce35325f24d25d1415baf49cbdb82aa9
Henry Loenwind <henry@loenwind.info>
2018-01-19 00:15:44 +0100

    Giving a mod instance to the mystery parameter "Object mod" is considered "game activity" by Forge. Giving the String mod id is fine.

--- enderio-machines/src/main/java/crazypants/enderio/machines/machine/obelisk/weather/BlockWeatherObelisk.java
+++ enderio-machines/src/main/java/crazypants/enderio/machines/machine/obelisk/weather/BlockWeatherObelisk.java
@@ -33,11 +33,11 @@ public class BlockWeatherObelisk extends AbstractBlockObelisk<TileWeatherObelisk
     MinecraftForge.EVENT_BUS.register(ret);
     return ret;
   }
-  
+
   @SubscribeEvent
   public void onEntityRegister(RegistryEvent.Register<EntityEntry> event) {
-    EntityRegistry.registerModEntity(new ResourceLocation(EnderIO.DOMAIN, "weather_rocket"), EntityWeatherRocket.class, "weather_rocket", 33, EnderIO.instance,
-        64, 3, false);
+    EntityRegistry.registerModEntity(new ResourceLocation(EnderIO.DOMAIN, "weather_rocket"), EntityWeatherRocket.class, "weather_rocket", 33, EnderIO.MODID, 64,
+        3, false);
   }
 
   private BlockWeatherObelisk(@Nonnull IModObject modObject) {
