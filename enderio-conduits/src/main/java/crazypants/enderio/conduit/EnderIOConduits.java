0d6bdd7acd1a5c4d5c37906464c2a3da15874ca1
Henry Loenwind <henry@loenwind.info>
2018-01-23 19:47:23 +0100

    Cleaned resources, fixed mod id and dependencies

--- enderio-conduits/src/main/java/crazypants/enderio/conduit/EnderIOConduits.java
+++ enderio-conduits/src/main/java/crazypants/enderio/conduit/EnderIOConduits.java
@@ -1,21 +1,22 @@
 package crazypants.enderio.conduit;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import crazypants.enderio.api.addon.IEnderIOAddon;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
-import static crazypants.enderio.conduit.EnderIOConduits.*;
-
-@Mod(modid = MODID, name = MOD_NAME, version = VERSION, dependencies = "after:" + crazypants.enderio.base.EnderIO.MODID)
+@Mod(modid = EnderIOConduits.MODID, name = EnderIOConduits.MOD_NAME, version = EnderIOConduits.VERSION, dependencies = EnderIOConduits.DEPENDENCIES)
 public class EnderIOConduits implements IEnderIOAddon {
 
-  public static final @Nonnull String MODID = "enderio-conduits";
+  public static final @Nonnull String MODID = "enderioconduits";
   public static final @Nonnull String DOMAIN = "enderio";
   public static final @Nonnull String MOD_NAME = "Ender IO Conduits";
   public static final @Nonnull String VERSION = "@VERSION@";
+
+  private static final @Nonnull String DEFAULT_DEPENDENCIES = "after:" + crazypants.enderio.base.EnderIO.MODID;
+  public static final @Nonnull String DEPENDENCIES = DEFAULT_DEPENDENCIES;
 
   @Override
   @Nullable
