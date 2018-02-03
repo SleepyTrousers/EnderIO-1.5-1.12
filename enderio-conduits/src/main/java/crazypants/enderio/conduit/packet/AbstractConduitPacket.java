ae37e8f871b5362078caa74e7e6cb6c5ef9a6622
Henry Loenwind <henry@loenwind.info>
2018-02-03 18:57:37 +0100

    Added sanity checks for conduit manipulation

--- enderio-conduits/src/main/java/crazypants/enderio/conduit/packet/AbstractConduitPacket.java
+++ enderio-conduits/src/main/java/crazypants/enderio/conduit/packet/AbstractConduitPacket.java
@@ -4,13 +4,20 @@ import java.util.UUID;
 
 import javax.annotation.Nonnull;
 
+import com.enderio.core.common.util.BlockCoord;
+
+import crazypants.enderio.base.EnderIO;
+import crazypants.enderio.base.Log;
 import crazypants.enderio.base.conduit.IConduit;
 import crazypants.enderio.base.conduit.IConduitBundle;
 import crazypants.enderio.base.conduit.registry.ConduitRegistry;
+import crazypants.enderio.conduit.TileConduitBundle;
+import crazypants.enderio.conduit.gui.ExternalConnectionContainer;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import net.minecraftforge.fml.relauncher.Side;
 
 public class AbstractConduitPacket<T extends IConduit> extends AbstractConduitBundlePacket {
 
@@ -49,6 +56,20 @@ public class AbstractConduitPacket<T extends IConduit> extends AbstractConduitBu
 
   @SuppressWarnings("unchecked")
   public T getConduit(MessageContext ctx) {
+    if (ctx.side == Side.SERVER) {
+      if (ctx.getServerHandler().player.openContainer instanceof ExternalConnectionContainer) {
+        final TileConduitBundle tileEntity = ((ExternalConnectionContainer) ctx.getServerHandler().player.openContainer).getTileEntity();
+        if (tileEntity == null || !tileEntity.getPos().equals(getPos())) {
+          Log.warn("Player " + ctx.getServerHandler().player.getName() + " tried to manipulate conduit while having another conduit's GUI open!");
+          return null;
+        }
+      } else {
+        if (BlockCoord.get(ctx.getServerHandler().player).distanceSq(getPos()) > EnderIO.proxy.getReachDistanceForPlayer(ctx.getServerHandler().player)) {
+          Log.warn("Player " + ctx.getServerHandler().player.getName() + " tried to manipulate conduit without having its GUI open or being near it!");
+          return null;
+        }
+      }
+    }
     World world = getWorld(ctx);
     TileEntity tileEntity = getTileEntity(world);
     if (tileEntity instanceof IConduitBundle) {
