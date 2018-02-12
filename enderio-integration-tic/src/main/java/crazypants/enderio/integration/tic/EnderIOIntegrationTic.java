15b858e53831f79ab00398e7d6c7e9917a70dff5
Henry Loenwind <henry@loenwind.info>
2018-02-12 23:36:16 +0100

    Trying to break the dep cycle

--- enderio-integration-tic/src/main/java/crazypants/enderio/integration/tic/EnderIOIntegrationTic.java
+++ enderio-integration-tic/src/main/java/crazypants/enderio/integration/tic/EnderIOIntegrationTic.java
@@ -35,7 +35,7 @@ public class EnderIOIntegrationTic implements IEnderIOAddon {
   public static final @Nonnull String MOD_NAME = "Ender IO Integration with Tinkers' Construct";
   public static final @Nonnull String VERSION = "@VERSION@";
 
-  private static final @Nonnull String DEFAULT_DEPENDENCIES = "before:tconstruct;before:" + crazypants.enderio.base.EnderIO.MODID;
+  private static final @Nonnull String DEFAULT_DEPENDENCIES = "before:tconstruct;before:enderiobase";
   public static final @Nonnull String DEPENDENCIES = DEFAULT_DEPENDENCIES;
 
   @EventHandler
