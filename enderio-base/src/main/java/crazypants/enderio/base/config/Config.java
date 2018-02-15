c5d403d909fa67c782092a32e85da48ab03e3316
Henry Loenwind <henry@loenwind.info>
2018-02-15 07:25:50 +0100

    Fixed the most important bug ever

--- enderio-base/src/main/java/crazypants/enderio/base/config/Config.java
+++ enderio-base/src/main/java/crazypants/enderio/base/config/Config.java
@@ -482,7 +482,7 @@ public final class Config {
         "The maximum storage for a single tier three capacitor in RF").getInt(capacitorBankTierThreeMaxStorageRF);
 
     capacitorBankRenderPowerOverlayOnItem = config.getBoolean("capacitorBankRenderPowerOverlayOnItem", sectionAesthetic.name,
-        capacitorBankRenderPowerOverlayOnItem, "When true the the capacitor bank item wil get a power bar in addition to the gauge on the bank");
+        capacitorBankRenderPowerOverlayOnItem, "When true the capacitor bank item wil get a power bar in addition to the gauge on the bank");
 
     powerConduitTierOneRF = config.get(sectionPower.name, "powerConduitTierOneRF", powerConduitTierOneRF, "The maximum IO for the tier 1 power conduit")
         .getInt(powerConduitTierOneRF);
