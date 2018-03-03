6c67cd487242b3cc6e14b345fba24dc4c688fec4
Henry Loenwind <henry@loenwind.info>
2018-03-03 16:28:40 +0100

    Polished the anvil

--- enderio-base/src/main/java/crazypants/enderio/base/block/darksteel/anvil/GuiDarkSteelAnvil.java
+++ enderio-base/src/main/java/crazypants/enderio/base/block/darksteel/anvil/GuiDarkSteelAnvil.java
@@ -4,8 +4,6 @@ import net.minecraft.client.gui.GuiRepair;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.world.World;
 
-//This class is almost a complete copy / paste of GuiRepair with the container changed to ContainerDarkSteelAnvil and the 
-//hard coded check for a max anvil level of 40 replaced to a call to EnderCoreMethods
 public class GuiDarkSteelAnvil extends GuiRepair {
 
   public GuiDarkSteelAnvil(InventoryPlayer inventoryIn, World worldIn) {
