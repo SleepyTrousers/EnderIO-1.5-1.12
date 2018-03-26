7f51fec85f0107c5597ce48210b425c67e0be582
Ryan Woodcock <epicsquid315@gmail.com>
2018-03-27 01:02:08 +1100

    Increased Wired Charger speed
    Now is percentage based (however it will not override the item's max input rate)

--- enderio-base/src/main/java/crazypants/enderio/base/capacitor/Scaler.java
+++ enderio-base/src/main/java/crazypants/enderio/base/capacitor/Scaler.java
@@ -98,6 +98,7 @@ public interface Scaler {
     }),
     OCTADIC_1_8(new IndexedScaler(.5f, 0, .5f, 1, 3, 2, 4, 8, 10, 16)),
     POWER(new IndexedScaler(1f, 0, 1, 3, 5, 8, 13)),
+    CHARGE(new IndexedScaler(1f, 100, 60, 20, 10, 1)),
     SPEED(new IndexedScaler(1f, 100, 20, 10, 2, 1)),
     POWER10(new IndexedScaler(1f, 0, 1, 2, 10, 20)),
     RANGE(new IndexedScaler(1f, 0, 4, 6, 10, 17)),
