7889449cbf4682a1615d83cd41bba1829b4395e3
Henry Loenwind <henry@loenwind.info>
2018-05-14 11:07:24 +0200

    Fixed XP calculations. Again. Maybe. Or made it worse. Again. Probably.

--- enderio-base/src/main/java/crazypants/enderio/base/xp/XpUtil.java
+++ enderio-base/src/main/java/crazypants/enderio/base/xp/XpUtil.java
@@ -2,6 +2,7 @@ package crazypants.enderio.base.xp;
 
 import javax.annotation.Nonnull;
 
+import crazypants.enderio.base.Log;
 import net.minecraft.entity.player.EntityPlayer;
 
 /**
@@ -15,25 +16,48 @@ public class XpUtil {
   // Values taken from OpenBlocks to ensure compatibility
   public static final int RATIO = 20;
 
+  /**
+   * Converts the given fluid amount into experience points.
+   * <p>
+   * Note that this calculation has a remainder!
+   * 
+   * @param liquid
+   *          fluid amount (in mB)
+   * @return experience
+   */
   public static int liquidToExperience(int liquid) {
     return liquid / RATIO;
   }
 
+  /**
+   * Converts the given experience into fluid.
+   * <p>
+   * This conversion is lossless.
+   * 
+   * @param xp
+   *          experience
+   * @return fluid amount (in mB)
+   */
   public static int experienceToLiquid(int xp) {
     return xp * RATIO;
   }
 
-  public static int getLiquidForLevel(int level) {
-    return experienceToLiquid(getExperienceForLevel(level));
-  }
+  /**
+   * The highest level that can be converted into experience points that can be stored as an {@link Integer}.
+   */
+  private static final int MAX_LEVEL = 21862;
+  private static final int[] xpmap = new int[MAX_LEVEL + 1];
 
-  private static final int[] xpmap = new int[21863];
-
+  /**
+   * 
+   * @param level
+   * @return The amount of XP a player needs to get from level 0 to the given level
+   */
   public static int getExperienceForLevel(int level) {
     if (level <= 0) {
       return 0;
     }
-    if (level >= 21863) {
+    if (level > MAX_LEVEL) {
       return Integer.MAX_VALUE;
     }
     return xpmap[level];
@@ -41,35 +65,35 @@ public class XpUtil {
 
   static {
     int res = 0;
-    for (int i = 0; i < 21863; i++) {
-      res += getXpBarCapacity(i);
+    for (int i = 0; i <= MAX_LEVEL; i++) {
       if (res < 0) {
         res = Integer.MAX_VALUE;
+        Log.error("Internal XP calculation is wrong. Level " + i + " already maxes out.");
       }
       xpmap[i] = res;
+      res += getXpBarCapacity(i);
     }
-  }
-
-  public static int getXpBarCapacity(int level) {
-    if (level == 0) {
-      return 0;
-    } else if (level <= 15) {
-      return xpMath(level, 7, 2);
-    } else if (level <= 30) {
-      return 315 + xpMath(level - 15, 37, 5);
-    }
-    return 1395 + xpMath(level - 30, 112, 9);
   }
 
   /**
-   * Used to calculate the amount of XP required to level up. Based on OpenBlocks sum method
+   * See {@link net.minecraft.entity.player.EntityPlayer#xpBarCap()}
    * 
-   * @return the capacity of the XP bar
+   * @param level
+   * @return The amount of XP a player at the given level needs to get to the <em>next</em> level
    */
-  private static int xpMath(int n, int a, int d) {
-    return n * (2 * a + (n - 1) * d) / 2;
+  public static int getXpBarCapacity(int level) {
+    if (level >= 30) {
+      return 112 + (level - 30) * 9;
+    } else {
+      return level >= 15 ? 37 + (level - 15) * 5 : 7 + level * 2;
+    }
   }
 
+  /**
+   * 
+   * @param experience
+   * @return The level a player with the given experience has.
+   */
   public static int getLevelForExperience(int experience) {
     for (int i = 1; i < xpmap.length; i++) {
       if (xpmap[i] > experience) {
@@ -79,6 +103,11 @@ public class XpUtil {
     return xpmap.length;
   }
 
+  /**
+   * 
+   * @param player
+   * @return The total amount of experience (from both their level and their xp bar) the given player has
+   */
   public static int getPlayerXP(@Nonnull EntityPlayer player) {
     return (int) (getExperienceForLevel(player.experienceLevel) + (player.experience * player.xpBarCap()));
   }
@@ -88,7 +117,7 @@ public class XpUtil {
     player.experienceTotal = experience;
     player.experienceLevel = getLevelForExperience(experience);
     int expForLevel = getExperienceForLevel(player.experienceLevel);
-    player.experience = (float) (experience - expForLevel) / (float) player.xpBarCap();
+    player.experience = (float) (experience - expForLevel) / (float) getXpBarCapacity(player.experienceLevel);
   }
 
 }
