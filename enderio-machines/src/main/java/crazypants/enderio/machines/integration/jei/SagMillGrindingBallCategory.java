ebefccb3700404989e25beb1d11e5092b181254f
David Vierra <codewarrior@hawaii.rr.com>
2018-05-17 15:00:30 -1000

    Add animated ball durability bars w/tooltips

--- enderio-machines/src/main/java/crazypants/enderio/machines/integration/jei/SagMillGrindingBallCategory.java
+++ enderio-machines/src/main/java/crazypants/enderio/machines/integration/jei/SagMillGrindingBallCategory.java
@@ -2,6 +2,7 @@ package crazypants.enderio.machines.integration.jei;
 
 import crazypants.enderio.base.EnderIO;
 import crazypants.enderio.base.lang.Lang;
+import crazypants.enderio.base.lang.LangPower;
 import crazypants.enderio.base.recipe.sagmill.GrindingBall;
 import crazypants.enderio.base.recipe.sagmill.SagMillRecipeManager;
 import crazypants.enderio.machines.EnderIOMachines;
@@ -10,15 +11,19 @@ import mezz.jei.api.IModRegistry;
 import mezz.jei.api.gui.IDrawable;
 import mezz.jei.api.gui.IGuiItemStackGroup;
 import mezz.jei.api.gui.IRecipeLayout;
+import mezz.jei.api.gui.ITickTimer;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.BlankRecipeCategory;
 import mezz.jei.api.recipe.BlankRecipeWrapper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
 import javax.annotation.Nonnull;
+
+import java.util.Collections;
 
 import static crazypants.enderio.machines.init.MachineObject.block_sag_mill;
 
@@ -30,6 +35,8 @@ public class SagMillGrindingBallCategory extends BlankRecipeCategory<SagMillGrin
 
     public static class GrindingBallWrapper extends BlankRecipeWrapper {
         private final GrindingBall ball;
+
+        public ITickTimer timer;
 
         public GrindingBallWrapper(GrindingBall ball) {
             this.ball = ball;
@@ -54,6 +61,14 @@ public class SagMillGrindingBallCategory extends BlankRecipeCategory<SagMillGrin
             fr.drawString(Lang.GRINDING_BALL_2.get("", toPercent(ball.getGrindingMultiplier())), 48, 4, 0x404040);
             fr.drawString(Lang.GRINDING_BALL_3.get("", toPercent(ball.getChanceMultiplier())), 48, 16, 0x404040);
             fr.drawString(Lang.GRINDING_BALL_4.get("", toPercent(ball.getPowerMultiplier())), 48, 28, 0x404040);
+
+            int dura = ball.getDurability();
+            int usedDura = (timer.getValue() * 400) % dura;
+            int barH = (16 * usedDura) / dura;
+
+            int barColor = 0xFFCA2F21;
+
+            Gui.drawRect(37, 12 + barH, 41, 28, barColor);
         }
 
         @Override
@@ -68,6 +83,8 @@ public class SagMillGrindingBallCategory extends BlankRecipeCategory<SagMillGrin
                     return crazypants.enderio.machines.lang.Lang.JEI_GRINDING_BALL_POWER.getLines();
                 }
 
+            } else if (mouseX >= 36 && mouseX <= 41 && mouseY >= 12 && mouseY <= 29) {
+                return Collections.singletonList(LangPower.RF(ball.getDurability()));
             }
             return super.getTooltipStrings(mouseX, mouseY);
         }
@@ -85,10 +102,12 @@ public class SagMillGrindingBallCategory extends BlankRecipeCategory<SagMillGrin
 
     @Nonnull
     private final IDrawable background;
+    private final ITickTimer timer;
 
     public SagMillGrindingBallCategory(IGuiHelper guiHelper) {
         ResourceLocation backgroundLocation = new ResourceLocation(EnderIO.DOMAIN, "textures/gui/jei.grinding_ball.png");
         background = guiHelper.createDrawable(backgroundLocation, 0, 0, 146, 40, 146, 40);
+        timer = guiHelper.createTickTimer(20000, 20000, false);
     }
 
     @Override
@@ -113,6 +132,8 @@ public class SagMillGrindingBallCategory extends BlankRecipeCategory<SagMillGrin
 
         guiItemStacks.init(0, true, 16, 11);
         guiItemStacks.set(ingredients);
+
+        recipeWrapper.timer = timer;
     }
 
     @Override
