cc6a7bc40f169be754250e684ab1315598906664
EpicSquid <epicsquid315@gmail.com>
2018-06-27 15:35:48 +1000

    Tweaked ender generator balance

--- enderio-machines/src/main/java/crazypants/enderio/machines/config/config/EnderGenConfig.java
+++ enderio-machines/src/main/java/crazypants/enderio/machines/config/config/EnderGenConfig.java
@@ -8,7 +8,7 @@ public final class EnderGenConfig {
 
   public static final IValueFactory F = Config.F.section("generator.ender");
 
-  public static final IValue<Integer> ticksPerBucketOfFuel = F.make("ticksPerBucketOfFuel", 10 * 360 * 20, //
+  public static final IValue<Integer> ticksPerBucketOfFuel = F.make("ticksPerBucketOfFuel", 10 * 400 * 20, //
       "The number of ticks one bucket of fuel lasts.").setMin(1).sync();
   public static final IValue<Float> minimumTankLevel = F.make("minimumTankLevel", 0.7f, //
       "How full does the tank need to be for the enderman head to produce energy?. (0.0-0.9995)").setRange(0, 0.9995).sync();
