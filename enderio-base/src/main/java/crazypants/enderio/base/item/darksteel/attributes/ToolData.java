99a6d8c5a8e77706c280b76711119195b2f6b6dd
EpicSquid <epicsquid315@gmail.com>
2018-06-29 02:12:44 +1000

    Buffed End steel pick and axe

--- enderio-base/src/main/java/crazypants/enderio/base/item/darksteel/attributes/ToolData.java
+++ enderio-base/src/main/java/crazypants/enderio/base/item/darksteel/attributes/ToolData.java
@@ -15,6 +15,6 @@ public class ToolData {
   // 3.0001f = more desirable for mobs (i.e. they'll pick it up even if they already have diamond)
 
   public static final @Nonnull ToolMaterial MATERIAL_END_STEEL = NullHelper
-      .notnull(EnumHelper.addToolMaterial("endSteel", Config.endSteelPickMinesTiCArdite ? 5 : 3, 2000, 10, 5f, 30), "failed to add tool material end steel");
+      .notnull(EnumHelper.addToolMaterial("endSteel", Config.endSteelPickMinesTiCArdite ? 5 : 3, 2000, 12, 5f, 30), "failed to add tool material end steel");
 
 }
