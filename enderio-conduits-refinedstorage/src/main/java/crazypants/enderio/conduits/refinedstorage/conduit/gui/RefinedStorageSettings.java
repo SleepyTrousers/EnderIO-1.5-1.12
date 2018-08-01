b6d85929f4d411a81f48680eed564b4314739b2c
EpicSquid <epicsquid315@gmail.com>
2018-08-01 15:07:14 +1000

    Made the RS gui significantly more informative

--- enderio-conduits-refinedstorage/src/main/java/crazypants/enderio/conduits/refinedstorage/conduit/gui/RefinedStorageSettings.java
+++ enderio-conduits-refinedstorage/src/main/java/crazypants/enderio/conduits/refinedstorage/conduit/gui/RefinedStorageSettings.java
@@ -1,13 +1,22 @@
 package crazypants.enderio.conduits.refinedstorage.conduit.gui;
 
+import java.awt.Rectangle;
+
 import javax.annotation.Nonnull;
+
+import com.enderio.core.client.gui.widget.GuiToolTip;
+import com.enderio.core.common.util.NNList;
 
 import crazypants.enderio.base.conduit.IClientConduit;
 import crazypants.enderio.base.conduit.IGuiExternalConnection;
 import crazypants.enderio.base.gui.IconEIO;
+import crazypants.enderio.base.init.ModObject;
 import crazypants.enderio.conduits.gui.BaseSettingsPanel;
+import crazypants.enderio.conduits.init.ConduitObject;
 import crazypants.enderio.conduits.refinedstorage.conduit.IRefinedStorageConduit;
 import crazypants.enderio.conduits.refinedstorage.init.ConduitRefinedStorageObject;
+import crazypants.enderio.conduits.refinedstorage.lang.Lang;
+import net.minecraft.item.ItemStack;
 
 public class RefinedStorageSettings extends BaseSettingsPanel {
 
@@ -17,6 +26,22 @@ public class RefinedStorageSettings extends BaseSettingsPanel {
     super(IconEIO.WRENCH_OVERLAY_RS, ConduitRefinedStorageObject.item_refined_storage_conduit.getUnlocalisedName(), gui, con, "filter_upgrade_settings");
 
     rsCon = (IRefinedStorageConduit) con;
+
+    filterExtractUpgradeTooltip = new GuiToolTip(new Rectangle(rightColumn, 70, 18, 18), Lang.GUI_RS_FILTER_UPGRADE_OUT.get(),
+        Lang.GUI_RS_FILTER_UPGRADE_OUT_2.get(), Lang.GUI_RS_FILTER_UPGRADE_OUT_3.get()) {
+      @Override
+      public boolean shouldDraw() {
+        return !gui.getContainer().hasFilter(false) && super.shouldDraw();
+      }
+    };
+
+    filterInsertUpgradeTooltip = new GuiToolTip(new Rectangle(leftColumn, 70, 18, 18), Lang.GUI_RS_FILTER_UPGRADE_IN.get(),
+        Lang.GUI_RS_FILTER_UPGRADE_IN_2.get(), Lang.GUI_RS_FILTER_UPGRADE_IN_3.get()) {
+      @Override
+      public boolean shouldDraw() {
+        return !gui.getContainer().hasFilter(true) && super.shouldDraw();
+      }
+    };
   }
 
   @Override
@@ -37,6 +62,13 @@ public class RefinedStorageSettings extends BaseSettingsPanel {
   @Override
   protected void initCustomOptions() {
     gui.getContainer().setInOutSlotsVisible(true, true, rsCon);
+
+    NNList<ItemStack> filtersAll = new NNList<>(new ItemStack(ModObject.itemBasicItemFilter.getItemNN()), new ItemStack(ModObject.itemFluidFilter.getItemNN()));
+    NNList<ItemStack> upgrades = new NNList<>(new ItemStack(ConduitObject.item_extract_speed_upgrade.getItemNN()),
+        new ItemStack(ConduitObject.item_extract_speed_downgrade.getItemNN()), new ItemStack(ConduitRefinedStorageObject.item_rs_crafting_upgrade.getItemNN()),
+        new ItemStack(ConduitRefinedStorageObject.item_rs_crafting_speed_upgrade.getItemNN()),
+        new ItemStack(ConduitRefinedStorageObject.item_rs_crafting_speed_downgrade.getItemNN()));
+    gui.getContainer().createGhostSlots(gui.getGhostSlotHandler().getGhostSlots(), filtersAll, filtersAll, upgrades);
 
     filtersChanged();
   }
