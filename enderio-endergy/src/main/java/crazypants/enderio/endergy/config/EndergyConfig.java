e5e4eababbe64f9d957b7d1a596a665e7bba94d1
EpicSquid <epicsquid315@gmail.com>
2018-08-08 22:50:36 +1000

    Changed some values around for Endergy

--- enderio-endergy/src/main/java/crazypants/enderio/endergy/config/EndergyConfig.java
+++ enderio-endergy/src/main/java/crazypants/enderio/endergy/config/EndergyConfig.java
@@ -14,15 +14,15 @@ public final class EndergyConfig {
   public static final IValueFactory FE = F.section(".energy");
 
   public static final NNList<IValue<Integer>> maxIO = new NNList<>( //
-      FE.make("tier1_maxIO", 40, "The maximum IO for the cobble endergy conduit.").setRange(1, MAXIO).sync(),
-      FE.make("tier2_maxIO", 80, "The maximum IO for the iron endergy conduit.").setRange(1, MAXIO).sync(),
-      FE.make("tier3_maxIO", 160, "The maximum IO for the gold endergy conduit.").setRange(1, MAXIO).sync(),
-      FE.make("tier4_maxIO", 320, "The maximum IO for the silver endergy conduit.").setRange(1, MAXIO).sync(),
+      FE.make("tier1_maxIO", 20, "The maximum IO for the cobble endergy conduit.").setRange(1, MAXIO).sync(),
+      FE.make("tier2_maxIO", 40, "The maximum IO for the iron endergy conduit.").setRange(1, MAXIO).sync(),
+      FE.make("tier3_maxIO", 80, "The maximum IO for the gold endergy conduit.").setRange(1, MAXIO).sync(),
+      FE.make("tier4_maxIO", 160, "The maximum IO for the silver endergy conduit.").setRange(1, MAXIO).sync(),
+      FE.make("tier5_maxIO", 320, "The maximum IO for the electrum endergy conduit.").setRange(1, MAXIO).sync(),
       // normal tier 1 here, 640
-      FE.make("tier5_maxIO", 1280, "The maximum IO for the electrum endergy conduit.").setRange(1, MAXIO).sync(),
-      FE.make("tier6_maxIO", 2560, "The maximum IO for the aluminium endergy conduit.").setRange(1, MAXIO).sync(),
+      FE.make("tier6_maxIO", 1280, "The maximum IO for the aluminium endergy conduit.").setRange(1, MAXIO).sync(),
+      FE.make("tier7_maxIO", 2560, "The maximum IO for the copper endergy conduit.").setRange(1, MAXIO).sync(),
       // normal tier 2 here, 5120
-      FE.make("tier7_maxIO", 10240, "The maximum IO for the copper endergy conduit.").setRange(1, MAXIO).sync(),
       // normal tier 3 here, 20480
       FE.make("tier8_maxIO", 40960, "The maximum IO for the crystalline endergy conduit.").setRange(1, MAXIO).sync(),
       FE.make("tier9_maxIO", 256000, "The maximum IO for the melodic endergy conduit.").setRange(1, MAXIO).sync(),
