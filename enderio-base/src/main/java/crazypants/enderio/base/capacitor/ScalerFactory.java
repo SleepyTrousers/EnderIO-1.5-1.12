d2250f193e2017e3b74bf23d704ee2851b12eefa
Henry Loenwind <henry@loenwind.info>
2018-08-14 11:38:38 +0200

    Delay error message creation

--- enderio-base/src/main/java/crazypants/enderio/base/capacitor/ScalerFactory.java
+++ enderio-base/src/main/java/crazypants/enderio/base/capacitor/ScalerFactory.java
@@ -34,6 +34,7 @@ public enum ScalerFactory implements Scaler {
     @Override
     public float scaleValue(float idx) {
       return (float) Math.pow(2, idx - 1);
+      // replace with identity
     }
   }),
   QUADRATIC_1_8(new Scaler() { // 1-2-4-8-8-8
@@ -46,14 +47,33 @@ public enum ScalerFactory implements Scaler {
     @Override
     public float scaleValue(float idx) {
       return (float) Math.pow(3, idx - 1);
+      // ab^x * x^c
+      // a = 0.9999999999999999
+      // b = 1
+      // c = 1.584962500721159
     }
   }),
   OCTADIC_1_8(new IndexedScaler(.5f, 0, .5f, 1, 3, 2, 4, 8, 10, 16)),
   POWER(new IndexedScaler(1f, 0, 1, 3, 5, 8, 13, 18)),
+  // indexed 0, 1, 3, 4, 5, 6.5, 8, 10.5, 13, 15.5, 18
+  // better:
+  // a + bx + cx^2 + dx^3 + ex^4
+  // a -2.86111111110643E+00
+  // b 5.16712962962717E+00
+  // c -1.51296296296188E+00
+  // d 2.16550925925753E-01
+  // e -9.60648148147314E-03
+
   CHARGE(new IndexedScaler(1f, 1000, 100, 60, 20, 10, 1)),
+  // ( a + bx ) / ( 1 + cx + dx^2 )
+  // a 9.99987140430512E+02
+  // b -9.78706412647100E+01
+  // c 8.36064838806583E+00
+  // d -5.96021930516349E-01
   SPEED(new IndexedScaler(1f, 100, 20, 10, 2, 1)),
   POWER10(new IndexedScaler(1f, 0, 1, 2, 10, 20, 40)),
   RANGE(new IndexedScaler(1f, 0, 4, 6, 10, 17, 24)),
+  // 2 + 2x
   FIXED(new Scaler() { // 1-1-1
     @Override
     public float scaleValue(float idx) {
@@ -61,6 +81,12 @@ public enum ScalerFactory implements Scaler {
     }
   }),
   SPAWNER(new IndexedScaler(1f, 0, 1, 5, 10, 20, 40)),
+  // a + bx + cx^2 + dx^3 + ex^4
+  // a -6.62857142856527E+00
+  // b 1.02380952380926E+01
+  // c -3.04166666666589E+00
+  // d 4.49404761904681E-01
+  // e -1.72619047619040E-02
   BURNTIME(new IndexedScaler(1f, 0.8f, 1f, 1.25f, 1.5f, 1.5f, 2f, 2.5f) {
     @Override
     public float scaleValue(float idx) {
