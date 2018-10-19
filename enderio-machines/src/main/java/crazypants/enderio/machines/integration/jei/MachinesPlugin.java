72d7e592d5a90403600002bb4a791704f1058ba6
Henry Loenwind <henry@loenwind.info>
2018-10-19 04:39:19 +0200

    Added bar steps for the JEI plugins

--- enderio-machines/src/main/java/crazypants/enderio/machines/integration/jei/MachinesPlugin.java
+++ enderio-machines/src/main/java/crazypants/enderio/machines/integration/jei/MachinesPlugin.java
@@ -9,6 +9,7 @@ import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.JEIPlugin;
 import mezz.jei.api.recipe.IRecipeCategoryRegistration;
+import net.minecraftforge.fml.common.ProgressManager;
 
 @JEIPlugin
 public class MachinesPlugin implements IModPlugin {
@@ -21,25 +22,45 @@ public class MachinesPlugin implements IModPlugin {
 
   @Override
   public void register(@Nonnull IModRegistry registry) {
+    ProgressManager.ProgressBar bar = ProgressManager.push("Ender IO Machines", 17, true);
 
     IGuiHelper guiHelper = registry.getJeiHelpers().getGuiHelper();
 
+    bar.step("Alloy Smelter");
     AlloyRecipeCategory.register(registry, guiHelper);
+    bar.step("Combustion Generator");
     CombustionRecipeCategory.register(registry, guiHelper);
+    bar.step("Crafter");
     CrafterRecipeTransferHandler.register(registry);
+    bar.step("Enchanter");
     EnchanterRecipeCategory.register(registry, guiHelper);
+    bar.step("Painter");
     PainterRecipeCategory.register(registry);
+    bar.step("Sag Mill");
     SagMillRecipeCategory.register(registry, guiHelper);
+    bar.step("Grinding Balls");
     SagMillGrindingBallCategory.register(registry, guiHelper);
+    bar.step("Slice'n'Splice");
     SliceAndSpliceRecipeCategory.register(registry, guiHelper);
+    bar.step("Solar Panels");
     SolarPanelRecipeCategory.register(registry, guiHelper);
+    bar.step("Soul Binder");
     SoulBinderRecipeCategory.register(registry, guiHelper);
+    bar.step("Stirling Generator");
     StirlingRecipeCategory.register(registry, guiHelper);
+    bar.step("Tank");
     TankRecipeCategory.register(registry, guiHelper);
+    bar.step("The Vat");
     VatRecipeCategory.register(registry, guiHelper);
+    bar.step("Wired Charger");
     WiredChargerRecipeCategory.register(registry, guiHelper);
+    bar.step("Weather Obelisk");
     WeatherObeliskRecipeCategory.register(registry, guiHelper);
+    bar.step("Zombie Generator");
     ZombieGeneratorRecipeCategory.register(registry, guiHelper);
+    bar.step("Ender Generator");
     EnderGeneratorRecipeCategory.register(registry, guiHelper);
+
+    ProgressManager.pop(bar);
   }
 }
