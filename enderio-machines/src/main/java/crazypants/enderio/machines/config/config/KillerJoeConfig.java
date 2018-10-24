572ed0c5ef67e5718c99253155e6c7091a33a942
Henry Loenwind <henry@loenwind.info>
2018-10-24 14:36:59 +0200

    Updated descriptions of Killer Joe config settings

--- enderio-machines/src/main/java/crazypants/enderio/machines/config/config/KillerJoeConfig.java
+++ enderio-machines/src/main/java/crazypants/enderio/machines/config/config/KillerJoeConfig.java
@@ -15,11 +15,11 @@ public final class KillerJoeConfig {
   public static final IValue<Double> killerJoeAttackLength = F.make("killerJoeAttackLength", 4.0, //
       "The reach of attacks in front of Joe.").setRange(1, 32).sync();
   public static final IValue<Double> killerJoeHooverXpHeight = F.make("killerJoeHooverXpHeight", 2.0, //
-      "The distance from which XP will be gathered above and below Joe.").setRange(1, 32).sync();
+      "The distance from which XP will be gathered above and below Joe. (only used when killerMendingEnabled is enabled)").setRange(1, 32).sync();
   public static final IValue<Double> killerJoeHooverXpWidth = F.make("killerJoeHooverXpWidth", 5.0, //
-      "The distance from which XP will be gathered to each side of Joe.").setRange(1, 32).sync();
+      "The distance from which XP will be gathered to each side of Joe. (only used when killerMendingEnabled is enabled)").setRange(1, 32).sync();
   public static final IValue<Double> killerJoeHooverXpLength = F.make("killerJoeHooverXpLength", 10.0, //
-      "The distance from which XP will be gathered in front of Joe.").setRange(1, 32).sync();
+      "The distance from which XP will be gathered in front of Joe. (only used when killerMendingEnabled is enabled)").setRange(1, 32).sync();
   public static final IValue<Boolean> killerJoeMustSee = F.make("killerJoeMustSee", false, //
       "Set whether the Killer Joe can attack through blocks.").sync();
   public static final IValue<Boolean> killerPvPoffDisablesSwing = F.make("killerPvPoffDisablesSwing", false, //
@@ -27,7 +27,7 @@ public final class KillerJoeConfig {
   public static final IValue<Boolean> killerPvPoffIsIgnored = F.make("killerPvPoffIsIgnored", false, //
       "Set whether the Killer Joe ignores PvP settings and always hits players (killerPvPoffDisablesSwing must be off for this to work).").sync();
   public static final IValue<Boolean> killerMendingEnabled = F.make("killerMendingEnabled", true, //
-      "If enabled, picked up XP will be used for the enchantement 'Mending' on the weapon.").sync();
+      "If enabled, the Killer Joe will pick up XP for the enchantement 'Mending' on the weapon.").sync();
   public static final IValue<Integer> killerJoeNutrientUsePerAttackMb = F.make("killerJoeNutrientUsePerAttackMb", 5, //
       "The number of millibuckets of nutrient fluid used per attack.").setMin(1).sync();
   public static final IValue<Boolean> killerProvokesCreeperExpolosions = F.make("killerProvokesCreeperExpolosions", false, //
