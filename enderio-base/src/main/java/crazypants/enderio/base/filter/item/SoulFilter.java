1cc3757796f5657712370a3301689caf41666d04
Henry Loenwind <henry@loenwind.info>
2018-11-01 14:22:06 +0100

    First draft for soul filter

---
+++ enderio-base/src/main/java/crazypants/enderio/base/filter/item/SoulFilter.java
@@ -0,0 +1,169 @@
+package crazypants.enderio.base.filter.item;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.enderio.core.client.gui.widget.GhostSlot;
+import com.enderio.core.common.network.NetworkUtil;
+import com.enderio.core.common.util.NNList;
+
+import crazypants.enderio.base.init.ModObject;
+import crazypants.enderio.util.CapturedMob;
+import crazypants.enderio.util.Prep;
+import info.loenwind.autosave.Reader;
+import info.loenwind.autosave.Writer;
+import info.loenwind.autosave.annotations.Storable;
+import info.loenwind.autosave.annotations.Store;
+import io.netty.buffer.ByteBuf;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.items.IItemHandler;
+
+@Storable
+public class SoulFilter implements IItemFilter.WithGhostSlots {
+
+  public static final int GUI_COLUMNS = 9;
+
+  @Store
+  private @Nonnull NNList<CapturedMob> souls = new NNList<>();
+  @Store
+  private boolean blacklist = false;
+  @Store
+  private boolean sticky = false;
+  @Store
+  private int slotCount = 9;
+
+  @Override
+  public boolean isValid() {
+    return !isEmpty();
+  }
+
+  @Override
+  public int getSlotCount() {
+    return slotCount;
+  }
+
+  @Override
+  public boolean isSticky() {
+    return sticky;
+  }
+
+  public boolean isBlacklist() {
+    return blacklist;
+  }
+
+  public void setBlacklist(boolean blacklist) {
+    this.blacklist = blacklist;
+  }
+
+  public void setSticky(boolean sticky) {
+    this.sticky = sticky;
+  }
+
+  public void setSlotCount(int slotCount) {
+    this.slotCount = slotCount;
+  }
+
+  @Override
+  public void readFromNBT(@Nonnull NBTTagCompound nbtRoot) {
+    Reader.read(nbtRoot, this);
+  }
+
+  @Override
+  public void writeToNBT(@Nonnull NBTTagCompound nbtRoot) {
+    Writer.write(nbtRoot, this);
+  }
+
+  @Override
+  public void writeToByteBuf(@Nonnull ByteBuf buf) {
+    NBTTagCompound tag = new NBTTagCompound();
+    writeToNBT(tag);
+    NetworkUtil.writeNBTTagCompound(tag, buf);
+  }
+
+  @Override
+  public void readFromByteBuf(@Nonnull ByteBuf buf) {
+    readFromNBT(NetworkUtil.readNBTTagCompound(buf));
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return souls.isEmpty();
+  }
+
+  @Override
+  public boolean doesItemPassFilter(@Nullable IItemHandler inv, @Nonnull ItemStack item) {
+    if (CapturedMob.containsSoul(item)) {
+      CapturedMob input = CapturedMob.create(item);
+      for (CapturedMob mob : souls) {
+        if (mob.isSameType(input)) {
+          return !blacklist;
+        }
+      }
+    }
+    return blacklist;
+  }
+
+  @Override
+  @Nonnull
+  public ItemStack getInventorySlotContents(int slot) {
+    if (slot < souls.size()) {
+      return souls.get(slot).toGenericStack(ModObject.itemSoulVial.getItemNN(), 1, 1);
+    }
+    return Prep.getEmpty();
+  }
+
+  @Override
+  public void createGhostSlots(@Nonnull NNList<GhostSlot> slots, int xOffset, int yOffset, @Nullable Runnable cb) {
+    int row = 0, col = 0;
+    for (int i = 0; i < slotCount; i++) {
+      int x = xOffset + col * 18;
+      int y = yOffset + row * 10;
+
+      slots.add(new SoulFilterGhostSlot(i, x, y, cb));
+
+      col++;
+      if (col >= GUI_COLUMNS) {
+        col = 0;
+        row++;
+      }
+    }
+  }
+
+  class SoulFilterGhostSlot extends GhostSlot {
+    private final Runnable cb;
+
+    SoulFilterGhostSlot(int slot, int x, int y, Runnable cb) {
+      this.setX(x);
+      this.setY(y);
+      this.setSlot(slot);
+      this.cb = cb;
+      this.setDisplayStdOverlay(false);
+      this.setStackSizeLimit(1);
+    }
+
+    @Override
+    public void putStack(@Nonnull ItemStack stack, int realsize) {
+      CapturedMob input = null;
+      if (CapturedMob.containsSoul(stack)) {
+        input = CapturedMob.create(stack);
+      }
+      if (input != null) {
+        if (getSlot() < souls.size()) {
+          souls.set(getSlot(), input);
+        } else {
+          souls.add(input);
+        }
+      } else if (getSlot() < souls.size()) {
+        souls.remove(getSlot());
+      }
+      cb.run();
+    }
+
+    @Override
+    public @Nonnull ItemStack getStack() {
+      return getInventorySlotContents(getSlot());
+    }
+  }
+
+}
---
+++ enderio-base/src/main/java/crazypants/enderio/base/filter/item/items/ItemSoulFilter.java
@@ -0,0 +1,114 @@
+package crazypants.enderio.base.filter.item.items;
+
+import java.util.List;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import com.enderio.core.api.client.gui.IResourceTooltipProvider;
+import com.enderio.core.client.handlers.SpecialTooltipHandler;
+import com.enderio.core.common.TileEntityBase;
+
+import crazypants.enderio.api.IModObject;
+import crazypants.enderio.base.EnderIOTab;
+import crazypants.enderio.base.filter.FilterRegistry;
+import crazypants.enderio.base.filter.IFilterContainer;
+import crazypants.enderio.base.filter.gui.BasicItemFilterGui;
+import crazypants.enderio.base.filter.gui.ContainerFilter;
+import crazypants.enderio.base.filter.item.IItemFilter;
+import crazypants.enderio.base.filter.item.SoulFilter;
+import crazypants.enderio.base.init.ModObject;
+import crazypants.enderio.base.lang.Lang;
+import crazypants.enderio.util.NbtValue;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class ItemSoulFilter extends Item implements IItemFilterItemUpgrade, IResourceTooltipProvider {
+
+  private final int size;
+
+  public static ItemSoulFilter createNormal(@Nonnull IModObject modObject) {
+    return new ItemSoulFilter(modObject, 1 * 9);
+  }
+
+  public static ItemSoulFilter createBig(@Nonnull IModObject modObject) {
+    return new ItemSoulFilter(modObject, 3 * 9);
+  }
+
+  protected ItemSoulFilter(@Nonnull IModObject modObject, int size) {
+    setCreativeTab(EnderIOTab.tabEnderIOItems);
+    modObject.apply(this);
+    setMaxDamage(0);
+    setHasSubtypes(true);
+    setMaxStackSize(64);
+    this.size = size;
+  }
+
+  @Override
+  public IItemFilter createFilterFromStack(@Nonnull ItemStack stack) {
+    SoulFilter filter = new SoulFilter();
+    filter.setSlotCount(size);
+    NBTTagCompound tag = NbtValue.FILTER.getTag(stack);
+    if (!tag.hasNoTags()) {
+      filter.readFromNBT(tag);
+    }
+    return filter;
+  }
+
+  @Override
+  @Nonnull
+  public String getUnlocalizedNameForTooltip(@Nonnull ItemStack itemStack) {
+    return getUnlocalizedName();
+  }
+
+  @Override
+  @Nonnull
+  public ActionResult<ItemStack> onItemRightClick(@Nonnull World world, @Nonnull EntityPlayer player, @Nonnull EnumHand hand) {
+    if (!world.isRemote && player.isSneaking()) {
+      ModObject.itemBasicItemFilter.openGui(world, player.getPosition(), player, null, hand.ordinal());// TODO
+      return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, player.getHeldItem(hand));
+    }
+    return super.onItemRightClick(world, player, hand);
+  }
+
+  @Override
+  @SideOnly(Side.CLIENT)
+  public void addInformation(@Nonnull ItemStack stack, @Nullable World worldIn, @Nonnull List<String> tooltip, @Nonnull ITooltipFlag flagIn) {
+    super.addInformation(stack, worldIn, tooltip, flagIn);
+    if (FilterRegistry.isFilterSet(stack)) {
+      if (SpecialTooltipHandler.showAdvancedTooltips()) {
+        tooltip.add(Lang.ITEM_FILTER_CONFIGURED.get(TextFormatting.ITALIC));
+        tooltip.add(Lang.ITEM_FILTER_CLEAR.get(TextFormatting.ITALIC));
+      }
+    }
+  }
+
+  @Override
+  @Nullable
+  @SideOnly(Side.CLIENT)
+  public GuiScreen getClientGuiElement(@Nonnull EntityPlayer player, @Nonnull World world, @Nonnull BlockPos pos, @Nullable EnumFacing facing, int param1) {
+    Container container = player.openContainer;
+    if (container instanceof IFilterContainer) {
+      return new BasicItemFilterGui(player.inventory, new ContainerFilter(player, (TileEntityBase) world.getTileEntity(pos), facing, param1), // TODO
+          world.getTileEntity(pos), ((IFilterContainer<IItemFilter>) container).getFilter(param1));
+    } else {
+      return new BasicItemFilterGui(player.inventory, new ContainerFilter(player, null, facing, param1), null, // TODO
+          FilterRegistry.getFilterForUpgrade(player.getHeldItem(EnumHand.values()[param1])));
+    }
+  }
+
+}
