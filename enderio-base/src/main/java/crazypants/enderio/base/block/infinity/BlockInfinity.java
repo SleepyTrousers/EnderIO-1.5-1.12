ea168d7f95416a61eeeff4f300584ef6e35f236f
Henry Loenwind <henry@loenwind.info>
2019-01-31 01:33:23 +0100

    Made infinity block fall and react to neighboring water

--- enderio-base/src/main/java/crazypants/enderio/base/block/infinity/BlockInfinity.java
+++ enderio-base/src/main/java/crazypants/enderio/base/block/infinity/BlockInfinity.java
@@ -7,13 +7,12 @@ import javax.annotation.Nonnull;
 import com.enderio.core.api.client.gui.IResourceTooltipProvider;
 
 import crazypants.enderio.api.IModObject;
-import crazypants.enderio.base.BlockEio;
 import crazypants.enderio.base.EnderIOTab;
-import crazypants.enderio.base.TileEntityEio;
 import crazypants.enderio.base.render.IHaveRenderers;
 import crazypants.enderio.util.CapturedMob;
 import crazypants.enderio.util.ClientUtil;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockFalling;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
@@ -37,7 +36,7 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class BlockInfinity extends BlockEio<TileEntityEio> implements IResourceTooltipProvider, IHaveRenderers {
+public class BlockInfinity extends BlockFalling implements IResourceTooltipProvider, IHaveRenderers, IModObject.WithBlockItem {
 
   public static BlockInfinity create(@Nonnull IModObject modObject) {
     BlockInfinity result = new BlockInfinity(modObject);
@@ -47,16 +46,21 @@ public class BlockInfinity extends BlockEio<TileEntityEio> implements IResourceT
   public static final @Nonnull PropertyInteger LEVEL = PropertyInteger.create("level", 0, 2);
 
   protected BlockInfinity(@Nonnull IModObject modObject) {
-    super(modObject, Material.ROCK);
+    super(Material.ROCK);
+    modObject.apply(this);
     setHardness(50.0F);
     setResistance(2000.0F);
     setSoundType(SoundType.GROUND); // SoundType.GROUND should be SoundType.GRAVEL
     setCreativeTab(EnderIOTab.tabEnderIOMaterials);
-    setShape(mkShape(BlockFaceShape.SOLID));
     initDefaultState();
     for (int i = 0; i <= 2; i++) {
       setHarvestLevel("shovel", i + 1, getDefaultState().withProperty(LEVEL, i));
     }
+  }
+
+  @Override
+  public @Nonnull BlockFaceShape getBlockFaceShape(@Nonnull IBlockAccess worldIn, @Nonnull IBlockState state, @Nonnull BlockPos pos, @Nonnull EnumFacing face) {
+    return BlockFaceShape.SOLID;
   }
 
   @Override
@@ -117,16 +121,32 @@ public class BlockInfinity extends BlockEio<TileEntityEio> implements IResourceT
 
       worldIn.spawnParticle(EnumParticleTypes.REDSTONE, x, y, z, (col >> 16 & 255) / 255d, (col >> 8 & 255) / 255d, (col & 255) / 255d);
     }
+    // adapted from super, added dust through one layer of solid block:
+    if (rand.nextInt(16) == 0) {
+      for (int i = 0; i <= 1; i++) {
+        BlockPos blockpos = pos.down(i);
+
+        if (canFallThrough(worldIn.getBlockState(blockpos))) {
+          double d0 = pos.getX() + rand.nextFloat();
+          double d1 = pos.getY() - 0.05D;
+          double d2 = pos.getZ() + rand.nextFloat();
+          worldIn.spawnParticle(EnumParticleTypes.FALLING_DUST, d0, d1, d2, 0.0D, 0.0D, 0.0D, Block.getStateId(state));
+          return;
+        }
+      }
+    }
+
+  }
+
+  @Override
+  @SideOnly(Side.CLIENT)
+  public int getDustColor(@Nonnull IBlockState state) {
+    return -COLS[RANDOM.nextInt(COLS.length)];
   }
 
   @Override
   public @Nonnull String getUnlocalizedNameForTooltip(@Nonnull ItemStack itemStack) {
     return itemStack.getItem().getUnlocalizedName(itemStack);
-  }
-
-  @Override
-  public boolean canBeWrenched() {
-    return false;
   }
 
   private boolean breakup(@Nonnull IBlockState state, @Nonnull World worldIn, @Nonnull BlockPos pos, float baseChance) {
@@ -153,6 +173,8 @@ public class BlockInfinity extends BlockEio<TileEntityEio> implements IResourceT
         CapturedMob capturedMob = CapturedMob.create(new ResourceLocation("minecraft", "endermite"));
         if (capturedMob != null) {
           capturedMob.doSpawn(worldIn, pos, EnumFacing.DOWN, false);
+        } else {
+          System.out.println("oops");
         }
       }
       return true;
@@ -171,6 +193,30 @@ public class BlockInfinity extends BlockEio<TileEntityEio> implements IResourceT
       super.onFallenUpon(worldIn, pos, entityIn, fallDistance - 3f);
     } else {
       super.onFallenUpon(worldIn, pos, entityIn, fallDistance);
+    }
+  }
+
+  @Override
+  public void neighborChanged(@Nonnull IBlockState state, @Nonnull World worldIn, @Nonnull BlockPos pos, @Nonnull Block blockIn, @Nonnull BlockPos fromPos) {
+    if (!tryTouchWater(worldIn, pos, state)) {
+      super.neighborChanged(state, worldIn, pos, blockIn, fromPos);
+    }
+  }
+
+  private boolean tryTouchWater(@Nonnull World worldIn, @Nonnull BlockPos pos, @Nonnull IBlockState state) {
+    for (EnumFacing enumfacing : EnumFacing.values()) {
+      if (enumfacing != EnumFacing.DOWN && enumfacing != null && worldIn.getBlockState(pos.offset(enumfacing)).getMaterial().isLiquid()) {
+        breakup(state, worldIn, pos, 1f);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @Override
+  public void onBlockAdded(@Nonnull World worldIn, @Nonnull BlockPos pos, @Nonnull IBlockState state) {
+    if (!tryTouchWater(worldIn, pos, state)) {
+      super.onBlockAdded(worldIn, pos, state);
     }
   }
 
