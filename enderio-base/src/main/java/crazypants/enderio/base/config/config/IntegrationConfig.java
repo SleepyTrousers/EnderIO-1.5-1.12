1178f9258dead80a22db9c6be2389d709b26f4ff
Henry Loenwind <henry@loenwind.info>
2019-04-14 16:11:07 +0200

    Added more PI configurations

--- enderio-base/src/main/java/crazypants/enderio/base/config/config/IntegrationConfig.java
+++ enderio-base/src/main/java/crazypants/enderio/base/config/config/IntegrationConfig.java
@@ -1,5 +1,7 @@
 package crazypants.enderio.base.config.config;
 
+import java.awt.Rectangle;
+
 import info.loenwind.autoconfig.factory.IValue;
 import info.loenwind.autoconfig.factory.IValueFactory;
 
@@ -26,7 +28,19 @@ public final class IntegrationConfig {
       "Enables the integration with Biomes'o'Plenty (harvesting flowers).").startup();
 
   public static final IValue<Boolean> enableProjectIntelligence = F.make("enableProjectIntelligence", true, //
-      "Enables the integration with Project Intelligence.");
+      "Enables the integration with Project Intelligence (ingame documentation).");
+
+  public static final IValueFactory FPI = F.section(".projectIntelligence");
+
+  public static final IValue<Integer> marginTop = FPI.make("marginTop", 3, "Margin around the documentation.");
+  public static final IValue<Integer> marginLeft = FPI.make("marginLeft", 3, "Margin around the documentation.");
+  public static final IValue<Integer> marginRight = FPI.make("marginRight", 3, "Margin around the documentation.");
+  public static final IValue<Integer> marginBottom = FPI.make("marginBottom", 3, "Margin around the documentation.");
+
+  public static Rectangle rectangleWithPIMargins(int x, int y, int width, int height) {
+    return new Rectangle(x + marginLeft.get(), y + marginTop.get(), width - marginLeft.get() - marginRight.get(),
+        height - marginTop.get() - marginBottom.get());
+  }
 
   public static final IValue<Boolean> enableThaumcraftAspects = F.make("enableThaumcraftAspects", true, //
       "Registers aspects for Ender IO items with Thaumcraft.");
