f693f0fba13b9ba01d54b14654094e0d472254dd
Henry Loenwind <henry@loenwind.info>
2019-07-11 16:50:00 +0200

    Fixed de-duping of alloy recipes

--- enderio-base/src/main/java/crazypants/enderio/base/recipe/alloysmelter/AlloyRecipeManager.java
+++ enderio-base/src/main/java/crazypants/enderio/base/recipe/alloysmelter/AlloyRecipeManager.java
@@ -3,8 +3,10 @@ package crazypants.enderio.base.recipe.alloysmelter;
 import java.util.List;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import com.enderio.core.common.util.NNList;
+import com.enderio.core.common.util.NullHelper;
 import com.enderio.core.common.util.stackable.Things;
 
 import crazypants.enderio.base.Log;
@@ -53,7 +55,8 @@ public class AlloyRecipeManager extends ManyToOneRecipeManager {
     RecipeOutput recipeOutput = new RecipeOutput(output, 1, xpChance);
 
     if (prohibitDupes && input.size() > 1) {
-      // display recipe for JEI only (AlloyRecipeWrapper will de-dupe this with the same logic into a single display recipe)
+      Log.debug("Beginning de-duping loop for recipe that outputs ", output);
+      // display recipe for JEI only (AlloyRecipeWrapper will explode this into a single display recipe with rotating inputs)
       final Recipe recipe = new Recipe(recipeOutput, energyCost, RecipeBonusType.NONE, input.toArray(new IRecipeInput[input.size()])) {
         @Override
         public boolean isValidInput(int slot, @Nonnull ItemStack item) {
@@ -69,35 +72,39 @@ public class AlloyRecipeManager extends ManyToOneRecipeManager {
       List<ItemStack> list1 = inputStacks.size() >= 2 ? inputStacks.get(1) : new NNList<>(Prep.getEmpty());
       List<ItemStack> list2 = inputStacks.size() >= 3 ? inputStacks.get(2) : new NNList<>(Prep.getEmpty());
 
-      int count = 0;
+      Log.debug("Got these inputs:");
+      Log.debug(" slot 0: ", list0);
+      Log.debug(" slot 1: ", list1);
+      Log.debug(" slot 2: ", list2);
+
+      NNList<Tuple> list = new NNList<>();
 
       for (ItemStack stack0 : list0) {
-        if (stack0 != null) {
-          IRecipeInput rinp0 = new RecipeInput(stack0, stack0.getItemDamage() != OreDictionary.WILDCARD_VALUE, input.get(0).getMulitplier(),
-              input.get(0).getSlotNumber());
-          for (ItemStack stack1 : list1) {
-            if (stack1 != null && !eq(stack0, stack1)) {
-              IRecipeInput rinp1 = input.size() >= 2
-                  ? new RecipeInput(stack1, stack1.getItemDamage() != OreDictionary.WILDCARD_VALUE, input.get(1).getMulitplier(), input.get(1).getSlotNumber())
-                  : null;
-              for (ItemStack stack2 : list2) {
-                if (stack2 != null && !eq(stack0, stack2) && !eq(stack1, stack2)) {
-                  IRecipeInput rinp2 = input.size() >= 3 ? new RecipeInput(stack2, stack2.getItemDamage() != OreDictionary.WILDCARD_VALUE,
-                      input.get(2).getMulitplier(), input.get(2).getSlotNumber()) : null;
-                  addRecipe(new BasicManyToOneRecipe(new Recipe(recipeOutput, energyCost, RecipeBonusType.NONE,
-                      new NNList<>().addIf(rinp0).addIf(rinp1).addIf(rinp2).toArray(new IRecipeInput[0]))).setSynthetic());
-                  count++;
-                }
-              }
+        IRecipeInput rinp0 = new RecipeInput(stack0, stack0.getItemDamage() != OreDictionary.WILDCARD_VALUE, input.get(0).getMulitplier(),
+            input.get(0).getSlotNumber());
+        for (ItemStack stack1 : list1) {
+          IRecipeInput rinp1 = new RecipeInput(stack1, stack1.getItemDamage() != OreDictionary.WILDCARD_VALUE, input.get(1).getMulitplier(),
+              input.get(1).getSlotNumber());
+          for (ItemStack stack2 : list2) {
+            Tuple t = new Tuple(stack0, stack1, stack2);
+            if (t.isValid() && !list.contains(t)) {
+              list.add(t);
+              Log.debug("Found valid combination ", stack0, " / ", stack1, " / ", stack2);
+              addRecipe(new BasicManyToOneRecipe(new Recipe(recipeOutput, energyCost, RecipeBonusType.NONE,
+                  new NNList<>().addIf(rinp0).addIf(rinp1).addIf(input.size() >= 3 ? new RecipeInput(stack2,
+                      stack2.getItemDamage() != OreDictionary.WILDCARD_VALUE, input.get(2).getMulitplier(), input.get(2).getSlotNumber()) : null)
+                      .toArray(new IRecipeInput[0]))).setSynthetic());
+            } else {
+              Log.debug("Skipping invalid combination ", stack0, " / ", stack1, " / ", stack2);
             }
           }
         }
       }
 
-      if (count == 0) {
+      if (list.isEmpty()) {
         Log.warn("Splitting alloying recipe for ", output, " into sub-recipes yielded nothing. Something may be wrong with this recipe.");
       } else {
-        Log.debug("Split alloying recipe for ", output, " into ", count, " sub-recipes");
+        Log.debug("Split alloying recipe for ", output, " into ", list.size(), " sub-recipes");
         addRecipe(new BasicManyToOneRecipe(recipe).setDedupeInput());
       }
     } else {
@@ -130,6 +137,42 @@ public class AlloyRecipeManager extends ManyToOneRecipeManager {
       outputThing.setNbt(output.getTagCompound());
 
       TicProxy.registerAlloyRecipe(outputThing, inputs);
+    }
+  }
+
+  private static class Tuple {
+    protected final @Nonnull ItemStack stack0, stack1, stack2;
+
+    Tuple(@Nullable ItemStack stack0, @Nullable ItemStack stack1, @Nullable ItemStack stack2) {
+      this.stack0 = NullHelper.first(stack0, Prep.getEmpty());
+      this.stack1 = NullHelper.first(stack1, Prep.getEmpty());
+      this.stack2 = NullHelper.first(stack2, Prep.getEmpty());
+    }
+
+    boolean isValid() {
+      // Note: stack2 is optional, 0 and 1 are not
+      return Prep.isValid(stack0) && Prep.isValid(stack1) && !eq(stack0, stack1) && !eq(stack0, stack2) && !eq(stack2, stack1);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (!(o instanceof Tuple)) {
+        return false;
+      }
+      Tuple other = (Tuple) o;
+      return this == other || //
+          (eq(stack0, other.stack0) && eq(stack1, other.stack1) && eq(stack2, other.stack2)) || //
+          (eq(stack0, other.stack1) && eq(stack1, other.stack0) && eq(stack2, other.stack2)) || //
+          (eq(stack0, other.stack0) && eq(stack1, other.stack2) && eq(stack2, other.stack1)) || //
+          (eq(stack0, other.stack2) && eq(stack1, other.stack1) && eq(stack2, other.stack0)) || //
+          (eq(stack0, other.stack1) && eq(stack1, other.stack2) && eq(stack2, other.stack0)) || //
+          (eq(stack0, other.stack0) && eq(stack1, other.stack2) && eq(stack2, other.stack1));
+    }
+
+    @Override
+    public int hashCode() {
+      // not used, just making the compiler happy
+      return stack0.hashCode() ^ stack1.hashCode() ^ stack2.hashCode();
     }
   }
 
