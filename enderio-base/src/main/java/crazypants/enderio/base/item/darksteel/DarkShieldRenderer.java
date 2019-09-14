5aca03caa520c4dc48e21e4e9eacb73ac75ae98c
Henry Loenwind <henry@loenwind.info>
2019-09-14 22:45:27 +0200

    Added glint to our shield renderer

--- enderio-base/src/main/java/crazypants/enderio/base/item/darksteel/DarkShieldRenderer.java
+++ enderio-base/src/main/java/crazypants/enderio/base/item/darksteel/DarkShieldRenderer.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import javax.annotation.Nonnull;
 
+import org.lwjgl.opengl.GL11;
+
 import crazypants.enderio.api.upgrades.IEquipmentData;
 import crazypants.enderio.util.NNPair;
 import net.minecraft.client.Minecraft;
@@ -12,6 +14,7 @@ import net.minecraft.client.model.ModelShield;
 import net.minecraft.client.renderer.BannerTextures;
 import net.minecraft.client.renderer.BannerTextures.Cache;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.tileentity.TileEntityItemStackRenderer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -25,6 +28,11 @@ public final class DarkShieldRenderer extends TileEntityItemStackRenderer {
 
   public static final @Nonnull DarkShieldRenderer INSTANCE = new DarkShieldRenderer();
 
+  /**
+   * net.minecraft.client.renderer.RenderItem.RES_ITEM_GLINT is private
+   */
+  private static final @Nonnull ResourceLocation RES_ITEM_GLINT = new ResourceLocation("textures/misc/enchanted_item_glint.png");
+
   private static final @Nonnull Map<String, NNPair<ResourceLocation, BannerTextures.Cache>> TEX = new HashMap<>();
 
   private final @Nonnull TileEntityBanner banner = new TileEntityBanner();
@@ -34,8 +42,8 @@ public final class DarkShieldRenderer extends TileEntityItemStackRenderer {
   }
 
   @Override
-  public void renderByItem(@Nonnull ItemStack p_192838_1_, float partialTicks) {
-    Item item = p_192838_1_.getItem();
+  public void renderByItem(@Nonnull ItemStack stack, float partialTicks) {
+    Item item = stack.getItem();
     if (item instanceof ItemDarkSteelShield) {
 
       final IEquipmentData data = ((ItemDarkSteelShield) item).getEquipmentData();
@@ -43,8 +51,8 @@ public final class DarkShieldRenderer extends TileEntityItemStackRenderer {
       NNPair<ResourceLocation, Cache> tex = TEX.computeIfAbsent(shieldBase, base -> NNPair.of(new ResourceLocation(base + "_base_nopattern.png"),
           new BannerTextures.Cache("D" + data.getArmorMaterial().getName(), new ResourceLocation(base + "_base.png"), "textures/entity/shield/")));
 
-      if (p_192838_1_.getSubCompound("BlockEntityTag") != null) {
-        banner.setItemValues(p_192838_1_, true);
+      if (stack.getSubCompound("BlockEntityTag") != null) {
+        banner.setItemValues(stack, true);
         ResourceLocation resourceLocation = tex.getRight().getResourceLocation(banner.getPatternResourceLocation(), banner.getPatternList(),
             banner.getColorList());
         if (resourceLocation == null) {
@@ -58,9 +66,40 @@ public final class DarkShieldRenderer extends TileEntityItemStackRenderer {
       GlStateManager.pushMatrix();
       GlStateManager.scale(1.0F, -1.0F, -1.0F);
       modelShield.render();
+
+      // work around for https://bugs.mojang.com/browse/MC-69683
+      if (stack.hasEffect()) {
+        // see net.minecraft.client.renderer.RenderItem.renderEffect(IBakedModel)
+        GlStateManager.depthMask(false);
+        GlStateManager.depthFunc(GL11.GL_EQUAL);
+        GlStateManager.disableLighting();
+        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_COLOR, GlStateManager.DestFactor.ONE);
+        Minecraft.getMinecraft().getTextureManager().bindTexture(RES_ITEM_GLINT);
+        GlStateManager.matrixMode(GL11.GL_TEXTURE);
+        GlStateManager.pushMatrix();
+        GlStateManager.scale(.8F, .8F, .8F);
+        GlStateManager.translate((Minecraft.getSystemTime() >>> 3) % 3000L / 3000.0F, 0.0F, 0.0F); // reduce speed before scaling to avoid jerkyness
+        GlStateManager.rotate(-50.0F, 0.0F, 0.0F, 1.0F);
+        GlStateManager.color(0.65f, 0.65f, 0.65f, 1); // don't be that bright
+        modelShield.render();
+        GlStateManager.popMatrix();
+        GlStateManager.pushMatrix();
+        GlStateManager.scale(.8F, .8F, .8F);
+        GlStateManager.translate(-((Minecraft.getSystemTime() >> 2) % 4873L / 4873.0F), 0.0F, 0.0F); // reduce speed before scaling to avoid jerkyness
+        GlStateManager.rotate(10.0F, 0.0F, 0.0F, 1.0F);
+        modelShield.render();
+        GlStateManager.popMatrix();
+        GlStateManager.matrixMode(GL11.GL_MODELVIEW);
+        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+        GlStateManager.enableLighting();
+        GlStateManager.depthFunc(GL11.GL_LEQUAL);
+        GlStateManager.depthMask(true);
+        Minecraft.getMinecraft().getTextureManager().bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+      }
+
       GlStateManager.popMatrix();
     } else {
-      throw new RuntimeException(p_192838_1_ + " is not one of our shields. Why should we render it?");
+      throw new RuntimeException(stack + " is not one of our shields. Why should we render it?");
     }
   }
 
