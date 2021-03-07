e652277b4aeaeaf15e2faf51c898d2e56ccc1294
Henry Loenwind <henry@loenwind.info>
2021-03-07 13:29:44 +0100

    Recovered code lost in 2016

--- enderio-base/src/main/java/crazypants/enderio/base/integration/thaumcraft/ThaumcraftUtil.java
+++ enderio-base/src/main/java/crazypants/enderio/base/integration/thaumcraft/ThaumcraftUtil.java
@@ -2,12 +2,19 @@ package crazypants.enderio.base.integration.thaumcraft;
 
 import javax.annotation.Nonnull;
 
+import com.enderio.core.common.util.NNList;
+import com.enderio.core.common.util.stackable.Things;
+
+import crazypants.enderio.api.farm.IFarmerJoe;
 import crazypants.enderio.api.upgrades.IDarkSteelUpgrade;
 import crazypants.enderio.base.EnderIO;
 import crazypants.enderio.base.Log;
 import crazypants.enderio.base.config.config.IntegrationConfig;
 import crazypants.enderio.base.events.EnderIOLifecycleEvent;
 import crazypants.enderio.base.farming.FarmersRegistry;
+import crazypants.enderio.util.Prep;
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -37,6 +44,28 @@ public class ThaumcraftUtil {
   @SubscribeEvent
   public static void registerHoes(@Nonnull EnderIOLifecycleEvent.Init.Pre event) {
     FarmersRegistry.registerHoes(MODID_THAUMCRAFT, "thaumium_hoe", "void_hoe", "elemental_hoe");
+    Log.info("Farming Station: Thaumcraft integration for hoes loaded");
+  }
+
+  @SubscribeEvent
+  public static void registerFarmers(@Nonnull RegistryEvent.Register<IFarmerJoe> event) {
+    if (Loader.isModLoaded(MODID_THAUMCRAFT)) {
+      String manaBean = "ItemManaBean"; // TODO find real IDs, check if the farmer still works in 1.12
+      String manaPod = "blockManaPod";
+
+      Things blockThing = new Things("block:" + MODID_THAUMCRAFT + ":" + manaPod);
+      Things itemThing = new Things("block:" + MODID_THAUMCRAFT + ":" + manaBean);
+
+      NNList<Block> blocks = blockThing.getBlocks();
+      ItemStack itemStack = itemThing.getItemStack();
+
+      if (!blocks.isEmpty() && Prep.isValid(itemStack)) {
+        event.getRegistry().register(new ManaBeanFarmer(blocks.get(0), itemStack).setRegistryName(MODID_THAUMCRAFT, "manabean"));
+        Log.info("Farming Station: Thaumcraft integration for farming mana beans loaded");
+      } else {
+        Log.info("Farming Station: Thaumcraft integration for farming mana beans not loaded");
+      }
+    }
   }
 
 }
