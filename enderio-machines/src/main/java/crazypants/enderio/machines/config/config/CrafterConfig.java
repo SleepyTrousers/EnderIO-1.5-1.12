5bcaa6be4bf5bdc69f978038808a4b64ba9a8327
Henry Loenwind <henry@loenwind.info>
2021-03-13 22:47:23 +0100

    Fixed crafter config
    * re: #5511

--- enderio-machines/src/main/java/crazypants/enderio/machines/config/config/CrafterConfig.java
+++ enderio-machines/src/main/java/crazypants/enderio/machines/config/config/CrafterConfig.java
@@ -6,9 +6,9 @@ import info.loenwind.autoconfig.factory.IValueFactory;
 
 public final class CrafterConfig {
 
-  public static final IValueFactory F = Config.F.section("generator.stirling");
+  public static final IValueFactory F = Config.F.section("crafter");
 
-  public static final IValue<Boolean> respectsGravity = F.make("respectsGravitySimple", true,
-      "If true, the Simple Crafter will respect gravity and fall like an anvil when not attached to a block.").sync();
+  public static final IValue<Boolean> respectsGravity = F
+      .make("respectsGravitySimple", true, "If true, the Simple Crafter will respect gravity and fall like an anvil when not attached to a block.").sync();
 
 }
