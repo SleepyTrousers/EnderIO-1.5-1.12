2503f44b62c90e2eeaf6595b6a02dd47faa438c6
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2021-09-29 11:12:52 +0200

    Simple SoulboundHandler (#6)
    * fsmall fixes
    
    +format
    +TODO's
    
    * Update SoulBoundHandler.java
    
    Made a basic SoulboundHandler

--- enderio-base/src/main/java/com/enderio/base/common/enchantments/SoulBoundHandler.java
+++ enderio-base/src/main/java/com/enderio/base/common/enchantments/SoulBoundHandler.java
@@ -10,65 +10,57 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.GameRules;
-import net.minecraftforge.client.event.ClientPlayerNetworkEvent.RespawnEvent;
 import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-//TODO
+//TODO testing against graves and other mods
 @EventBusSubscriber
 public class SoulBoundHandler {
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void deathHandler(LivingDropsEvent event) {
-        // TODO: Integration with EnderIO graves system.
         if (event.getEntityLiving() == null || event.getEntityLiving() instanceof FakePlayer || event.isCanceled()) {
             return;
         }
-        if (event.getEntityLiving().level
-            .getGameRules()
-            .getBoolean(GameRules.RULE_KEEPINVENTORY)) {
+        if (event.getEntityLiving().level.getGameRules().getBoolean(GameRules.RULE_KEEPINVENTORY)) {
             return;
         }
-        ArrayList<ItemStack> drops = new ArrayList<ItemStack>();
-        if (event.getEntityLiving() instanceof Player player) {
-            Iterator<ItemEntity> iter = event
-                .getDrops()
-                .iterator();
+        ArrayList<ItemStack> soulitems = new ArrayList<ItemStack>();
+        if (event.getEntityLiving()instanceof Player player) {
+            Iterator<ItemEntity> iter = event.getDrops().iterator();
             while (iter.hasNext()) {
                 ItemEntity ei = iter.next();
                 ItemStack item = ei.getItem();
                 if (isSoulBound(item)) {
                     if (player.addItem(item)) {
                         iter.remove();
-                    }
-                    drops.add(item);
+                    }//TODO More detailed and better item storage.
+                    soulitems.add(item);
                 }
             }
-            if (!drops.isEmpty()) {
+            if (soulitems.isEmpty()) {
                 return;
             }
-            drops.forEach((item) -> player.addItem(item));
+            soulitems.forEach((item) -> player.addItem(item));
         }
     }
 
-    public static void reviveHandler(RespawnEvent event) {
-
-        if (!event
-            .getOldPlayer()
-            .isDeadOrDying()) {
+    @SubscribeEvent
+    public static void reviveHandler(PlayerEvent.Clone event) {
+        if (!event.getOriginal().isDeadOrDying()) {
             return;
-        }
-        event
-            .getOldPlayer()
-            .getInventory().items.forEach((item -> event
-                .getNewPlayer()
-                .addItem(item)));
+        }//TODO More detailed and better item recovery.
+        event.getOriginal().getInventory().items.forEach(item -> event.getPlayer().addItem(item));
+        event.getOriginal().getInventory().armor.forEach(armor -> event.getPlayer().addItem(armor));
+        event.getOriginal().getInventory().offhand.forEach(offhand -> event.getPlayer().addItem(offhand));
     }
 
     public static boolean isSoulBound(ItemStack item) {
-        return EnchantmentHelper.getItemEnchantmentLevel(EIOEnchantments.SOULBOUND.get(), item) > -1;
+        return EnchantmentHelper.getItemEnchantmentLevel(EIOEnchantments.SOULBOUND.get(), item) > 0;
     }
+    
 }
