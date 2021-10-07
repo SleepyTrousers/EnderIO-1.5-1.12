04a67752a1481c5bfc4746fa82876dcc2573452d
agnor99 <36055315+agnor99@users.noreply.github.com>
2021-10-07 23:25:15 +0200

    gotta climb fast (#14)

--- enderio-base/src/main/java/com/enderio/base/common/block/DarkSteelLadderBlock.java
+++ enderio-base/src/main/java/com/enderio/base/common/block/DarkSteelLadderBlock.java
@@ -1,26 +1,12 @@
 package com.enderio.base.common.block;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.MoverType;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.LadderBlock;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.Vec3;
-
-import javax.annotation.Nonnull;
 
 public class DarkSteelLadderBlock extends LadderBlock {
+
     public DarkSteelLadderBlock(Properties p_54345_) {
         super(p_54345_);
     }
 
-    @Override
-    public void entityInside(@Nonnull BlockState pState, @Nonnull Level pLevel, @Nonnull BlockPos pPos, @Nonnull Entity pEntity) {
-        if (!(pEntity instanceof Player playerEntity) || pEntity.isOnGround() || pEntity.isCrouching() || !playerEntity.onClimbable())
-            return;
-
-        // TODO: Make the player go faster.
-    }
+    //Still here for adding textdata
 }
---
+++ enderio-base/src/main/java/com/enderio/base/common/block/DarkSteelLadderHandler.java
@@ -0,0 +1,31 @@
+package com.enderio.base.common.block;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.entity.MoverType;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.LogicalSide;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(Dist.CLIENT)
+public class DarkSteelLadderHandler {
+
+    @SubscribeEvent
+    public static void onTick(TickEvent.PlayerTickEvent playerTickEvent) {
+        if (playerTickEvent.phase == TickEvent.Phase.START && playerTickEvent.side == LogicalSide.CLIENT && playerTickEvent.player == Minecraft.getInstance().player) {
+            LocalPlayer player = Minecraft.getInstance().player;
+            if (player.onClimbable() && player.level.getBlockState(player.blockPosition()).is(EIOBlocks.DARK_STEEL_LADDER.get())) {
+                if (!Minecraft.getInstance().options.keyShift.isDown()) {
+                    if (Minecraft.getInstance().options.keyUp.isDown()) {
+                        player.move(MoverType.SELF, new Vec3(0,0.15f,0));
+                    } else {
+                        player.move(MoverType.SELF, new Vec3(0,-0.15f,0));
+                    }
+                }
+            }
+        }
+    }
+}
