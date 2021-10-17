ed4fdcf3fba3834259253834ff6b18a0616d3c5e
CrazyPants <crazypants.mc@gmail.com>
2021-10-17 22:13:53 +0800

    Added the rest of the recipes

--- enderio-base/src/main/java/com/enderio/base/data/recipe/standard/BlockRecipes.java
+++ enderio-base/src/main/java/com/enderio/base/data/recipe/standard/BlockRecipes.java
@@ -23,13 +23,26 @@ public class BlockRecipes extends RecipeProvider {
 
     @Override
     protected void buildCraftingRecipes(@Nonnull Consumer<FinishedRecipe> recipeConsumer) {
+        //eio plates
         addPressurePlateRecipe(recipeConsumer, EIOBlocks.DARK_STEEL_PRESSURE_PLATE, EIOItems.DARK_STEEL_INGOT::get);
         addSilentPressurePlateRecipe(recipeConsumer, EIOBlocks.SILENT_DARK_STEEL_PRESSURE_PLATE, EIOBlocks.DARK_STEEL_PRESSURE_PLATE.get());
         addPressurePlateRecipe(recipeConsumer, EIOBlocks.SOULARIUM_PRESSURE_PLATE, EIOItems.SOULARIUM_INGOT::get);
         addSilentPressurePlateRecipe(recipeConsumer, EIOBlocks.SILENT_SOULARIUM_PRESSURE_PLATE, EIOBlocks.SOULARIUM_PRESSURE_PLATE.get());
-
+        //wooden silent plates
         addSilentPressurePlateRecipe(recipeConsumer, EIOBlocks.SILENT_OAK_PRESSURE_PLATE, Blocks.OAK_PRESSURE_PLATE);
+        addSilentPressurePlateRecipe(recipeConsumer,  EIOBlocks.SILENT_ACACIA_PRESSURE_PLATE, Blocks.ACACIA_PRESSURE_PLATE);
+        addSilentPressurePlateRecipe(recipeConsumer,  EIOBlocks.SILENT_DARK_OAK_PRESSURE_PLATE , Blocks.DARK_OAK_PRESSURE_PLATE);
+        addSilentPressurePlateRecipe(recipeConsumer,  EIOBlocks.SILENT_SPRUCE_PRESSURE_PLATE, Blocks.SPRUCE_PRESSURE_PLATE);
+        addSilentPressurePlateRecipe(recipeConsumer,  EIOBlocks.SILENT_BIRCH_PRESSURE_PLATE, Blocks.BIRCH_PRESSURE_PLATE);
+        addSilentPressurePlateRecipe(recipeConsumer,  EIOBlocks.SILENT_JUNGLE_PRESSURE_PLATE, Blocks.JUNGLE_PRESSURE_PLATE);
+        addSilentPressurePlateRecipe(recipeConsumer,  EIOBlocks.SILENT_CRIMSON_PRESSURE_PLATE , Blocks.CRIMSON_PRESSURE_PLATE);
+        addSilentPressurePlateRecipe(recipeConsumer,  EIOBlocks.SILENT_WARPED_PRESSURE_PLATE, Blocks.WARPED_PRESSURE_PLATE);
+        //stone silent plates
         addSilentPressurePlateRecipe(recipeConsumer, EIOBlocks.SILENT_STONE_PRESSURE_PLATE, Blocks.STONE_PRESSURE_PLATE);
+        addSilentPressurePlateRecipe(recipeConsumer, EIOBlocks.SILENT_POLISHED_BLACKSTONE_PRESSURE_PLATE,Blocks.POLISHED_BLACKSTONE_PRESSURE_PLATE);
+        //wighted silent plates
+        addSilentPressurePlateRecipe(recipeConsumer, EIOBlocks.SILENT_HEAVY_WEIGHTED_PRESSURE_PLATE, Blocks.HEAVY_WEIGHTED_PRESSURE_PLATE);
+        addSilentPressurePlateRecipe(recipeConsumer, EIOBlocks.SILENT_LIGHT_WEIGHTED_PRESSURE_PLATE, Blocks.LIGHT_WEIGHTED_PRESSURE_PLATE);
     }
 
     private void addPressurePlateRecipe(Consumer<FinishedRecipe> recipeConsumer, BlockEntry<? extends Block> result, ItemLike ingredient) {
