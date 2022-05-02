6a00ab5d88acdf91f8344ff054bd69829125af79
MuXiu1997 <MuXiu1997@Gmail.com>
2022-05-03 02:06:16 +0800

    Fix TravelUpgrade (#66)

--- src/main/java/crazypants/enderio/item/darksteel/upgrade/TravelUpgrade.java
+++ src/main/java/crazypants/enderio/item/darksteel/upgrade/TravelUpgrade.java
@@ -7,9 +7,11 @@ import crazypants.enderio.material.Material;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 
+import java.util.stream.Stream;
+
 public class TravelUpgrade extends AbstractUpgrade {
 
-  private static String UPGRADE_NAME = "travel";
+  private static final String UPGRADE_NAME = "travel";
 
   public static final TravelUpgrade INSTANCE = new TravelUpgrade();
 
@@ -32,17 +34,18 @@ public class TravelUpgrade extends AbstractUpgrade {
   }
 
   public TravelUpgrade() {
-    super(UPGRADE_NAME, "enderio.darksteel.upgrade.travel", new ItemStack(EnderIO.itemMaterial,1,Material.ENDER_CRYSTAL.ordinal()), Config.darkSteelTravelCost);
+    super(UPGRADE_NAME, "enderio.darksteel.upgrade.travel", new ItemStack(EnderIO.itemMaterial, 1, Material.ENDER_CRYSTAL.ordinal()), Config.darkSteelTravelCost);
   }
 
   @Override
   public boolean canAddToItem(ItemStack stack) {
-    if(stack == null || (stack.getItem() != DarkSteelItems.itemDarkSteelSword && stack.getItem() != DarkSteelItems.itemEndSteelPickaxe && stack.getItem() != DarkSteelItems.itemEndSteelSword && stack.getItem() != DarkSteelItems.itemDarkSteelPickaxe)|| !EnergyUpgrade.itemHasAnyPowerUpgrade(stack)) {
-      return false;
-    }
-    TravelUpgrade up = loadFromItem(stack);
-    if(up == null) {
-      return true;
+    if(stack == null) return false;
+    if(Stream.of(
+      DarkSteelItems.itemDarkSteelSword, DarkSteelItems.itemEndSteelSword, DarkSteelItems.itemStellarSword,
+      DarkSteelItems.itemDarkSteelPickaxe, DarkSteelItems.itemEndSteelPickaxe, DarkSteelItems.itemStellarPickaxe,
+      DarkSteelItems.itemEndSteelAxe, DarkSteelItems.itemStellarAxe
+    ).anyMatch(item -> stack.getItem() == item)) {
+      return EnergyUpgrade.itemHasAnyPowerUpgrade(stack) && loadFromItem(stack) == null;
     }
     return false;
   }
