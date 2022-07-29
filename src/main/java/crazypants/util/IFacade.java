eb4401f755f2e57adc272133f6c44198f2b9e728
miozune <miozune@gmail.com>
2022-07-29 20:43:27 +0900

    Fix crash when Waila is loaded and Chisel is not loaded (#80)

--- src/main/java/crazypants/util/IFacade.java
+++ src/main/java/crazypants/util/IFacade.java
@@ -2,8 +2,12 @@ package crazypants.util;
 
 import cofh.api.block.IBlockAppearance;
 import cpw.mods.fml.common.Optional;
+import net.minecraft.block.Block;
+import net.minecraft.world.IBlockAccess;
 
 @Optional.Interface(modid = "chisel", iface = "com.cricketcraft.chisel.api.IFacade")
 public interface IFacade extends com.cricketcraft.chisel.api.IFacade, IBlockAppearance {
-    // :(
+    Block getFacade(IBlockAccess world, int x, int y, int z, int side);
+
+    int getFacadeMetadata(IBlockAccess world, int x, int y, int z, int side);
 }
