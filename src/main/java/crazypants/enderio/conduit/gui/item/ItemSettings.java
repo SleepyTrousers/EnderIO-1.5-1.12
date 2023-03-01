7f6a0a6a4c620c1e9f197b0616c3a444dc3ef4af
Raven Szewczyk <git@eigenraven.me>
2023-03-01 06:32:43 +0000

    Fix existing item filter (#101)
    * Update buildscript and dependencies
    
    * Remove the extra conduit packet update that overrides some of the previous change packets
    
    * spotlessApply (#102)
    
    Co-authored-by: GitHub GTNH Actions <>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/crazypants/enderio/conduit/gui/item/ItemSettings.java
+++ src/main/java/crazypants/enderio/conduit/gui/item/ItemSettings.java
@@ -25,7 +25,6 @@ import crazypants.enderio.conduit.gui.GuiExternalConnection;
 import crazypants.enderio.conduit.item.IItemConduit;
 import crazypants.enderio.conduit.item.filter.IItemFilter;
 import crazypants.enderio.conduit.packet.PacketExtractMode;
-import crazypants.enderio.conduit.packet.PacketItemConduitFilter;
 import crazypants.enderio.gui.IconEIO;
 import crazypants.enderio.gui.RedstoneModeButton;
 import crazypants.enderio.machine.IRedstoneModeControlable;
@@ -216,7 +215,6 @@ public class ItemSettings extends BaseSettingsPanel {
         }
         if (insertFilterGui != null) insertFilterGui.actionPerformed(guiButton);
         if (extractFilterGui != null) extractFilterGui.actionPerformed(guiButton);
-        PacketHandler.INSTANCE.sendToServer(new PacketItemConduitFilter(itemConduit, gui.getDir()));
     }
 
     @Override
